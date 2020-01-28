2264c2507b8baf9adc16e1809ef73afa4a1c2281
cygnus-x-1 <39781856+cygnus-x-1@users.noreply.github.com>
2020-01-28 18:03:48 +1100

    --added a comment to highlight a TODO

--- CoreTableView/src/au/gov/asd/tac/constellation/views/tableview2/TableViewTopComponent.java
+++ CoreTableView/src/au/gov/asd/tac/constellation/views/tableview2/TableViewTopComponent.java
@@ -171,13 +171,13 @@ public final class TableViewTopComponent extends JavaFxTopComponent<TableViewPan
                 });
             }
 
-            final Thread updateThread = new Thread("Table View: Update Table") {
+            final Thread tableUpdateThread = new Thread("Table View: Update Table") {
                 @Override
                 public void run() {
                     pane.updateTable(graph, currentState);
                 }
             };
-            updateThread.start();
+            tableUpdateThread.start();
 
             if (currentState != null && currentState.getColumnAttributes() != null && !columnAttributeChanges.getSecond().isEmpty()) {
                 columnAttributeChanges.getSecond().forEach(attributeTuple -> {
@@ -185,13 +185,13 @@ public final class TableViewTopComponent extends JavaFxTopComponent<TableViewPan
                             attributeTuple.getSecond().getElementType(),
                             attributeTuple.getSecond().getName(),
                             g -> {
-                                final Thread thread = new Thread("Table View: Update Data") {
+                                final Thread dataUpdateThread = new Thread("Table View: Update Data") {
                                     @Override
                                     public void run() {
                                         pane.updateData(g, currentState);
                                     }
                                 };
-                                thread.start();
+                                dataUpdateThread.start();
                             }));
                 });
             }
@@ -225,6 +225,19 @@ public final class TableViewTopComponent extends JavaFxTopComponent<TableViewPan
             public void run() {
                 while (stateLock != null && currentState == stateSnapshot) {
                     try {
+                        // TODO: REMOVE THIS!
+                        // ...but there is an async issue which needs to be 
+                        // resolved first. When showSelected() is called, the 
+                        // order of operations is to update the Table View 
+                        // state (if required) and then to select the rows in 
+                        // the table based on the current graph selection. The 
+                        // issue is that the state is updated by writing a 
+                        // TableViewState object to the graph and letting a 
+                        // Table View listener respond to that. Unfortunately, 
+                        // there is no obvious way for this operation to know 
+                        // when the Table View listener has finished responding, 
+                        // so for now we just wait until the currentState object 
+                        // matches the state object we updated it to.
                         Thread.sleep(10);
                     } catch (final InterruptedException ex) {
                         // DO NOTHING
