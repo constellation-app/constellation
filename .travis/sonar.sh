ee7f25d876b9d93cb405cb35a66fa707d9c17cbb
TheDen <deniskhoshaba@gmail.com>
2020-02-05 23:42:40 +1100

    remove sonarqube arg

--- .travis/sonar.sh
+++ .travis/sonar.sh
@@ -2,13 +2,13 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   SONAR_PULLREQUEST_BRANCH="$(echo ${TRAVIS_PULL_REQUEST_SLUG} | awk '{split($0,a,"/"); print a[1]}')/${TRAVIS_PULL_REQUEST_BRANCH}"
-  sonar-scanner sonarqube \
+  sonar-scanner \
     -Dsonar.login="${SONAR_TOKEN}" \
     -Dsonar.pullrequest.key="${TRAVIS_PULL_REQUEST}" \
     -Dsonar.pullrequest.branch="${SONAR_PULLREQUEST_BRANCH}" \
     -Dsonar.pullrequest.base="${TRAVIS_BRANCH}"
 else
-  sonar-scanner sonarqube \
+  sonar-scanner \
     -Dsonar.login="${SONAR_TOKEN}" \
     -Dsonar.branch.name="${TRAVIS_BRANCH}"
 fi
