4f21950c52d66623b31c933ab56039aee5fcff4c
TheDen <deniskhoshaba@gmail.com>
2020-02-05 23:25:47 +1100

    refactor sonar scanning

--- .travis/sonar.sh
+++ .travis/sonar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  SONAR_PULLREQUEST_BRANCH="$(echo $TRAVIS_PULL_REQUEST_SLUG | awk '{split($0,a,"/"); print a[1]}')/$TRAVIS_PULL_REQUEST_BRANCH"
+  SONAR_PULLREQUEST_BRANCH="$(echo ${TRAVIS_PULL_REQUEST_SLUG} | awk '{split($0,a,"/"); print a[1]}')/${TRAVIS_PULL_REQUEST_BRANCH}"
   sonar-scanner sonarqube \
     -Dsonar.login="${SONAR_TOKEN}" \
     -Dsonar.pullrequest.key="${TRAVIS_PULL_REQUEST}" \
