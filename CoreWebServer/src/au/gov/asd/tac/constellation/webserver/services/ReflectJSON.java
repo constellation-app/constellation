f7d128eed17732730d9aee25544e8b7e60517b7f
algol60 <algol60@users.noreply.github.com>
2020-03-10 21:12:09 +1100

    Add reflection and named color services.

---
+++ CoreWebServer/src/au/gov/asd/tac/constellation/webserver/services/ReflectJSON.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010-2020 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.webserver.services;
+
+import au.gov.asd.tac.constellation.pluginframework.parameters.PluginParameters;
+import au.gov.asd.tac.constellation.webserver.restapi.RestService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author algol
+ */
+@ServiceProvider(service=RestService.class)
+public class ReflectJSON extends RestService {
+    private static final String NAME = "reflect_json";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Reflect the incoming JSON as output. This is a tet to ensure JSON input and output works.";
+    }
+
+    @Override
+    public void service(final PluginParameters parameters, final InputStream in, final OutputStream out) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonNode json = mapper.readTree(in);
+        mapper.writeValue(out, json);
+    }
+}
