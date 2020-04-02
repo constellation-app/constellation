51af378ed16adc3b83987a8dea40bd889dc97dbc
antares1470 <62311243+antares1470@users.noreply.github.com>
2020-04-02 13:45:36 +1100

    minor change

--- CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/infomap/infomap/InfomapBase.java
+++ CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/infomap/infomap/InfomapBase.java
@@ -761,7 +761,7 @@ public abstract class InfomapBase {
         if (verbose) {
             if (config.getVerbosity() == 0) {
                 final String fmt = String.format("%s.%df", "%", config.getVerboseNumberPrecision());
-                System.out.printf("to %d modules with codelength %s%n", getNumTopModules(),fmt, codelength);
+                System.out.printf("to %d modules with codelength %s%n", getNumTopModules(), fmt, codelength);
             } else {
                 System.out.printf("Two-level codelength: %f + %f = %f%n", indexCodelength, moduleCodelength, codelength);
             }
