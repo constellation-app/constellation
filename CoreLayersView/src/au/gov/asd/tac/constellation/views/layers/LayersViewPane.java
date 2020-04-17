af66ae49c6ac87195139d894e6c4448a602bbc76
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2020-04-17 14:49:20 +0930

    Switch Layers to GridPane
    Update UI with Gridpane

--- CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/LayersViewPane.java
+++ CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/LayersViewPane.java
@@ -22,6 +22,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -32,10 +33,6 @@ import org.openide.NotifyDescriptor;
 /**
  * Layers View Pane.
  *
- * TODO: Note the code initially in this file is for proof of concept and trial
- * only. Full scale implementation will require refactoring and neatening of UI
- * elements.
- *
  * @author aldebaran30701
  */
 public class LayersViewPane extends BorderPane {
@@ -43,8 +40,8 @@ public class LayersViewPane extends BorderPane {
     private static final String DEFAULT_LAYER_PLACEHOLDER = "Default";
 
     private final LayersViewController controller;
+    private final GridPane layersGridPane;
     private final VBox layersViewPane;
-    private final VBox layers;
     private final HBox options;
     private int currentIndex;
 
@@ -55,33 +52,30 @@ public class LayersViewPane extends BorderPane {
 
         // create layers
         final Label layerIdHeadingText = new Label("Layer\nID");
-        HBox.setHgrow(layerIdHeadingText, Priority.ALWAYS);
         final Label visibilityHeadingText = new Label("Visibility");
-        HBox.setHgrow(visibilityHeadingText, Priority.ALWAYS);
         final Label queryHeadingText = new Label("Query");
-        HBox.setHgrow(queryHeadingText, Priority.ALWAYS);
         final Label descriptionHeadingText = new Label("Description");
-        HBox.setHgrow(descriptionHeadingText, Priority.ALWAYS);
-        final HBox headingBox = new HBox(5, layerIdHeadingText,
+
+        // create gridpane and alignments
+        layersGridPane = new GridPane();
+        layersGridPane.setHgap(5);
+        layersGridPane.setVgap(5);
+        layersGridPane.setPadding(new Insets(0, 10, 10, 10));
+        layersGridPane.addRow(0, layerIdHeadingText,
                 visibilityHeadingText, queryHeadingText, descriptionHeadingText);
-        VBox.setMargin(headingBox, new Insets(5));
-        VBox.setVgrow(headingBox, Priority.ALWAYS);
 
         // set heading alignments
+        GridPane.setMargin(layerIdHeadingText, new Insets(15, 0, 0, 0));
         layerIdHeadingText.setTextAlignment(TextAlignment.CENTER);
         layerIdHeadingText.setMinWidth(40);
         layerIdHeadingText.setMinHeight(25);
         layerIdHeadingText.setPrefWidth(30);
-        visibilityHeadingText.setPrefWidth(50);
+        visibilityHeadingText.setPrefWidth(55);
         visibilityHeadingText.setMinWidth(50);
         queryHeadingText.setPrefWidth(10000);
         queryHeadingText.setMinWidth(80);
         descriptionHeadingText.setPrefWidth(10000);
         descriptionHeadingText.setMinWidth(80);
-
-        this.layers = new VBox(5, headingBox);
-        layers.setAlignment(Pos.TOP_LEFT);
-//        layers.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
 
         // add default layers
         this.currentIndex = 0;
@@ -92,7 +86,7 @@ public class LayersViewPane extends BorderPane {
         final Button addButton = new Button("Add New Layer");
         addButton.setAlignment(Pos.CENTER_RIGHT);
         addButton.setOnAction(event -> {
-            if (layers.getChildren().size() <= 32) {
+            if (layersGridPane.getRowCount() <= 32) { // 32 layers plus headings
                 createLayer(false);
             } else {
                 final NotifyDescriptor nd = new NotifyDescriptor.Message(
@@ -105,16 +99,13 @@ public class LayersViewPane extends BorderPane {
 
         this.options = new HBox(5, addButton);
         options.setAlignment(Pos.TOP_LEFT);
-//        options.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        options.setPadding(new Insets(0, 0, 0, 10));
 
-        // add layers and options to pane
-        this.layersViewPane = new VBox(5, layers, options);
-//        layersViewPane.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        // add layers grid and options to pane
+        this.layersViewPane = new VBox(5, layersGridPane, options);
 
         // create layout bindings
-        headingBox.prefWidthProperty().bind(layers.widthProperty());
         layersViewPane.prefWidthProperty().bind(this.widthProperty());
-        layers.prefWidthProperty().bind(layersViewPane.widthProperty());
         options.prefWidthProperty().bind(layersViewPane.widthProperty());
 
         this.setCenter(layersViewPane);
@@ -123,26 +114,23 @@ public class LayersViewPane extends BorderPane {
     private void createLayer(final boolean defaultLayer) {
         final Label layerIdText = new Label(String.format("%02d", ++currentIndex));
         layerIdText.setMinWidth(30);
+        layerIdText.setPrefWidth(40);
         layerIdText.setTextAlignment(TextAlignment.CENTER);
-        layerIdText.setPadding(new Insets(0,0,0,10));
-        HBox.setHgrow(layerIdText, Priority.ALWAYS);
+        layerIdText.setPadding(new Insets(0, 0, 0, 10));
 
         final CheckBox visibilityCheckBox = new CheckBox();
+        visibilityCheckBox.setMinWidth(60);
+        visibilityCheckBox.setPadding(new Insets(0, 30, 0, 15));
         visibilityCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
             controller.submit();
             controller.execute();
         });
-        HBox.setHgrow(visibilityCheckBox, Priority.ALWAYS);
-        visibilityCheckBox.setMinWidth(60);
-        visibilityCheckBox.setPadding(new Insets(0,25,0,25));
 
         final TextArea queryTextArea = new TextArea();
         queryTextArea.setPrefRowCount(1);
-        HBox.setHgrow(queryTextArea, Priority.ALWAYS);
 
         final TextArea descriptionTextArea = new TextArea();
         descriptionTextArea.setPrefRowCount(1);
-        HBox.setHgrow(descriptionTextArea, Priority.ALWAYS);
 
         if (defaultLayer) {
             layerIdText.setDisable(true);
@@ -153,20 +141,12 @@ public class LayersViewPane extends BorderPane {
             descriptionTextArea.setText("Show All");
             descriptionTextArea.setDisable(true);
         }
-
-        final HBox layerBox = new HBox(5, layerIdText,
+        layersGridPane.addRow(currentIndex, layerIdText,
                 visibilityCheckBox, queryTextArea, descriptionTextArea);
-        VBox.setMargin(layerBox, new Insets(5));
-        VBox.setVgrow(layerBox, Priority.ALWAYS);
-        layerBox.setAlignment(Pos.CENTER);
-        layers.getChildren().add(layerBox);
-
-        layerIdText.setPrefWidth(40);
-        layerBox.prefWidthProperty().bind(layers.widthProperty());
     }
 
-    public VBox getLayers() {
-        return layers;
+    public GridPane getLayers() {
+        return layersGridPane;
     }
 
     public HBox getOptions() {
