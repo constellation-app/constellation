d2b287303ab2bf2a906ac53119b869152bde83bb
Guilty-Spark-343 <63685177+Guilty-Spark-343@users.noreply.github.com>
2020-05-11 12:25:54 +1000

    Add feedback dialog for delimter importer

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/ActionPane.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/ActionPane.java
@@ -16,13 +16,20 @@
 package au.gov.asd.tac.constellation.plugins.importexport.delimited;
 
 import au.gov.asd.tac.constellation.plugins.PluginException;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -33,8 +40,24 @@ import javafx.scene.layout.HBox;
 public class ActionPane extends BorderPane {
     
     private static final Logger LOGGER = Logger.getLogger(ActionPane.class.getName());
+    final private static String SUCCESS_ICON_PATH = "au/gov/asd/tac/constellation/plugins/importexport/delimited/resources/success.jpg";
 
     private final ImportController importController;
+    
+    private void displayAlert(String title, String header, boolean successful){
+        final Alert dialog;
+        if(successful){
+            dialog = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.OK);
+            final ImageView SUCCESS_ICON = new ImageView(SUCCESS_ICON_PATH);
+            dialog.setGraphic(SUCCESS_ICON);
+            
+        }else{
+            dialog = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
+        }
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.showAndWait();
+    }
 
     public ActionPane(final ImportController importController) {
         this.importController = importController;
@@ -57,12 +80,26 @@ public class ActionPane extends BorderPane {
             @Override
             public void handle(ActionEvent t) {
                 try {
-                    importController.processImport();
+                    
+                    List<File> importedFiles = importController.processImport();
+                    String[] filenames = new String[importedFiles.size()];
+                    long noOfRows = 0;
+                    for (int i = 0; i < importedFiles.size(); i++) {
+                        filenames[i] = importedFiles.get(i).getName();
+                        Path path = importedFiles.get(i).toPath();
+                        noOfRows += Files.lines(path).count();
+                    }
+                    displayAlert("Success", "Successfully imported "+ noOfRows
+                            +" rows from the following file(s):\n"+String.join("\n", filenames), true);
+                    
                 } catch (IOException ex) {
                     LOGGER.log(Level.SEVERE, ex.getLocalizedMessage(), ex);
+                    displayAlert("Import Failed", ex.getLocalizedMessage(), false);
                 } catch (InterruptedException ex) {
                     Thread.currentThread().interrupt();
+                    displayAlert("import Failed", ex.getLocalizedMessage(), false);
                 } catch (PluginException ex) {
+                    displayAlert("Import Failed", ex.getLocalizedMessage(), false);
                 }
             }
         });
