50addda996358cac5fb5a98b6bae4fc8883a9d8d
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2020-05-14 12:56:28 +0930

    Add context menu merge

--- CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/utilities/LayersAddContextMenu.java
+++ CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/utilities/LayersAddContextMenu.java
@@ -24,13 +24,20 @@ import au.gov.asd.tac.constellation.graph.visual.contextmenu.ContextMenuProvider
 import au.gov.asd.tac.constellation.plugins.PluginExecution;
 import au.gov.asd.tac.constellation.utilities.graphics.Vector3f;
 import au.gov.asd.tac.constellation.views.layers.layer.LayerDescription;
+import au.gov.asd.tac.constellation.views.layers.state.LayersViewConcept;
 import au.gov.asd.tac.constellation.views.layers.state.LayersViewState;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.openide.util.lookup.ServiceProvider;
 
+/**
+ * Add context menu for Layers View
+ *
+ * @author aldebaran30701
+ */
 @ServiceProvider(service = ContextMenuProvider.class, position = 210)
 public class LayersAddContextMenu implements ContextMenuProvider {
 
@@ -45,13 +52,15 @@ public class LayersAddContextMenu implements ContextMenuProvider {
     @Override
     public List<String> getItems(final GraphReadMethods graph, final GraphElementType elementType, final int elementId) {
         if (elementType == GraphElementType.VERTEX || elementType == GraphElementType.TRANSACTION) {
-            final int stateAttributeId = LayersViewState.LayersViewConcept.MetaAttribute.LAYERS_VIEW_STATE.get(graph);
+            final int stateAttributeId = LayersViewConcept.MetaAttribute.LAYERS_VIEW_STATE.get(graph);
             final List<String> currentLayers = new ArrayList<>();
             if (stateAttributeId != GraphConstants.NOT_FOUND) {
                 final LayersViewState currentState = graph.getObjectValue(stateAttributeId, 0);
-                for (final LayerDescription layer : currentState.getAllLayers()) {
+                for (final LayerDescription layer : currentState.getLayers()) {
                     if (layer.getLayerIndex() > 1) {
-                        currentLayers.add(String.valueOf(layer.getLayerIndex()) + " " + layer.getDescription());
+                        final String description = StringUtils.isBlank(layer.getLayerDescription())
+                                ? "[NO DESCRIPTION]" : layer.getLayerDescription();
+                        currentLayers.add(String.valueOf(layer.getLayerIndex()) + " - " + description);
                     }
                 }
             } else {
@@ -65,7 +74,8 @@ public class LayersAddContextMenu implements ContextMenuProvider {
 
     @Override
     public void selectItem(final String item, final Graph graph, final GraphElementType elementType, final int elementId, final Vector3f unprojected) {
-        PluginExecution.withPlugin(new UpdateElementBitmaskPlugin(Integer.parseInt(item), LayerAction.ADD))
+        PluginExecution.withPlugin(new UpdateElementBitmaskPlugin(
+                Integer.parseInt(item.substring(0, 2).trim()), LayerAction.ADD))
                 .executeLater(GraphManager.getDefault().getActiveGraph());
     }
 }
