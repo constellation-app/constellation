921044cb00cb1c08e8897734e0b77afb569045ab
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2020-06-09 11:24:24 +1000

    :art: Update to package name based on feedback

--- ProjectUpdater/src/au/gov/asd/tac/constellation/projectupdater/ProjectUpdater.java
+++ ProjectUpdater/src/au/gov/asd/tac/constellation/projectupdater/ProjectUpdater.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package au.gov.asd.tac.projectupdater;
+package au.gov.asd.tac.constellation.projectupdater;
 
 import java.io.File;
 import java.io.FileInputStream;
