3bd7905205e4a36c8e1c4c6a195ff4c77ba8aea3
sol695510 <58677774+sol695510@users.noreply.github.com>
2020-06-30 10:19:09 +0930

    Created Notes View Package

---
+++ CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/NotesViewPane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.gov.asd.tac.constellation.views.notes;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author sol695510
+ */
+public class NotesViewPane extends BorderPane {
+    
+}
