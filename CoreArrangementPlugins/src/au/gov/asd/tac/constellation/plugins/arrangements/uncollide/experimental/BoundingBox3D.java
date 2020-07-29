90445b5ba68d14f0810a5a2ef5dbc4c775ff33a1
Nova-2119 <liam.banks@defence.gov.au>
2020-07-29 12:22:32 +1000

    Created BoundingBox3D class with tests. Created a 3d version of the bounding box for future use with the 3D version of the experimental uncollide plugin.

---
+++ CoreArrangementPlugins/src/au/gov/asd/tac/constellation/plugins/arrangements/uncollide/experimental/BoundingBox3D.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2010-2020 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.plugins.arrangements.uncollide.experimental;
+
+import au.gov.asd.tac.constellation.graph.GraphElementType;
+import au.gov.asd.tac.constellation.graph.GraphReadMethods;
+import au.gov.asd.tac.constellation.graph.schema.visual.concept.VisualConcept;
+
+/**
+ * This class is designed to provide a 3D bounding box for a graph.
+ * <p>
+ * 
+ * @author algol
+ * @author Nova
+ */
+class BoundingBox3D { 
+    final float minX;
+    final float minY;
+    final float minZ;
+    final float maxX;
+    final float maxY;
+    final float maxZ;
+    final float midX;
+    final float midY;
+    final float midZ;
+    
+    /**
+     * Generate a 3D bounding box for the graph.
+     * <p>
+     * This method creates a bounding box for the verticies of a given graph.
+     * It does this by finding the extremes of the X,Y and Z axis.
+     * These values are then made available as attributes of the class instance.
+     * 
+     * @param wg  the graph
+     * @return  instance of class BoundingBox2D based on input graph
+     */
+    BoundingBox3D(final GraphReadMethods wg) {
+        final int xId = wg.getAttribute(GraphElementType.VERTEX, VisualConcept.VertexAttribute.X.getName());
+        final int yId = wg.getAttribute(GraphElementType.VERTEX, VisualConcept.VertexAttribute.Y.getName());
+        final int zId = wg.getAttribute(GraphElementType.VERTEX, VisualConcept.VertexAttribute.Y.getName());
+
+        float minX = wg.getFloatValue(xId, wg.getVertex(0));
+        float minY = wg.getFloatValue(yId, wg.getVertex(0));
+        float minZ = wg.getFloatValue(zId, wg.getVertex(0));
+        float maxX = minX;
+        float maxY = minY;
+        float maxZ = minZ;
+
+
+        final int vxCount = wg.getVertexCount();
+        if(vxCount == 0) {
+            throw new IllegalArgumentException("Graph must contain atleast one vertex to find BoundingBox");
+        }
+              
+        for (int position = 1; position < vxCount; position++) {
+            final int vxId = wg.getVertex(position);
+
+            final float x = wg.getFloatValue(xId, vxId);
+            final float y = wg.getFloatValue(yId, vxId);
+            final float z = wg.getFloatValue(zId, vxId);
+            
+            if (x < minX) {
+                minX = x;
+            }
+            if (x > maxX) {
+                maxX = x;
+            }
+            if (y < minY) {
+                minY = y;
+            }
+            if (y > maxY) {
+                maxY = y;
+            }
+            if (z < minZ) {
+                minZ = z;
+            }
+            if (z > maxZ) {
+                maxZ = z;
+            }
+        }
+
+        this.minX = minX;
+        this.minY = minY;
+        this.minZ = minZ;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.maxZ = maxZ;
+        this.midX = minX + (maxX - minX)*(float)0.5;
+        this.midY = minY + (maxY - minY)*(float)0.5;
+        this.midZ = minZ + (maxZ - minZ)*(float)0.5;
+    }
+
+    private BoundingBox3D(final float minX, final float maxX, final float minY, final float maxY, final float minZ, final float maxZ) {
+        this.minX = minX;
+        this.minY = minY;
+        this.minZ = minZ;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.maxZ = maxZ;
+        this.midX = minX + (maxX - minX)*(float)0.5;
+        this.midY = minY + (maxY - minY)*(float)0.5;
+        this.midZ = minZ + (maxZ - minZ)*(float)0.5;
+    }
+
+    /**
+     * Gets BoundingBox3D of the top left front octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D topLeftFrontOctant() {
+        return new BoundingBox3D(minX, midX, midY, maxY, midZ, maxZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the top right front octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D topRightFrontOctant() {
+        return new BoundingBox3D(midX, maxX, midY, maxY, midZ, maxZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the bottom left front octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D bottomLeftFrontOctant() {
+        return new BoundingBox3D(minX, midX, minY, midY, midZ, maxZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the bottom right front octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D bottomRightFrontOctant() {
+        return new BoundingBox3D(midX, maxX, minY, midY, midZ, maxZ);
+    }  
+
+    /**
+     * Gets BoundingBox3D of the top left back octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D topLeftBackOctant() {
+        return new BoundingBox3D(minX, midX, midY, maxY, minZ, midZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the top right back octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D topRightBackOctant() {
+        return new BoundingBox3D(midX, maxX, midY, maxY, minZ, midZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the bottom left back octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D bottomLeftBackOctant() {
+        return new BoundingBox3D(minX, midX, minY, midY, minZ, midZ);
+    }
+    
+    /**
+     * Gets BoundingBox3D of the bottom right back octant of this BoundingBox3D
+     * 
+     * @return  BoundingBox3D
+     */
+    BoundingBox3D bottomRightBackOctant() {
+        return new BoundingBox3D(midX, maxX, minY, midY, minZ, midZ);
+    }  
+    @Override
+    public String toString() {
+        return String.format("Box: Min(%f, %f, %f), Max(%f, %f, %f)", minX, minY, minZ, maxX, maxY, maxZ);
+    }
+}
