fe2a61bec32ba3c821ec5718a55fb0166e17c358
sol695510 <58677774+sol695510@users.noreply.github.com>
2020-08-21 15:55:06 +0930

    Fix Code Smells

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/parser/PCAPImportFileParser.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/parser/PCAPImportFileParser.java
@@ -22,7 +22,6 @@ import io.pkts.buffer.Buffer;
 import io.pkts.packet.Packet;
 import io.pkts.packet.IPPacket;
 import io.pkts.packet.TCPPacket;
-import io.pkts.packet.TransportPacket;
 import io.pkts.packet.UDPPacket;
 import io.pkts.protocol.Protocol;
 import java.io.IOException;
@@ -32,9 +31,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.openide.util.Exceptions;
 import org.openide.util.lookup.ServiceProvider;
-
 
 /**
  *
@@ -44,7 +41,7 @@ import org.openide.util.lookup.ServiceProvider;
 public class PCAPImportFileParser extends ImportFileParser {
 
     private static final Logger LOGGER = Logger.getLogger(PCAPImportFileParser.class.getName());    
-    private ArrayList<String[]> allResults = new ArrayList<>();
+    final private List<String[]> allResults = new ArrayList<>();
     private int frameCounter = 1;
  
     /**
@@ -57,7 +54,6 @@ public class PCAPImportFileParser extends ImportFileParser {
     private List<String[]> getResults(final InputSource input, final int limit) throws IOException {
         try {
             final String[] headings = {"Frame", "Time", "Source", "Source Port", "Source Type", "Destination", "Destination Port", "Destination Type", "Protocol", "Length"};
-            final ArrayList<String[]> results = new ArrayList<>();
             
             // TODO: use classwide allResults to give visibility into nextPacket, is there another way
             allResults.clear();
@@ -69,13 +65,13 @@ public class PCAPImportFileParser extends ImportFileParser {
             
             pcap.loop(new PacketHandler() {
                 @Override
-                public boolean nextPacket(Packet packet) throws IOException {
+                public boolean nextPacket(final Packet packet) throws IOException {
                     
                     IPPacket ipPacket = null;
                     
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+                    final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
                     Date timestamp = new Date(packet.getArrivalTime() / 1000);
-                    String frame = Integer.toString(frameCounter);
+                    final String frame = Integer.toString(frameCounter);
                     String sourceIP = "";
                     String sourcePort = "";
                     String sourceType = "";
@@ -88,19 +84,18 @@ public class PCAPImportFileParser extends ImportFileParser {
                     String length = Integer.toString(buffer.capacity());
                          
                     try {               
-                        
                         if (packet.hasProtocol(Protocol.TCP)) {
                             protocol = "TCP";
                             TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
                             sourcePort = Integer.toString(tcpPacket.getSourcePort());
                             destinationPort = Integer.toString(tcpPacket.getDestinationPort());
-                            ipPacket = (IPPacket) tcpPacket.getParentPacket();
+                            ipPacket = tcpPacket.getParentPacket();
                         } else if (packet.hasProtocol(Protocol.UDP)) {
                             protocol = "UDP";
                             UDPPacket udpPacket = (UDPPacket) packet.getPacket(Protocol.UDP);
                             sourcePort = Integer.toString(udpPacket.getSourcePort());
                             destinationPort = Integer.toString(udpPacket.getDestinationPort());
-                            ipPacket = (IPPacket) udpPacket.getParentPacket();
+                            ipPacket = udpPacket.getParentPacket();
                         }
                         
                         if (ipPacket != null) {
@@ -111,13 +106,18 @@ public class PCAPImportFileParser extends ImportFileParser {
                             destinationType = ipVersion + " Address";
                         }
                         
-                        LOGGER.log(Level.INFO, "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}", new Object[]{frame, formatter.format(timestamp), sourceIP, sourcePort, sourceType, destinationIP, destinationPort, destinationType, protocol, length });
-                        final String[] row = {frame, formatter.format(timestamp), sourceIP, sourcePort, sourceType, destinationIP, destinationPort, destinationType, protocol, length};
+                        LOGGER.log(Level.INFO, "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}",
+                            new Object[]{frame, formatter.format(timestamp),
+                            sourceIP, sourcePort, sourceType, destinationIP, destinationPort, destinationType, protocol, length});
+                        
+                        final String[] row = {frame, formatter.format(timestamp),
+                            sourceIP, sourcePort, sourceType, destinationIP, destinationPort, destinationType, protocol, length};
+                        
                         allResults.add(row);
-                    } catch (AssertionError e) {
+                        
+                    } catch (final AssertionError e) {
                         LOGGER.log(Level.INFO, "ASSERTION THROWN 1 {0}", e.toString());
-                    }
-                    catch (Exception e) {
+                    } catch (final Exception e) {
                         LOGGER.log(Level.INFO, "EXCEPTION THROWN 1 {0}", e.toString());
                     }
                     
@@ -127,19 +127,17 @@ public class PCAPImportFileParser extends ImportFileParser {
             });
             
             return allResults;
-        } catch (AssertionError e) {
-                        LOGGER.log(Level.INFO, "ASSERTION THROWN 2 {0}", e.toString());
-          return allResults;
-        } catch (Exception ex) {
+            
+        } catch (final AssertionError e) {
+            LOGGER.log(Level.INFO, "ASSERTION THROWN 2 {0}", e.toString());
+            return allResults;
+        } catch (final Exception ex) {
             // Unexpected exceptions
             LOGGER.log(Level.INFO, "EXCEPTION THROWN 2");
-           // Exceptions.printStackTrace(ex);
-          //  throw (ex);
-          return allResults;
+            return allResults;
         }
     }
 
-    
     /**
      * Reads the entire file and returns a List of String arrays, each of which
      * represents a row in the resulting table.
