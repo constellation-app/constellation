93a7e6b01659b2033b1eb6b20489a2c021ddaf84
formalhaut69 <58677762+formalhaut69@users.noreply.github.com>
2020-08-26 07:04:26 +0930

    Feature/layers view keyboard shortcuts (#782)
    * New Layer shortcut
    
    * Added the Deselect all layers plugin
    
    * Updated with a switch statement
    
    * added final modifiers
    
    * Added test cases for new plugins
    
    * Requested changes
    
    * Added update to whats new
    
    * Upadated Whats new and resolved merge conflict
    
    * Changed keyboard shortcut to Ctrl-Alt-D

---
+++ CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/LayersViewShortcuts.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010-2020 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.views.layers;
+
+import au.gov.asd.tac.constellation.graph.manager.GraphManager;
+import au.gov.asd.tac.constellation.plugins.PluginExecution;
+import au.gov.asd.tac.constellation.views.layers.utilities.DeselectAllLayersPlugin;
+import au.gov.asd.tac.constellation.views.layers.utilities.NewLayerPlugin;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
+
+/**
+ * Defines the keyboard shortcuts for use with the Layers view
+ *
+ * @author formalhaut69
+ */
+@ActionID(category = "Options", id = "au.gov.asd.tac.constellation.views.layersviewshortcuts")
+@ActionRegistration(displayName = "#CTL_LayersViewShortcuts", surviveFocusChange = true)
+@ActionReferences({
+    @ActionReference(path = "Shortcuts", name = "CA-S"),
+    @ActionReference(path = "Shortcuts", name = "CA-L")
+})
+@NbBundle.Messages("CTL_LayersViewShortcuts=Layers View: Shortcuts")
+public class LayersViewShortcuts extends AbstractAction{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        final String hotkey = e.getActionCommand();
+        switch (hotkey) {
+            case "CA-L":
+                PluginExecution.withPlugin(new NewLayerPlugin()).executeLater(GraphManager.getDefault().getActiveGraph());
+                break;
+            case "CA-D":
+                PluginExecution.withPlugin(new DeselectAllLayersPlugin()).executeLater(GraphManager.getDefault().getActiveGraph());
+                break;
+            default:
+                break;
+        }
+    }
+}
