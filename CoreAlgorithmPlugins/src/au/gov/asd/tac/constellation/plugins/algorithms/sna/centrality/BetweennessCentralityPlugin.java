41a0af3e76dbf30b81cdaf747c4e57e47dc5b3db
Nova-2119 <liam.banks@defence.gov.au>
2020-09-01 15:41:51 +0930

    :art: :bug: Added Nan check Added a NaN check to account for the scenario where we attempt to divide 0 by a number (resulting a Nan) which then causes an error later in the code. Refactored multiple calls graph.setFloatValue with one argument difference based on if conditions to one call with the argument only set by if conditions. argument set

--- CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/sna/centrality/BetweennessCentralityPlugin.java
+++ CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/sna/centrality/BetweennessCentralityPlugin.java
@@ -153,23 +153,31 @@ public class BetweennessCentralityPlugin extends SimpleEditPlugin {
         // update the graph with betweenness values
         for (int vertexPosition = 0; vertexPosition < vertexCount; vertexPosition++) {
             final int vertexId = graph.getVertex(vertexPosition);
+            float betweennessAttributeValue;
             if (normaliseByPossible) {
                 if (normaliseConnectedComponents) {
                     final float subgraphVertexCount = subgraphs[vertexPosition].cardinality();
+                    betweennessAttributeValue = betweennesses[vertexPosition] / (((subgraphVertexCount - 1) * (subgraphVertexCount - 2)) / 2);
                     graph.setFloatValue(betweennessAttribute, vertexId, betweennesses[vertexPosition] / (((subgraphVertexCount - 1) * (subgraphVertexCount - 2)) / 2));
                 } else {
+                    betweennessAttributeValue = betweennesses[vertexPosition] / (((vertexCount - 1) * (vertexCount - 2)) / 2f);
                     graph.setFloatValue(betweennessAttribute, vertexId, betweennesses[vertexPosition] / (((vertexCount - 1) * (vertexCount - 2)) / 2f));
                 }
             } else if (normaliseByAvailable && maxBetweenness > 0) {
                 if (normaliseConnectedComponents) {
                     final float maxBetweennessConnectedComponent = maxBetweennessConnectedComponents.get(subgraphs[vertexPosition]);
+                    betweennessAttributeValue = betweennesses[vertexPosition] / maxBetweennessConnectedComponent;
                     graph.setFloatValue(betweennessAttribute, vertexId, betweennesses[vertexPosition] / maxBetweennessConnectedComponent);
                 } else {
+                    betweennessAttributeValue = betweennesses[vertexPosition] / maxBetweenness;
                     graph.setFloatValue(betweennessAttribute, vertexId, betweennesses[vertexPosition] / maxBetweenness);
                 }
             } else {
+                betweennessAttributeValue = betweennesses[vertexPosition];
                 graph.setFloatValue(betweennessAttribute, vertexId, betweennesses[vertexPosition]);
             }
+            
+            graph.setFloatValue(betweennessAttribute, vertexId, Float.isNaN(betweennessAttributeValue) ? 0 : betweennessAttributeValue);
         }
     }
 }
