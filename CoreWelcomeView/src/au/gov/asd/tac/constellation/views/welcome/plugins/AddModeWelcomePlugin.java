d92c760699c48aa52e27800555b96c41252bedeb
Delphinus8821 <73083035+Delphinus8821@users.noreply.github.com>
2021-02-04 08:40:09 +1030

    Welcome Tab Updates (#944)
    * Update spacing of page when constellation opens
    
    * New graph in selection mode added
    
    * JDBC Imported added to welcome page
    
    * Only the welcome page opens on startup
    
    * Convert tutorial page to what's new page
    
    * Tutorial pane no longer opens on startup
    
    * Variable name and colour updates
    
    * Refactor tutorial to whats new page
    
    Core Whats New View module has been created. The tutorial page files have been converted to whats new.
    
    * Made requested changes
    
    Also updated the copyright on the files for the welcome tab and whats new view.
    
    * Update to how plugins are added to the welcome page
    
    * Updates to UI
    
    * Remove unused Imports
    
    * Requested changes
    
    * Service Providers have been updated to improve flexibility
    
    * Update to variables
    
    * Updates to requested changes
    
    * welcome tab pr update (#961)
    
    * removed the need for a few variables and functions
    
    * minor change
    
    * minor update
    
    * minor update
    
    Co-authored-by: antares1470 <62311243+antares1470@users.noreply.github.com>

--- CoreWelcomeView/src/au/gov/asd/tac/constellation/views/welcome/plugins/AddModeWelcomePlugin.java
+++ CoreWelcomeView/src/au/gov/asd/tac/constellation/views/welcome/plugins/AddModeWelcomePlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 Australian Signals Directorate
+ * Copyright 2010-2021 Australian Signals Directorate
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 package au.gov.asd.tac.constellation.views.welcome.plugins;
 
-import au.gov.asd.tac.constellation.views.welcome.WelcomePageProvider;
+import au.gov.asd.tac.constellation.views.welcome.WelcomePluginInterface;
 import au.gov.asd.tac.constellation.views.welcome.WelcomeTopComponent;
 import au.gov.asd.tac.constellation.graph.Graph;
 import au.gov.asd.tac.constellation.graph.StoreGraph;
@@ -34,7 +34,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import org.openide.util.NbBundle;
-import org.openide.util.lookup.ServiceProvider;
 
 /**
  * The New Graph in Add Mode plugin for the Welcome Page.
@@ -42,10 +41,9 @@ import org.openide.util.lookup.ServiceProvider;
  * @author canis_majoris
  */
 
-@ServiceProvider(service = WelcomePageProvider.class, position = 1)
 @PluginInfo(tags = {"WELCOME"})
 @NbBundle.Messages("AddModeWelcomePlugin=Add Mode Welcome Plugin")
-public class AddModeWelcomePlugin extends WelcomePageProvider {
+public class AddModeWelcomePlugin implements WelcomePluginInterface {
     
     public static final String NEW_GRAPH = "resources/welcome_add_graph.png";
     final ImageView addView = new ImageView(new Image(WelcomeTopComponent.class.getResourceAsStream(NEW_GRAPH)));
