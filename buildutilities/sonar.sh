68b043d83f5170df268b16083a4c0c6ecc6bd4f5
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-12 09:35:11 +1030

    Update sonar.sh

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -5,7 +5,7 @@ source buildutilities/functions.sh
 title "Run Sonar Scanning"
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  if [ "${TRAVIS_PULL_REQUEST_SLUG}" != "constellation-app/constellation" ]; then
+  if [ "${TRAVIS_PULL_REQUEST_SLUG}" != "aldebaran30701/constellation" ]; then
     echo "skipping running sonar-scanner"
   else
     SONAR_PULLREQUEST_BRANCH="$(echo "${TRAVIS_PULL_REQUEST_SLUG}" | awk '{split($0,a,"/"); print a[1]}')/${TRAVIS_PULL_REQUEST_BRANCH}"
