f93f8c45259947671426b35216886215c655219a
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-11 14:48:53 +1030

    test fail

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/query/QueryEvaluatorNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/query/QueryEvaluatorNGTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010-2020 Australian Signals Directorate
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -63,6 +63,7 @@ public class QueryEvaluatorNGTest {
         List<String> returning = QueryEvaluator.tokeniser(infix);
         assertEquals(returning.get(0), result1);
         assertEquals(returning.size(), results.size());
+        assertEquals(0, 1);
     }
 
     @Test
