9b89aa6408a2834a9e4aab818a7a750deb422dff
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-12 11:31:37 +1030

    Update sonar.sh

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -6,7 +6,7 @@ title "Run Sonar Scanning"
 
 if [ github.event.number != "false" ]; then
   if [ "${TRAVIS_PULL_REQUEST_SLUG}" != "aldebaran30701/constellation" ]; then
-    echo ${{github.event.pull_request.head.repo.full_name}}
+    echo $((github.event.pull_request.head.repo.full_name))
     echo "skipping running sonar-scanner"
   else
     SONAR_PULLREQUEST_BRANCH="$(echo "${TRAVIS_PULL_REQUEST_SLUG}" | awk '{split($0,a,"/"); print a[1]}')/${TRAVIS_PULL_REQUEST_BRANCH}"
