9b4d65e2ba371014c03d96a0e9fad90ae129e63d
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-12 15:48:14 +1030

    made sonar check

--- buildutilities/sonar.sh
+++ buildutilities/sonar.sh
@@ -9,10 +9,6 @@ if [ ! -z $2 ]; then
     echo "skipping running sonar-scanner"
   else
     echo "This is a Pull Request"
-    echo $1
-    echo $2
-    echo $3
-    echo $4
     SONAR_PULLREQUEST_BRANCH="$(echo $1 | awk '{split($0,a,"/"); print a[1]}')/$4"
     sonar-scanner \
       -Dsonar.login="${SONAR_TOKEN}" \
