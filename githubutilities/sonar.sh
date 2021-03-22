f3a83b1f50620bfdd702a4cbbf73aa1c7ba6dd1f
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-03-22 15:11:48 +1030

    relocate download sonar scanner code to dockerfile

--- githubutilities/sonar.sh
+++ githubutilities/sonar.sh
@@ -2,7 +2,7 @@
 
 source githubutilities/functions.sh
 
-echo "Updating dependencies and building."
+title "Updating dependencies and building"
 
 ant \
   -Dnbplatform.active.dir="${NETBEANS_HOME}" \
@@ -11,33 +11,10 @@ ant \
   -Dupdate.dependencies=true \
   -Dbuild.compiler.debug=true update-dependencies-clean-build
 
-echo "Finished Updating dependencies and building."
-
 title "Run Sonar Scanning"
 
-cd /tmp || exit
-echo "Downloading sonar-scanner....."
-if [ -d "/tmp/sonar-scanner-cli-3.2.0.1227-linux.zip" ];then
-    sudo rm /tmp/sonar-scanner-cli-3.2.0.1227-linux.zip
-fi
-wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
-echo "Download completed."
-
-echo "Unziping downloaded file..."
-unzip sonar-scanner-cli-4.0.0.1744-linux.zip
-echo "Unzip completed."
-rm sonar-scanner-cli-4.0.0.1744-linux.zip
-
-echo "Installing to opt..."
-if [ -d "/var/opt/sonar-scanner-4.0.0.1744-linux" ];then
-    rm -rf /var/opt/sonar-scanner-4.0.0.1744-linux
-fi
-mv sonar-scanner-4.0.0.1744-linux /var/opt/sonar-scanner
-export PATH="$PATH:/var/opt/sonar-scanner/bin"
-echo "Installation completed successfully."
-
 if [ ! -z $2 ]; then
-  if [ $1 != "constellation-app/constellation" ]; then
+  if [ $1 != "aldebaran30701/constellation" ]; then
     echo "skipping running sonar-scanner"
   else
     SONAR_PULLREQUEST_BRANCH="$(echo $1 | awk '{split($0,a,"/"); print a[1]}')/$4"
