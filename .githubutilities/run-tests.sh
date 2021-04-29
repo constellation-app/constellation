2a3e51c5d02e71f38a70365db8c61b7c14a63f95
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-04-29 11:07:19 +0930

    remove commented out code

--- .githubutilities/run-tests.sh
+++ .githubutilities/run-tests.sh
@@ -29,4 +29,3 @@ while IFS= read -r -d '' file; do
   xml_output="${file%.exec}.xml"
   java -jar "${JACOCO_HOME}/lib/jacococli.jar" report "${file}" --classfiles "${classfile}" --xml "${xml_output}"
 done < <(find . -iname "*jacoco.exec" -print0)
-
