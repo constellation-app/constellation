5b70874a12b68324f4a5e3f250eb2f1951ad471d
Auriga2 <auriga2345@gmail.com>
2021-05-12 13:11:50 +0930

    Fix the copyright header and replace JFXPanel()

--- CoreQualityControlView/test/unit/src/au/gov/asd/tac/constellation/views/qualitycontrol/widget/DefaultQualityControlAutoButtonNGTest.java
+++ CoreQualityControlView/test/unit/src/au/gov/asd/tac/constellation/views/qualitycontrol/widget/DefaultQualityControlAutoButtonNGTest.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2010-2021 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package au.gov.asd.tac.constellation.views.qualitycontrol.widget;
 
@@ -15,7 +25,7 @@ import au.gov.asd.tac.constellation.views.qualitycontrol.daemon.QualityControlSt
 import au.gov.asd.tac.constellation.views.qualitycontrol.rules.QualityControlRule;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.embed.swing.JFXPanel;
+import javafx.application.Application;
 import javafx.scene.control.Tooltip;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
@@ -72,7 +82,7 @@ public class DefaultQualityControlAutoButtonNGTest {
     public void testQualityControlChangedWithNullState() throws InterruptedException {
         System.out.println("qualityControlChanged");
         QualityControlState state = null;
-        new JFXPanel();
+        Application.launch("");
         DefaultQualityControlAutoButton instance = new DefaultQualityControlAutoButton();
 
         String expRiskText = String.format(instance.QUALITY_CONTROL_WIDGET_TEXT, Bundle.MSG_NoRisk());
@@ -98,7 +108,7 @@ public class DefaultQualityControlAutoButtonNGTest {
     public void testQualityControlChangedWithValidState() throws InterruptedException {
         System.out.println("qualityControlChanged");
         QualityControlState state = new QualityControlState(graph.getId(), events, rules);
-        new JFXPanel();
+        Application.launch("");
         DefaultQualityControlAutoButton instance = new DefaultQualityControlAutoButton();
 
         final String expRiskText = "Quality Category: FATAL";
