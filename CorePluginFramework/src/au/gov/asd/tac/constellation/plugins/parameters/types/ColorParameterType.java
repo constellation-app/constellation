5753e842882d022b77c656f2f4254e0fda398a37
aldebaran30701 <Aldebaran30701@gmail.com>
2021-05-18 10:35:00 +0930

    instance of comparison 2

--- CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/ColorParameterType.java
+++ CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/ColorParameterType.java
@@ -22,8 +22,7 @@ import au.gov.asd.tac.constellation.utilities.color.ConstellationColor;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
- * The ColorParameterType defines {@link PluginParameter} objects that hold
- * {@link ConstellationColor} values.
+ * The ColorParameterType defines {@link PluginParameter} objects that hold {@link ConstellationColor} values.
  *
  * @author sirius
  */
@@ -38,19 +37,16 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
     /**
      * Constructs a new instance of this type.
      * <p>
-     * Note: This constructor should not be called directly; it is public for
-     * the purposes of lookup (which may be removed for types in the future). To
-     * buildId parameters from the type, the static method
-     * {@link #build buildId()} should be used, or the singleton
-     * {@link #INSTANCE INSTANCE}.
+     * Note: This constructor should not be called directly; it is public for the purposes of lookup (which may be
+     * removed for types in the future). To buildId parameters from the type, the static method {@link #build buildId()}
+     * should be used, or the singleton {@link #INSTANCE INSTANCE}.
      */
     public ColorParameterType() {
         super(ID);
     }
 
     /**
-     * The singleton instance of the type that should be used to construct all
-     * parameters that have this type.
+     * The singleton instance of the type that should be used to construct all parameters that have this type.
      */
     public static final ColorParameterType INSTANCE = new ColorParameterType();
 
@@ -65,12 +61,11 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
     }
 
     /**
-     * Construct a new {@link PluginParameter} of this type with initial value
-     * represented by the given {@link ColorParameterValue}.
+     * Construct a new {@link PluginParameter} of this type with initial value represented by the given
+     * {@link ColorParameterValue}.
      *
      * @param id The String id of the parameter to construct.
-     * @param pv A {@link ColorParameterValue} describing the initial value of
-     * the parameter being constructed.
+     * @param pv A {@link ColorParameterValue} describing the initial value of the parameter being constructed.
      * @return A {@link PluginParameter} of ColorParameterType.
      */
     public static PluginParameter<ColorParameterValue> build(String id, final ColorParameterValue pv) {
@@ -78,8 +73,8 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
     }
 
     /**
-     * An implementation of {@link ParameterValue} corresponding to this type.
-     * It holds {@link ConstellationColor} values.
+     * An implementation of {@link ParameterValue} corresponding to this type. It holds {@link ConstellationColor}
+     * values.
      */
     public static class ColorParameterValue extends ParameterValue {
 
@@ -93,11 +88,9 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
         }
 
         /**
-         * Constructs a new ColorParameterValue holding the specified
-         * {@link ConstellationColor}.
+         * Constructs a new ColorParameterValue holding the specified {@link ConstellationColor}.
          *
-         * @param c The {@link ConstellationColor} that this parameter value
-         * should hold.
+         * @param c The {@link ConstellationColor} that this parameter value should hold.
          */
         public ColorParameterValue(final ConstellationColor c) {
             this.c = c;
@@ -106,8 +99,7 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
         /**
          * Get the current value from this parameter value.
          *
-         * @return The {@link ConstellationColor} that this parameter value is
-         * holding.
+         * @return The {@link ConstellationColor} that this parameter value is holding.
          */
         public ConstellationColor get() {
             return c;
@@ -116,10 +108,8 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
         /**
          * Set the current value
          *
-         * @param newc The {@link ConstellationColor} for this parameter value
-         * to hold.
-         * @return True if the new value was different to the current value,
-         * false otherwise.
+         * @param newc The {@link ConstellationColor} for this parameter value to hold.
+         * @return True if the new value was different to the current value, false otherwise.
          */
         public boolean set(final ConstellationColor newc) {
             if (newc != c) {
@@ -178,7 +168,7 @@ public class ColorParameterType extends PluginParameterType<ColorParameterValue>
 
         @Override
         public boolean equals(final Object o) {
-            return o instanceof ColorParameterValue && c.equals(((ColorParameterValue) o).c);
+            return this.getClass() == o.getClass() && c.equals(((ColorParameterValue) o).c);
         }
 
         @Override
--- CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/DateTimeRange.java
+++ CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/DateTimeRange.java
@@ -27,8 +27,7 @@ import java.util.Date;
 /**
  * A date time range, with the start and end times truncated to the second.
  * <p>
- * This can be an absolute time range with fixed values for start and end, or a
- * period relative to "now".
+ * This can be an absolute time range with fixed values for start and end, or a period relative to "now".
  *
  * @author algol
  */
@@ -69,8 +68,8 @@ public class DateTimeRange {
     /**
      * A relative date range.
      * <p>
-     * When the start and end dates are retrieved, the end date will be "now",
-     * and the start date will be the period before the end date.
+     * When the start and end dates are retrieved, the end date will be "now", and the start date will be the period
+     * before the end date.
      *
      * @param period the period that represents the time range.
      * @param zi the id of the time zone.
@@ -95,11 +94,9 @@ public class DateTimeRange {
     /**
      * The relative period of this instance.
      * <p>
-     * If the period is null, this instance is absolute; use getZonedStartEnd
-     * instead.
+     * If the period is null, this instance is absolute; use getZonedStartEnd instead.
      *
-     * @return The relative Period of this instance, or null if the range is
-     * absolute.
+     * @return The relative Period of this instance, or null if the range is absolute.
      */
     public Period getPeriod() {
         return period;
@@ -112,8 +109,7 @@ public class DateTimeRange {
     /**
      * If the range is absolute, start and end are returned as-is.
      * <p>
-     * If the range is relative, end is "now" and start is the relative datetime
-     * prior to start.
+     * If the range is relative, end is "now" and start is the relative datetime prior to start.
      *
      * @return A ZonedDateTime[2] containing the start,end of the range.
      */
@@ -135,7 +131,7 @@ public class DateTimeRange {
     public boolean equals(final Object other) {
         if (this == other) {
             return true;
-        } else if (!(other instanceof DateTimeRange)) {
+        } else if (this.getClass() != obj.getClass()) {
             return false;
         } else {
             final DateTimeRange otherRange = (DateTimeRange) other;
@@ -162,8 +158,7 @@ public class DateTimeRange {
     /**
      * Parse the output of toString().
      * <p>
-     * If a DateTimeParseException is thrown, a default DateTimeRange value will
-     * be returned.
+     * If a DateTimeParseException is thrown, a default DateTimeRange value will be returned.
      *
      * @param s The string to be parsed.
      *
@@ -214,8 +209,8 @@ public class DateTimeRange {
     }
 
     /**
-     * Absolute ranges return two ISO8601 dates, relative ranges return an
-     * ISO8601 duration; both of these have a timezone appended.
+     * Absolute ranges return two ISO8601 dates, relative ranges return an ISO8601 duration; both of these have a
+     * timezone appended.
      *
      * @return A representation of the range.
      */
