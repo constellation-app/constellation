bb1444e0e3b8bcccc57ab3218ea51a02dc3dedbc
antares1470 <62311243+antares1470@users.noreply.github.com>
2021-05-21 15:00:41 +1000

    re-added test

--- CoreGraphNode/test/unit/src/au/gov/asd/tac/constellation/graph/node/plugins/DefaultPluginInteractionNGTest.java
+++ CoreGraphNode/test/unit/src/au/gov/asd/tac/constellation/graph/node/plugins/DefaultPluginInteractionNGTest.java
@@ -27,8 +27,10 @@ import au.gov.asd.tac.constellation.plugins.parameters.PluginParameters;
 import au.gov.asd.tac.constellation.plugins.reporting.GraphReport;
 import au.gov.asd.tac.constellation.plugins.reporting.PluginReport;
 import au.gov.asd.tac.constellation.plugins.templates.SimplePlugin;
+import java.lang.Thread.State;
 import org.openide.windows.TopComponent;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -102,7 +104,7 @@ public class DefaultPluginInteractionNGTest {
      *
      * @throws InterruptedException
      */
-    /*@Test//(expectedExceptions = InterruptedException.class)
+    @Test
     public void testSetProgress() throws InterruptedException {
         System.out.println("setProgress");
 
@@ -132,7 +134,7 @@ public class DefaultPluginInteractionNGTest {
 
         assertEquals(interaction.getTimer().getState(), State.TERMINATED);
         assertEquals(interaction.getProgress(), null);
-    }*/
+    }
 
     /**
      * Test of setProgress method, of class DefaultPluginInteraction with total
