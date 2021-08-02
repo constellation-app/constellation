42c64f3a8351d893c18fa2ce05f7e4bc5d9fb8bf
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2021-08-02 18:44:31 +1000

    :white_check_mark: Add basic testing for the abstract FilterPlugin class

---
+++ CoreDataAccessView/test/unit/src/au/gov/asd/tac/constellation/views/dataaccess/templates/FilterPluginNGTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2010-2021 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.views.dataaccess.templates;
+
+import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.plugins.PluginException;
+import au.gov.asd.tac.constellation.plugins.PluginInteraction;
+import au.gov.asd.tac.constellation.plugins.parameters.PluginParameters;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Filter Plugin Test
+ *
+ * @author arcturus
+ */
+public class FilterPluginNGTest {
+
+    public FilterPluginNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of createParameters method, of class FilterPlugin.
+     */
+    @Test
+    public void testCreateParameters() {
+        System.out.println("createParameters");
+        final FilterPlugin instance = new FilterPluginImpl();
+        final PluginParameters result = instance.createParameters();
+        assertTrue(result.getParameters().containsKey(FilterPluginImpl.FILTER_TYPE_PARAMETER_ID));
+    }
+
+    public class FilterPluginImpl extends FilterPlugin {
+
+        @Override
+        protected void edit(GraphWriteMethods graph, PluginInteraction interaction, PluginParameters parameters) throws InterruptedException, PluginException {
+            // intentionally left blank
+        }
+    }
+
+}
