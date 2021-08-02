8d92a6a965ddf214db64897940cbc18017169920
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2021-08-02 17:13:17 +1000

    Slowly adding the index pages

--- build_help.sh
+++ build_help.sh
@@ -1,7 +1,26 @@
-find */src/ -name \*.md | xargs rm
-find */src/ -name *.html | while read x; do ~/applications/pandoc-2.13/bin/pandoc -f html -t commonmark $x > "$x".md; done
-find */src/ -name *.html.md | while read x; do mv "$x" "${x%.html.md}".md; done
+#!/bin/bash
+
+# remove any existing markdown files
+find */src/ -name \*.md | 
+    xargs rm
+
+# convert html to markdown
+find */src/ -name *.html | 
+    while read x
+    do 
+        ~/applications/pandoc-2.13/bin/pandoc -f html -t commonmark $x > "$x".md
+    done
+
+find */src/ -name *.html.md | 
+    while read x
+    do
+        mv "$x" "${x%.html.md}".md
+    done
+
+# remove the old html files
 #find */src/ -name *.html | while read x; do rm "$x"; done
+
+# add files to git
 git add *.html
 git add *.png
 git add \*.md
