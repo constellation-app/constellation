cb1e60b4e9f806714048fd37536de38b39e6f818
Auriga2 <64826928+Auriga2@users.noreply.github.com>
2021-09-06 17:22:50 +0930

    Import from File and Import from Database - Reset graph view after importing (#1357)
    * Reset graph view after importing
    
    * whatnew update
    
    * Update date

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/ImportDelimitedPlugin.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/delimited/ImportDelimitedPlugin.java
@@ -19,6 +19,7 @@ import au.gov.asd.tac.constellation.graph.Attribute;
 import au.gov.asd.tac.constellation.graph.Graph;
 import au.gov.asd.tac.constellation.graph.GraphElementType;
 import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.graph.interaction.InteractiveGraphPluginRegistry;
 import au.gov.asd.tac.constellation.graph.processing.GraphRecordStoreUtilities;
 import au.gov.asd.tac.constellation.graph.schema.visual.concept.VisualConcept;
 import au.gov.asd.tac.constellation.plugins.Plugin;
@@ -253,6 +254,7 @@ public class ImportDelimitedPlugin extends SimpleEditPlugin {
             PluginExecutor.startWith(ArrangementPluginRegistry.GRID_COMPOSITE)
                     .followedBy(ArrangementPluginRegistry.PENDANTS)
                     .followedBy(ArrangementPluginRegistry.UNCOLLIDE)
+                    .followedBy(InteractiveGraphPluginRegistry.RESET_VIEW)
                     .executeNow(vlGraph.getInclusionGraph());
             vlGraph.retrieveCoords();
         }
