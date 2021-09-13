c8968594d2818a921fe4401d468a6083afb71dbe
Delphinus8821 <73083035+Delphinus8821@users.noreply.github.com>
2021-09-13 15:24:51 +0930

    Update Pandoc template and move help script

--- move_help.sh
+++ move_help.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 # convert markdown to html & add headers 
-find */src/ -path '*/docs/*'-name "*.md" | 
+find */src/ -path '*/docs/*' -name "*.md" | 
     while read x
     do 
-        ~/AppData/Local/Pandoc/pandoc --include-in-header="onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html 
+   	 pandoc --template="onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html
     done
 
+# we do need to rename them to .html instead of .md.html for the next line to work 
 
 # move all html files to the docs folder 
-find */src/ -path '*/docs/*' -name "*.md.html" | xargs cp --parents -t ./documentation
+find */src/ -path '*/docs/*' -name "*html" | xargs cp --parents -t ./documentation
 
 # move all of the png & jpegs to the docs folder structure 
 find */src/ -path '*/docs/*' -name "*.png" | xargs cp --parents -t ./documentation
