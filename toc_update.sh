6a03727dcaa6a2c22e7a561fb0c648f4b9a87acf
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-15 14:18:49 +0930

    Update toc_update.sh

--- toc_update.sh
+++ toc_update.sh
@@ -4,15 +4,10 @@
 echo "find toc.md and convert with pandoc"
 pandoc -f commonmark -t html ./constellation/toc.md > "toc".html
 
-echo "rename toc.md.html to toc.html"
-mv "./constellation/toc.md.html" "./constellation/toc.html"
-
-
 echo "search and replace start"
-
 while read a; do
     echo ${a//md/html}
 done < ./constellation/toc.html > ./constellation/toc.html.t
-mv ./constellation/toc.html{.t,}
+mv ./constellation/toc.html.t ./constellation/toc.html
 mv ./constellation/toc.html ./pages/docs/help/toc.html
 echo "search and replace end"
