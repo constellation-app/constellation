03d33a20053965be9a892ffd652f72f01a4045ff
serpens2406 <58677108+serpens24@users.noreply.github.com>
2021-10-05 12:34:36 +1030

    Update delimited importer msg (#1454)
    * Update delimited importer msg
    
    Updated the summary alert created when importing delimited files.
    
    * Reworked alert message as its no longer in a dialog. (its become less useful TBH.
    
    * alternate text based on number of valid files
    
    Arcturus comments

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/RunPane.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/RunPane.java
@@ -91,6 +91,8 @@ public final class RunPane extends BorderPane implements KeyListener {
     private final AttributeList sourceVertexAttributeList;
     private final AttributeList destinationVertexAttributeList;
     private final AttributeList transactionAttributeList;
+    private String paneName = "";
+    
     private Point2D draggingOffset;
     private AttributeNode draggingAttributeNode;
     private ImportTableColumn mouseOverColumn;
@@ -150,8 +152,9 @@ public final class RunPane extends BorderPane implements KeyListener {
         }
     }
 
-    public RunPane(final ImportController importController, final String displayText) {
+    public RunPane(final ImportController importController, final String displayText, final String paneName) {
         this.importController = importController;
+        this.paneName = paneName;
 
         if (rowFilter == null) {
             try {
@@ -306,6 +309,15 @@ public final class RunPane extends BorderPane implements KeyListener {
         });
     }
 
+    /**
+     * Update name associated with this pane. This value is used in ImportDefinition construction to identify the
+     * source of the ImportDefinition - ultimately being used when performing import to support an import status dialog.
+     * @param paneName Value to set paneName to.
+     */
+    public void setPaneName(String paneName) {
+        this.paneName = paneName;
+    } 
+    
     public Point2D getDraggingOffset() {
         return draggingOffset;
     }
@@ -507,7 +519,7 @@ public final class RunPane extends BorderPane implements KeyListener {
             rf.setColumns(currentColumnLabels);
         }
 
-        final ImportDefinition definition = new ImportDefinition(firstRow, rf);
+        final ImportDefinition definition = new ImportDefinition(paneName, firstRow, rf);
 
         for (final TableColumn<TableRow, ?> column : sampleDataView.getColumns()) {
             final ImportTableColumn importTableColumn = (ImportTableColumn) column;
