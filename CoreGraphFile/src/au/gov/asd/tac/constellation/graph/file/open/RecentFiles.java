35fbc2674c731eed962b58c5c6c186c87f45e143
antares1470 <62311243+antares1470@users.noreply.github.com>
2021-10-19 09:57:44 +1100

    Welcome Page Recent Graphs Exception and Open Recent Duplicates Fix (#1465)
    * fixed issue with exceptions thrown loading recent graphs from welcome page
    
    * fixed graph load from welcome page to use path instead of name
    
    * updated javadoc
    
    * updated javadoc
    
    * converted to using HistoryItem instead of Tuple
    
    * fixed issue with duplicate recent files
    
    * removed need for RecentFilesWelcomePage
    
    * fixed sonar smell
    
    * made suggested changes
    
    * added unit test
    
    * converted function to use stream

--- CoreGraphFile/src/au/gov/asd/tac/constellation/graph/file/open/RecentFiles.java
+++ CoreGraphFile/src/au/gov/asd/tac/constellation/graph/file/open/RecentFiles.java
@@ -44,6 +44,7 @@
 package au.gov.asd.tac.constellation.graph.file.open;
 
 import au.gov.asd.tac.constellation.graph.file.open.RecentFiles.HistoryItem;
+import com.google.common.collect.ImmutableList;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
@@ -135,6 +136,19 @@ public final class RecentFiles {
             return Collections.unmodifiableList(HISTORY);
         }
     }
+    
+    /**
+     * Gets the read-only list of unique, existing recent closed files
+     *
+     * @return list of recent files
+     */
+    public static List<HistoryItem> getUniqueRecentFiles() {
+        return getRecentFiles().stream()
+                .filter(file -> convertPath2File(file.getPath()) != null)
+                .distinct()
+                .collect(ImmutableList.toImmutableList());
+    }
+    
     private static volatile boolean historyProbablyValid;
 
     /**
@@ -302,7 +316,7 @@ public final class RecentFiles {
         return f == null ? null : f.getPath();
     }
 
-    static FileObject convertPath2File(final String path) {
+    public static FileObject convertPath2File(final String path) {
         File f = new File(path);
         f = FileUtil.normalizeFile(f);
         return f == null ? null : FileUtil.toFileObject(f);
@@ -333,7 +347,7 @@ public final class RecentFiles {
      * One item of the recently closed files history. Comparable by the time
      * field, ascending from most recent to older items.
      */
-    static final class HistoryItem implements Comparable<HistoryItem> {
+    public static final class HistoryItem implements Comparable<HistoryItem> {
 
         private int id;
         private final String path;
