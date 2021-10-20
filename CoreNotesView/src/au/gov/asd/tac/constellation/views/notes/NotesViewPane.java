8537cb366cbae275d5d85d757c083474925b4466
Guilty-Spark-343 <63685177+Guilty-Spark-343@users.noreply.github.com>
2021-10-20 14:42:54 +1100

    Add autoscroll for new notes being added to the notes view

--- CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/NotesViewPane.java
+++ CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/NotesViewPane.java
@@ -514,7 +514,8 @@ public class NotesViewPane extends BorderPane {
                     createNote(note);
                 });
             }
-
+            notesListScrollPane.applyCss();
+            notesListScrollPane.layout();
             // Keeps the scroll bar at the bottom?
             notesListScrollPane.setVvalue(notesListScrollPane.getVmax());
         });
