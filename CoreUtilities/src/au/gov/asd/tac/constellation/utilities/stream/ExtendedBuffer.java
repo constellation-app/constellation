b0c21054db4a0813bbd543e8c3d1c4386cd2a55f
serpens24 <serpens2406@gmail.com>
2022-01-28 13:28:48 +1030

    Initial unit test commit, plus documentation of file under test

--- CoreUtilities/src/au/gov/asd/tac/constellation/utilities/stream/ExtendedBuffer.java
+++ CoreUtilities/src/au/gov/asd/tac/constellation/utilities/stream/ExtendedBuffer.java
@@ -28,29 +28,44 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class ExtendedBuffer {
 
+    /**
+     * Define individual buffer used by <code>ExtendedBuffer</code>. Each buffer will have
+     * fixed maximum length, defined by value of <code>bufferSize</code> passed to
+     * <code>ExtendedBuffer</code>  constructor and maintain awareness of number of bytes
+     * populated/read.
+     */
     private static class Buffer {
-
         private int length;
         private int position = 0;
         private byte[] data;
     }
 
+    // Marker to denote end of file.
     private static final Buffer END_OF_FILE_MARKER = new Buffer();
-
     static {
         END_OF_FILE_MARKER.length = 0;
         END_OF_FILE_MARKER.data = null;
     }
 
+    // Maximum size of data to store in each buffer instance
     private final int bufferSize;
 
+    // queue containing all buffers, each added buffer will have max size of
+    // <code>bufferSize</code>.
     private final BlockingQueue<Buffer> queue = new LinkedBlockingQueue<>();
 
+    // Buffer objects used for input/output of 'next' buffer to be added/removed from queue.
     private Buffer outputBuffer;
     private Buffer inputBuffer;
 
+    // Maintain atomically count of how many bytes are availalbe in the buffer(s) to read.
     private AtomicLong available = new AtomicLong(0L);
 
+    /**
+     * constructor to create ExtendedBuffer with defined maximum size of individual buffers.
+     *
+     * @param bufferSize The size of each individual buffer.
+     */
     public ExtendedBuffer(final int bufferSize) {
         this.bufferSize = bufferSize;
 
@@ -61,20 +76,35 @@ public class ExtendedBuffer {
         inputBuffer.length = inputBuffer.position = bufferSize;
     }
 
+    /**
+     * Return InputStream handle.
+     *
+     * @return Handle to the input stream that manages reads from <code>ExtendedBuffer</code>.
+     */
     public InputStream getInputStream() {
         return inputStream;
     }
 
+    /**
+     * Return OutputStream handle.
+     *
+     * @return Handle to the output stream that manages writes to <code>ExtendedBuffer</code>.
+     */
     public OutputStream getOutputStream() {
         return outputStream;
     }
 
+    /**
+     * Return number of bytes available to read in the <code>ExtendedBuffer</code>
+     *
+     * @return Number of bytes available to read.
+     */
     public long getAvailableSize() {
         return available.get();
     }
 
     /**
-     * Get the buffer data in a byte array.
+     * Get the entire extended buffer data in a byte array.
      * <p>
      * This is a one-off read; when the data has been read, it is gone.
      *
@@ -106,19 +136,32 @@ public class ExtendedBuffer {
 
     private final InputStream inputStream = new InputStream() {
 
+        /**
+         * Read values byte by byte from queue. As buffers are read from <code>queue</code>
+         * they are stored one at a time in <code>inputBuffer</code>.
+         * Read values are converted to integers. -1 is returned when no data is available to read.
+         */
         @Override
         public int read() throws IOException {
             if (inputBuffer.position < inputBuffer.length) {
+                // There are still bytes to read in inputBuffer, read the next one and reduce
+                // the atomic available count
                 available.getAndDecrement();
                 return inputBuffer.data[inputBuffer.position++] & 0xFF;
             } else if (inputBuffer.length < bufferSize) {
+                // nothing left to read
                 return -1;
             } else {
+                // see if another buffer can be taken from head of the queue of buffers
                 try {
                     inputBuffer = queue.take();
                     if (inputBuffer.length == 0) {
+                        // retrieved buffer is empty, nothing left to read
                         return -1;
                     }
+                    // Read the first byte from the newly taken buffer. inputBuffer now populated
+                    // for subsequent take calls. Reduce the atomic available counter
+                    // note - when buffers are added to thew queue their position is set to 0
                     available.getAndDecrement();
                     return inputBuffer.data[inputBuffer.position++] & 0xFF;
                 } catch (InterruptedException ex) {
@@ -165,29 +208,64 @@ public class ExtendedBuffer {
         }
     };
 
+    /**
+     * Manage output stream as a queue of output buffers to ensure all content is handled.
+     * Each individual buffer has a defined maximum size.
+     * After stream is closed all content will have been written into ExtendedBuffer.queue
+     * and will contain potentially multiple buffers of size bufferSize.
+     */
     private final OutputStream outputStream = new OutputStream() {
-
+        
+        /**
+         * Writes the 8 low-order bits (lowest order byte) of <code>b</code> to the end of
+         * the currently active <code>outputBuffer</code>. The 24 high order bits are
+         * ignored.
+         * If as a result of the write <code>outputBuffer</code> has been filled, it is added
+         * to <code>ExtendedBuffer.queue</code> and a new buffer created to handle future calls
+         * to <code>write</code>.
+         * 
+         * @param b The integer to extract lowest 8 bits from.
+         */
         @Override
         public void write(int b) throws IOException {
+            
+            // Append the lowest order byte from supplied integer to outputBuffer.
             outputBuffer.data[outputBuffer.position++] = (byte) b;
+            
+            // Handle outputBuffer becoming full by adding the buffer to ExtendedBuffer.queue
+            // and preparing a new outPutbuffer fur subsequent writes.
             if (outputBuffer.position == bufferSize) {
-                addQueueToBuffer(outputBuffer);
+                addBufferToQueue(outputBuffer);
                 outputBuffer = new Buffer();
                 outputBuffer.data = new byte[bufferSize];
             }
         }
 
+        /**
+         * Read a subset of bytes read from supplied byte array and write them into outputBuffer field.
+         * Should size of the data to be read exceed outputBuffer size (as specified by bufferSize)
+         * outputBuffer is iteratively filled and added to buffer queue, allowing full content to be read
+         * into 1 or more buffers, but each buffer has a known maximum size.
+         * 
+         * @param b The byte array to read bytes from.
+         * @param off The offset from start of source byte array to start reading from.
+         * @param len The maximum number of bytes to read from the source byte array.
+         */
         @Override
         public void write(byte[] b, int off, int len) throws IOException {
 
             while (len > 0) {
+                // Determine how many bytes from b (up to a maximum of len) can fit into the the output buffer
+                // given the buffer size and current position.
                 int bytesToCopy = Math.min(bufferSize - outputBuffer.position, len);
 
+                // Nibble off the number of bytes that can be read into the outputBuffer
                 System.arraycopy(b, off, outputBuffer.data, outputBuffer.position, bytesToCopy);
                 outputBuffer.position += bytesToCopy;
 
+                // outputBuffer is full, add it to the buffer queue for future processing and continue reading
                 if (outputBuffer.position == bufferSize) {
-                    addQueueToBuffer(outputBuffer);
+                    addBufferToQueue(outputBuffer);
                     outputBuffer = new Buffer();
                     outputBuffer.data = new byte[bufferSize];
                 }
@@ -197,19 +275,34 @@ public class ExtendedBuffer {
             }
         }
 
+        /**
+         * Close the output stream, ensuring that all output buffers have been added to
+         * ExtendedBuffer.queue.
+         */
         @Override
         public void close() {
-            addQueueToBuffer(outputBuffer);
+            addBufferToQueue(outputBuffer);
             if (outputBuffer.length == bufferSize) {
                 queue.add(END_OF_FILE_MARKER);
             }
             outputBuffer = null;
         }
 
-        private void addQueueToBuffer(Buffer buffer) {
+        /**
+         * Add the supplied buffer to the buffer queue and increment the atomic 'available' counter.
+         * 
+         * @param buffer The buffer being added to <code>ExtendedBuffer.queue</code>.
+         */
+        private void addBufferToQueue(Buffer buffer) {
+            // Set the size of content in the buffer and reset buffer position to start of buffer.
+            // Add the buffer to the queue.
             buffer.length = buffer.position;
             buffer.position = 0;
+            
+            // Increment atomoic available value to include the total bytes in the buffer.
             available.addAndGet(buffer.length);
+            
+            // Add the buffer to the queue of buffers.
             queue.add(buffer);
         }
     };
---
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/stream/ExtendedBufferNGTest.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.gov.asd.tac.constellation.utilities.stream;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author serpens24
+ */
+public class ExtendedBufferNGTest {
+    
+    final private int size = 10;
+    
+    public ExtendedBufferNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test the default constructor - essentially just show it creates the
+     * 
+     */
+    @Test
+    public void testDefaultConstructor() throws Exception {
+
+        ExtendedBuffer localBuffer = new ExtendedBuffer(size);
+        
+        Field bufferSize = ExtendedBuffer.class.getDeclaredField("bufferSize");
+        bufferSize.setAccessible(true);
+        assertEquals(size, (int)bufferSize.get(localBuffer));
+        assertEquals(localBuffer.getAvailableSize(), 0);
+    }
+
+
+    /**
+     * Test of getOutputStream method, of class ExtendedBuffer.
+     */
+    @Test
+    public void testOutputStream() throws Exception {
+
+        ExtendedBuffer buffer = new ExtendedBuffer(size);
+        OutputStream outputStream = buffer.getOutputStream();
+        byte[] bytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes();
+
+        try {
+            outputStream.write(bytes, 0, bytes.length);
+            outputStream.write(1);
+            outputStream.write(2);
+            outputStream.write(-2147483648); // lowest 8 bits = 00000000 = 0
+            outputStream.write(2147483647);  // lowest 8 bits = 11111111 = -1 (signed byte)
+            
+        } finally {
+            outputStream.close();
+        }
+        
+        // Show that entire bytes array has been read into buffer, despite exceeding buffer size
+        assertEquals(buffer.getAvailableSize(), bytes.length + 4);
+        
+        // Extract the data from buffer and confirm is matches what was iused to
+        // populate the buffer
+        byte[] outBytes = buffer.getData();
+        for (int i = 0; i < bytes.length; i++) {
+            assertEquals(outBytes[i], i + 65);
+        }
+        assertEquals(outBytes[bytes.length], 1);
+        assertEquals(outBytes[bytes.length + 1], 2);
+        assertEquals(outBytes[bytes.length + 2], 0);
+        assertEquals(outBytes[bytes.length + 3], -1);
+    }
+
+    
+    /**
+     * Test of getInputStream method, of class ExtendedBuffer.
+     */
+    @Test
+    public void testInputStream() throws Exception {
+        
+        ExtendedBuffer buffer = new ExtendedBuffer(size);
+        OutputStream outputStream = buffer.getOutputStream();
+        InputStream inputStream = buffer.getInputStream();
+
+        byte[] bytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes();
+        
+        // Populate data to write from inputStream
+        try {
+            outputStream.write(bytes, 0, bytes.length);
+        } finally {
+            outputStream.close();
+        }
+        
+        // Ready the initial content out
+        for (int i = 0; i < bytes.length; i++) {
+            assertEquals(inputStream.read(), i + 65);
+        }
+        
+    }
+    /**
+     * Test of getData method, of class ExtendedBuffer.
+     */
+    @Test
+    public void testGetData() throws Exception {
+        
+        ExtendedBuffer buffer = new ExtendedBuffer(size);
+        OutputStream outputStream = buffer.getOutputStream();
+        InputStream inputStream = buffer.getInputStream();
+
+        byte[] bytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes();
+        
+        // Populate data to write from inputStream
+        try {
+            outputStream.write(bytes, 0, bytes.length);
+        } finally {
+            outputStream.close();
+        }
+        
+        byte[] result = buffer.getData();
+        
+        // Ready the initial content out
+        for (int i = 0; i < bytes.length; i++) {
+            assertEquals(bytes[i], result[i]);
+        }
+        
+        
+    }
+    
+}
