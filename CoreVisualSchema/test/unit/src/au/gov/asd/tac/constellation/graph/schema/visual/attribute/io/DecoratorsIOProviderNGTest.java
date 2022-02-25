99e0131b62e45b8ac3ff3c0d6af627ef717f267c
serpens24 <serpens2406@gmail.com>
2022-02-25 10:42:11 +1030

    Update DecoratorsIOProviderNGTest.java

--- CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/attribute/io/DecoratorsIOProviderNGTest.java
+++ CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/attribute/io/DecoratorsIOProviderNGTest.java
@@ -68,7 +68,7 @@ public class DecoratorsIOProviderNGTest {
         System.out.println("DecoratorsIOProvider.testReadObject");
         
         final JsonNode mockJsonNode = mock(JsonNode.class);
-        final GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
+        GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
         
         int attributeId = 23;
         int elementId = 41;
@@ -76,37 +76,63 @@ public class DecoratorsIOProviderNGTest {
         // Create object under test
         DecoratorsIOProvider instance = new DecoratorsIOProvider();
 
-        final ArgumentCaptor<Integer> captorAtributeId = ArgumentCaptor.forClass(Integer.class);
-        final ArgumentCaptor<Integer> captorElementId = ArgumentCaptor.forClass(Integer.class);
-        final ArgumentCaptor<VertexDecorators> captorAttributeValue = ArgumentCaptor.forClass(VertexDecorators.class);
-        final ArgumentCaptor<String> captorStringAttributeValue = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> captorAtributeId = ArgumentCaptor.forClass(Integer.class);
+        ArgumentCaptor<Integer> captorElementId = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<VertexDecorators> captorObjectAttributeValue = ArgumentCaptor.forClass(VertexDecorators.class);
+        ArgumentCaptor<String> captorStringAttributeValue = ArgumentCaptor.forClass(String.class);
         
         
         // Call method under test with JsonNode.isNull=false and JsonNode.isObject=true
         when(mockJsonNode.isNull()).thenReturn(false);
         when(mockJsonNode.isObject()).thenReturn(true);
-        
         JsonNode decoratorCoordinate = new TextNode("decorator");
         when(mockJsonNode.get(anyString())).thenReturn(decoratorCoordinate);
         instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
-
-        Mockito.verify(mockGraph, times(1)).setObjectValue(captorAtributeId.capture(), captorElementId.capture(), captorAttributeValue.capture());
+        Mockito.verify(mockGraph, times(1)).setObjectValue(captorAtributeId.capture(), captorElementId.capture(), captorObjectAttributeValue.capture());
         assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
         assertEquals((int)captorElementId.getAllValues().get(0), elementId);
-        assertEquals(captorAttributeValue.getAllValues().get(0).getNorthEastDecoratorAttribute(), "decorator");
-        assertEquals(captorAttributeValue.getAllValues().get(0).getNorthWestDecoratorAttribute(), "decorator");
-        assertEquals(captorAttributeValue.getAllValues().get(0).getSouthEastDecoratorAttribute(), "decorator");
-        assertEquals(captorAttributeValue.getAllValues().get(0).getSouthWestDecoratorAttribute(), "decorator");
-        
+        assertEquals(captorObjectAttributeValue.getAllValues().get(0).getNorthEastDecoratorAttribute(), "decorator");
+        assertEquals(captorObjectAttributeValue.getAllValues().get(0).getNorthWestDecoratorAttribute(), "decorator");
+        assertEquals(captorObjectAttributeValue.getAllValues().get(0).getSouthEastDecoratorAttribute(), "decorator");
+        assertEquals(captorObjectAttributeValue.getAllValues().get(0).getSouthWestDecoratorAttribute(), "decorator");
+         
         // Call method under test with JsonNode.isNull=false and JsonNode.isObject=false
+        mockGraph = mock(GraphWriteMethods.class);
+        captorAtributeId = ArgumentCaptor.forClass(Integer.class);
+        captorElementId = ArgumentCaptor.forClass(Integer.class);
         when(mockJsonNode.isObject()).thenReturn(false);
         when(mockJsonNode.textValue()).thenReturn("Test");
         instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
         Mockito.verify(mockGraph, times(1)).setStringValue(captorAtributeId.capture(), captorElementId.capture(), captorStringAttributeValue.capture());
         assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
         assertEquals((int)captorElementId.getAllValues().get(0), elementId);
-        assertEquals(captorStringAttributeValue.getAllValues().get(0), "Test");  
+        assertEquals(captorStringAttributeValue.getAllValues().get(0), "Test");
 
+        // Call method under test with JsonNode.isNull=true and JsonNode.isObject=false
+        mockGraph = mock(GraphWriteMethods.class);
+        captorAtributeId = ArgumentCaptor.forClass(Integer.class);
+        captorElementId = ArgumentCaptor.forClass(Integer.class);
+        captorStringAttributeValue = ArgumentCaptor.forClass(String.class);
+        when(mockJsonNode.isNull()).thenReturn(true);
+        when(mockJsonNode.isObject()).thenReturn(false);
+        instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
+        Mockito.verify(mockGraph, times(1)).setStringValue(captorAtributeId.capture(), captorElementId.capture(), captorStringAttributeValue.capture());
+        assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
+        assertEquals((int)captorElementId.getAllValues().get(0), elementId);
+        assertEquals(captorStringAttributeValue.getAllValues().get(0), null);
+
+        // Call method under test with JsonNode.isNull=true and JsonNode.isObject=true
+        mockGraph = mock(GraphWriteMethods.class);
+        captorAtributeId = ArgumentCaptor.forClass(Integer.class);
+        captorElementId = ArgumentCaptor.forClass(Integer.class);
+        captorStringAttributeValue = ArgumentCaptor.forClass(String.class);
+        when(mockJsonNode.isNull()).thenReturn(true);
+        when(mockJsonNode.isObject()).thenReturn(true);
+        instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
+        Mockito.verify(mockGraph, times(1)).setStringValue(captorAtributeId.capture(), captorElementId.capture(), captorStringAttributeValue.capture());
+        assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
+        assertEquals((int)captorElementId.getAllValues().get(0), elementId);
+        assertEquals(captorStringAttributeValue.getAllValues().get(0), null);
     }
 
     /**
