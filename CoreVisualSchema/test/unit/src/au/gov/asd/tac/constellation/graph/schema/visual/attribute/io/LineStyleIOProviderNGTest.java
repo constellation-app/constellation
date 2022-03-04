8173cff4d988af102d48635d1f4fe5bb22dc707f
serpens24 <serpens2406@gmail.com>
2022-03-04 21:25:22 +1030

    Pulled LineStyleIOPRovider across to use common methods.

--- CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/attribute/io/LineStyleIOProviderNGTest.java
+++ CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/attribute/io/LineStyleIOProviderNGTest.java
@@ -1,22 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2010-2022 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package au.gov.asd.tac.constellation.graph.schema.visual.attribute.io;
 
-import au.gov.asd.tac.constellation.graph.GraphAttribute;
-import au.gov.asd.tac.constellation.graph.GraphElementType;
-import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
 import au.gov.asd.tac.constellation.graph.schema.visual.attribute.LineStyleAttributeDescription;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonNode;
-import org.mockito.ArgumentCaptor;
-import static org.mockito.ArgumentMatchers.anyInt;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -54,125 +53,10 @@ public class LineStyleIOProviderNGTest {
      */
     @Test
     public void testGetName() {
-        System.out.println("LineStyleIOProvider.testGetName");
+        System.out.println("LineStyleIOProviderNGTest.testGetName");
         
         LineStyleIOProvider instance = new LineStyleIOProvider();
         String result = instance.getName();
         assertEquals(result, LineStyleAttributeDescription.ATTRIBUTE_NAME);
     }
-
-    /**
-     * Test of readObject method, of class LineStyleIOProvider.
-     */
-    @Test
-    public void testReadObject() throws Exception {
-        System.out.println("LineStyleIOProvider.testReadObject");
-        
-        // Create object under test
-        LineStyleIOProvider instance = new LineStyleIOProvider();
-        
-        // Create mocks
-        JsonNode mockJsonNode = mock(JsonNode.class);
-        GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
-        
-        // Create argument captors
-        ArgumentCaptor<Integer> captorAtributeId = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<Integer> captorElementId = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<String> captorString = ArgumentCaptor.forClass(String.class);
-        
-        int attributeId = 23;
-        int elementId = 41;
-
-        // Test case where jnode.isnull is true
-        when(mockJsonNode.isNull()).thenReturn(true);
-        instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
-        Mockito.verify(mockJsonNode, times(0)).textValue();
-        Mockito.verify(mockGraph, times(1)).setStringValue(captorAtributeId.capture(), captorElementId.capture(), captorString.capture());
-        assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
-        assertEquals((int)captorElementId.getAllValues().get(0), elementId);
-        assertEquals(captorString.getAllValues().get(0), null);
-
-        // Test case where jnode.isnull is false
-        mockJsonNode = mock(JsonNode.class);
-        mockGraph = mock(GraphWriteMethods.class);
-        captorAtributeId = ArgumentCaptor.forClass(Integer.class);
-        captorElementId = ArgumentCaptor.forClass(Integer.class);
-        captorString = ArgumentCaptor.forClass(String.class);
-        when(mockJsonNode.isNull()).thenReturn(false);
-        when(mockJsonNode.textValue()).thenReturn("testvalue");
-        instance.readObject(attributeId, elementId, mockJsonNode, mockGraph, null, null, null, null);
-        Mockito.verify(mockJsonNode, times(1)).textValue();
-        Mockito.verify(mockGraph, times(1)).setStringValue(captorAtributeId.capture(), captorElementId.capture(), captorString.capture());
-        assertEquals((int)captorAtributeId.getAllValues().get(0), attributeId);
-        assertEquals((int)captorElementId.getAllValues().get(0), elementId);
-        assertEquals(captorString.getAllValues().get(0), "testvalue");
-
-        
-    }
-
-    /**
-     * Test of writeObject method, of class LineStyleIOProvider.
-     */
-    @Test
-    public void testWriteObject() throws Exception {
-        System.out.println("LineStyleIOProvider.testWriteObject");
-        
-        // Create object under test
-        LineStyleIOProvider instance = new LineStyleIOProvider();
-        
-        // Create mocks
-        JsonGenerator mockJsonGenerator = mock(JsonGenerator.class);
-        GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
-        
-        // Create argument captors
-        final ArgumentCaptor<Integer> captorAtributeId = ArgumentCaptor.forClass(Integer.class);
-        final ArgumentCaptor<Integer> captorElementId = ArgumentCaptor.forClass(Integer.class);
-        final ArgumentCaptor<String> captorAttrName = ArgumentCaptor.forClass(String.class);
-        final ArgumentCaptor<String> captorField = ArgumentCaptor.forClass(String.class);
-        final ArgumentCaptor<String> captorValue = ArgumentCaptor.forClass(String.class);
-
-        int attributeId = 23;
-        int elementId = 41;
-        String attrType = "attrType";
-        String attrName = "attrName";
-        String attrDesc = "attrDesc";
-        GraphAttribute attr = new GraphAttribute(attributeId, GraphElementType.GRAPH, attrType, attrName, attrDesc,
-            null, null);
-
-        // Test not verbose and graph.IsDefaultValue is true skips all processing
-        when(mockGraph.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
-        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraph, null, false);
-        Mockito.verify(mockGraph, times(0)).getObjectValue(captorAtributeId.capture(), captorElementId.capture());
-
-        // Test not verbose but graph.IsDefaultValue is false, graph.getObjectValue returns null
-        when(mockGraph.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
-        when(mockGraph.getStringValue(anyInt(), anyInt())).thenReturn(null);
-        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraph, null, false);
-        Mockito.verify(mockGraph, times(1)).getStringValue(captorAtributeId.capture(), captorElementId.capture());
-        Mockito.verify(mockJsonGenerator, times(1)).writeNullField(captorAttrName.capture());
-        assertEquals((int)captorAtributeId.getValue(), attributeId);
-        assertEquals((int)captorElementId.getValue(), elementId);
-        assertEquals(captorAttrName.getValue(), attrName);
-        
-        // Test verbose and graph.IsDefaultValue is false, graph.getObjectValue returns null 
-        mockJsonGenerator = mock(JsonGenerator.class);
-        mockGraph = mock(GraphWriteMethods.class);
-        when(mockGraph.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
-        when(mockGraph.getStringValue(anyInt(), anyInt())).thenReturn(null);
-        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraph, null, true);
-        Mockito.verify(mockGraph, times(1)).getStringValue(captorAtributeId.capture(), captorElementId.capture());
-        Mockito.verify(mockJsonGenerator, times(1)).writeNullField(captorAttrName.capture());
-        assertEquals((int)captorAtributeId.getValue(), attributeId);
-        assertEquals((int)captorElementId.getValue(), elementId);
-        assertEquals(captorAttrName.getValue(), attrName);
-        
-        // Test verbose and graph.IsDefaultValue is true, graph.getObjectValue returns decotrator object
-        when(mockGraph.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
-        when(mockGraph.getStringValue(anyInt(), anyInt())).thenReturn("teststring");
-        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraph, null, true);
-        Mockito.verify(mockJsonGenerator, times(1)).writeStringField(captorField.capture(), captorValue.capture());
-        assertEquals(captorField.getAllValues().get(0), attrName);
-        assertEquals(captorValue.getAllValues().get(0), "teststring");
-    }
-    
 }
