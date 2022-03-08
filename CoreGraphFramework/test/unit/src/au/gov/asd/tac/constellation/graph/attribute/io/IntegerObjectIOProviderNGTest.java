48898f4b5f8838a7695d480c0fdb6599f462160a
serpens24 <serpens2406@gmail.com>
2022-03-08 11:26:26 +1030

    Remaining IO providers in CoreGraphFramework

---
+++ CoreGraphFramework/test/unit/src/au/gov/asd/tac/constellation/graph/attribute/io/IntegerObjectIOProviderNGTest.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2010-2022 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.graph.attribute.io;
+
+import au.gov.asd.tac.constellation.graph.Attribute;
+import au.gov.asd.tac.constellation.graph.GraphAttribute;
+import au.gov.asd.tac.constellation.graph.GraphElementType;
+import au.gov.asd.tac.constellation.graph.GraphReadMethods;
+import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.graph.attribute.IntegerObjectAttributeDescription;
+import au.gov.asd.tac.constellation.utilities.datastructure.ImmutableObjectCache;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Map;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author serpens24
+ */
+public class IntegerObjectIOProviderNGTest {
+
+    // Create object under test
+    IntegerObjectIOProvider instance;
+
+    // Define mocks
+    GraphReadMethods mockGraphReadMethods;
+    GraphWriteMethods mockGraphWriteMethods;
+    JsonNode mockJsonNode;
+    JsonGenerator mockJsonGenerator;
+
+    // Test variables
+    final int attributeId = 23;
+    final int elementId = 41;
+    final String attribValue = "TestAttrib";
+    final GraphAttribute attr = new GraphAttribute(attributeId, GraphElementType.GRAPH, "attrType", "attrName", "attrDesc", null, null);
+    
+    public IntegerObjectIOProviderNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        instance = new IntegerObjectIOProvider();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    /**
+     * Perform reset of all mocks and argument captors to ensure clean test steps.
+     */
+    public void resetMocking() {
+        mockGraphReadMethods = mock(GraphReadMethods.class);
+        mockGraphWriteMethods = mock(GraphWriteMethods.class);
+        mockJsonNode = mock(JsonNode.class);
+        mockJsonGenerator = mock(JsonGenerator.class);
+    }
+
+    /**
+     * Test of getName method, of class IntegerObjectIOProvider.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("IntegerObjectIOProviderNGTest.testGetName");
+        assertEquals(instance.getName(), IntegerObjectAttributeDescription.ATTRIBUTE_NAME);
+    }
+
+    /**
+     * Test of readObject method, of class IntegerObjectIOProvider.
+     */
+    @Test
+    public void testReadObject() throws Exception {
+        System.out.println("IntegerObjectIOProviderNGTest.testReadObject");
+        
+        // Call method under test with JsonNode set to return isNull = true
+        resetMocking();
+        when(mockJsonNode.isNull()).thenReturn(true);
+        instance.readObject(attributeId, elementId, mockJsonNode, mockGraphWriteMethods, null, null, null, null);
+        Mockito.verify(mockJsonNode, times(0)).intValue();
+        Mockito.verify(mockGraphWriteMethods, times(1)).setObjectValue(attributeId, elementId, null);
+        
+        // Call method under test with JsonNode set to return isNull = false
+        resetMocking();
+        when(mockJsonNode.isNull()).thenReturn(false);
+        when(mockJsonNode.intValue()).thenReturn(5);
+        instance.readObject(attributeId, elementId, mockJsonNode, mockGraphWriteMethods, null, null, null, null);
+        Mockito.verify(mockJsonNode, times(1)).intValue();
+        Mockito.verify(mockGraphWriteMethods, times(1)).setObjectValue(attributeId, elementId, 5);
+    }
+
+    /**
+     * Test of writeObject method, of class IntegerObjectIOProvider.
+     */
+    @Test
+    public void testWriteObject() throws Exception {
+        System.out.println("IntegerObjectIOProviderNGTest.testWriteObject");
+        
+        // Test not verbose and graph.IsDefaultValue is true
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, false);
+        Mockito.verify(mockGraphReadMethods, times(0)).getObjectValue(anyInt(), anyInt());
+        
+        // Test verbose and graph.IsDefaultValue is true, getObjectValue to return null
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
+        when(mockGraphReadMethods.getObjectValue(anyInt(), anyInt())).thenReturn(null);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, true);
+        Mockito.verify(mockJsonGenerator, times(1)).writeNullField(attr.getName());
+        Mockito.verify(mockJsonGenerator, times(0)).writeNumberField(anyString(), anyInt());
+        
+        // Test not verbose and graph.IsDefaultValue is false, getObjectValue returns null 
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
+        when(mockGraphReadMethods.getObjectValue(anyInt(), anyInt())).thenReturn(null);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, false);
+        Mockito.verify(mockJsonGenerator, times(1)).writeNullField(attr.getName());
+        Mockito.verify(mockJsonGenerator, times(0)).writeNumberField(anyString(), anyInt());
+        
+        // Test verbose and graph.IsDefaultValue is false, getObjectValue returns int
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
+        when(mockGraphReadMethods.getObjectValue(anyInt(), anyInt())).thenReturn(5);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, true);
+        Mockito.verify(mockJsonGenerator, times(0)).writeNullField(anyString());
+        Mockito.verify(mockJsonGenerator, times(1)).writeNumberField(attr.getName(), 5);
+    }
+}
---
+++ CoreGraphFramework/test/unit/src/au/gov/asd/tac/constellation/graph/attribute/io/LongIOProviderNGTest.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2010-2022 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.graph.attribute.io;
+
+import au.gov.asd.tac.constellation.graph.GraphAttribute;
+import au.gov.asd.tac.constellation.graph.GraphElementType;
+import au.gov.asd.tac.constellation.graph.GraphReadMethods;
+import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.graph.attribute.LongAttributeDescription;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import static org.mockito.ArgumentMatchers.anyInt;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author serpens24
+ */
+public class LongIOProviderNGTest {
+
+    // Create object under test
+    LongIOProvider instance;
+
+    // Define mocks
+    GraphReadMethods mockGraphReadMethods;
+    GraphWriteMethods mockGraphWriteMethods;
+    JsonNode mockJsonNode;
+    JsonGenerator mockJsonGenerator;
+
+    // Test variables
+    final int attributeId = 23;
+    final int elementId = 41;
+    final String attribValue = "TestAttrib";
+    final GraphAttribute attr = new GraphAttribute(attributeId, GraphElementType.GRAPH, "attrType", "attrName", "attrDesc", null, null);
+
+    
+    public LongIOProviderNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        instance = new LongIOProvider();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    /**
+     * Perform reset of all mocks and argument captors to ensure clean test steps.
+     */
+    public void resetMocking() {
+        mockGraphReadMethods = mock(GraphReadMethods.class);
+        mockGraphWriteMethods = mock(GraphWriteMethods.class);
+        mockJsonNode = mock(JsonNode.class);
+        mockJsonGenerator = mock(JsonGenerator.class);
+    }
+
+    /**
+     * Test of getName method, of class LongIOProvider.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("LongIOProviderNGTest.testGetName");
+        assertEquals(instance.getName(), LongAttributeDescription.ATTRIBUTE_NAME);
+    }
+
+    /**
+     * Test of readObject method, of class LongIOProvider.
+     */
+    @Test
+    public void testReadObject() throws Exception {
+        System.out.println("LongIOProviderNGTest.testReadObject");
+        
+        // Call method under test with JsonNode set to return isNull = true
+        resetMocking();
+        when(mockJsonNode.longValue()).thenReturn((long)5);
+        instance.readObject(attributeId, elementId, mockJsonNode, mockGraphWriteMethods, null, null, null, null);
+        Mockito.verify(mockJsonNode, times(1)).longValue();
+        Mockito.verify(mockGraphWriteMethods, times(1)).setLongValue(attributeId, elementId, (long)5);
+    }
+
+    /**
+     * Test of writeObject method, of class LongIOProvider.
+     */
+    @Test
+    public void testWriteObject() throws Exception {
+        System.out.println("LongIOProviderNGTest.testWriteObject");
+        
+        // Test not verbose and graph.IsDefaultValue is true
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, false);
+        Mockito.verify(mockGraphReadMethods, times(0)).getLongValue(anyInt(), anyInt());
+        
+        // Test verbose and graph.IsDefaultValue is true
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(true);
+        when(mockGraphReadMethods.getLongValue(anyInt(), anyInt())).thenReturn((long)5);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, true);
+        Mockito.verify(mockJsonGenerator, times(1)).writeNumberField(attr.getName(), (long)5);
+        
+        // Test not verbose and graph.IsDefaultValue is false
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
+        when(mockGraphReadMethods.getLongValue(anyInt(), anyInt())).thenReturn((long)5);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, false);
+        Mockito.verify(mockJsonGenerator, times(1)).writeNumberField(attr.getName(), (long)5);
+        
+        // Test verbose and graph.IsDefaultValue is false
+        resetMocking();
+        when(mockGraphReadMethods.isDefaultValue(anyInt(), anyInt())).thenReturn(false);
+        when(mockGraphReadMethods.getLongValue(anyInt(), anyInt())).thenReturn((long)5);
+        instance.writeObject(attr, elementId, mockJsonGenerator, mockGraphReadMethods, null, true);
+        Mockito.verify(mockJsonGenerator, times(1)).writeNumberField(attr.getName(), (long)5);
+    }
+}
