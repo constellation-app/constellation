0ad1e90b898d188e2292d7730a3fb34606d9706f
serpens24 <serpens2406@gmail.com>
2022-03-11 10:10:02 +1030

    Update VisualSchemaV4UpdateProviderNGTest.java
    progress checkin

--- CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/compatibility/VisualSchemaV4UpdateProviderNGTest.java
+++ CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/compatibility/VisualSchemaV4UpdateProviderNGTest.java
@@ -15,12 +15,22 @@
  */
 package au.gov.asd.tac.constellation.graph.schema.visual.compatibility;
 
+import au.gov.asd.tac.constellation.graph.GraphElementType;
 import au.gov.asd.tac.constellation.graph.StoreGraph;
+import au.gov.asd.tac.constellation.graph.schema.Schema;
 import au.gov.asd.tac.constellation.graph.schema.SchemaFactory;
 import au.gov.asd.tac.constellation.graph.schema.SchemaFactoryUtilities;
+import au.gov.asd.tac.constellation.graph.schema.visual.GraphLabels;
 import au.gov.asd.tac.constellation.graph.schema.visual.VisualSchemaFactory;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -95,11 +105,48 @@ public class VisualSchemaV4UpdateProviderNGTest {
         resetMocking();
         assertEquals(instance.getToVersionNumber(), VisualSchemaV4UpdateProvider.SCHEMA_VERSION_THIS_UPDATE);
     }
-
+    
     /**
      * Test of schemaUpdate method, of class VisualSchemaV4UpdateProvider.
      */
     @Test
     public void testSchemaUpdate() {
+        System.out.println("VisualSchemaV4UpdateProviderNGTest.testSchemaUpdate");
+
+        ArgumentCaptor<GraphElementType> elementTypeCaptor = ArgumentCaptor.forClass(GraphElementType.class);
+        ArgumentCaptor<Object> arrayCaptor = ArgumentCaptor.forClass(Object.class);
+
+        when(mockStoreGraph.getAttribute(GraphElementType.VERTEX, "Name")).thenReturn(23);
+        when(mockStoreGraph.getAttribute(GraphElementType.TRANSACTION, "UniqueId")).thenReturn(708);
+        when(mockStoreGraph.getVertexCount()).thenReturn(2);
+        when(mockStoreGraph.getTransactionCount()).thenReturn(2);
+        when(mockStoreGraph.getVertex(anyInt())).thenReturn(0,1);
+        when(mockStoreGraph.getTransaction(anyInt())).thenReturn(2,3);
+        when(mockStoreGraph.getStringValue(23, 0)).thenReturn("Label0");
+        when(mockStoreGraph.getStringValue(23, 1)).thenReturn("Label1");
+        when(mockStoreGraph.getStringValue(708, 2)).thenReturn("Label2");
+        when(mockStoreGraph.getStringValue(708, 3)).thenReturn("Label3");
+        when(mockStoreGraph.getStringValue(0, 0)).thenReturn("LabelValue");
+        when(mockStoreGraph.getStringValue(0, 1)).thenReturn(null);
+        when(mockStoreGraph.getSchema()).thenReturn(new Schema(new VisualSchemaFactory()));
+              
+        instance.schemaUpdate(mockStoreGraph);
+        Mockito.verify(mockStoreGraph, times(1)).setPrimaryKey(GraphElementType.VERTEX);
+        Mockito.verify(mockStoreGraph, times(2)).setPrimaryKey(GraphElementType.TRANSACTION);
+        Mockito.verify(mockStoreGraph, times(1)).setStringValue(0, 0, "Label0");
+        Mockito.verify(mockStoreGraph, times(2)).setStringValue(0, 1, "Label1");
+        Mockito.verify(mockStoreGraph, times(1)).setStringValue(0, 2, "Label2");
+        Mockito.verify(mockStoreGraph, times(1)).setStringValue(0, 3, "Label3");
+        Mockito.verify(mockStoreGraph, times(1)).removeAttribute(23);
+        Mockito.verify(mockStoreGraph, times(1)).removeAttribute(708);
+        Mockito.verify(mockStoreGraph, times(5)).setPrimaryKey(elementTypeCaptor.capture(), (int[])arrayCaptor.capture());
+        Mockito.verify(mockStoreGraph, times(1)).setObjectValue(anyInt(), anyInt(), any(GraphLabels.class));
+        assertTrue("Node".equals(elementTypeCaptor.getAllValues().get(0).toString()));
+        assertTrue("Node".equals(elementTypeCaptor.getAllValues().get(1).toString()));
+        assertTrue("Transaction (no merging)".equals(elementTypeCaptor.getAllValues().get(2).toString()));
+        assertTrue("Transaction (no merging)".equals(elementTypeCaptor.getAllValues().get(3).toString()));
+        assertTrue("Transaction (no merging)".equals(elementTypeCaptor.getAllValues().get(4).toString()));
+        assertEquals(arrayCaptor.getAllValues().get(0), 0);
+        assertEquals(arrayCaptor.getAllValues().get(1), 0);
     } 
 }
