55fae6b4a0d9cae30e11c2d01a260aa523d40799
antares1470 <62311243+antares1470@users.noreply.github.com>
2022-05-02 15:55:41 +1000

    HOTFIX: reverted change that made transaction type parameter in most analytics required (#1688)

--- CoreAnalyticView/src/au/gov/asd/tac/constellation/views/analyticview/analytics/FactAnalyticPlugin.java
+++ CoreAnalyticView/src/au/gov/asd/tac/constellation/views/analyticview/analytics/FactAnalyticPlugin.java
@@ -166,7 +166,6 @@ public abstract class FactAnalyticPlugin extends AnalyticPlugin<FactResult> {
         final PluginParameter<MultiChoiceParameterValue> transactionTypeParameter = MultiChoiceParameterType.build(TRANSACTION_TYPES_PARAMETER_ID, TransactionTypeParameterValue.class);
         transactionTypeParameter.setName("Transaction Types");
         transactionTypeParameter.setDescription("Calculate analytic only on the subgraph of transactions of these types");
-        transactionTypeParameter.setRequired(true);
         MultiChoiceParameterType.setOptionsData(transactionTypeParameter, new ArrayList<>());
         MultiChoiceParameterType.setChoicesData(transactionTypeParameter, new ArrayList<>());
         parameters.addParameter(transactionTypeParameter);
