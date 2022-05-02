35c65c21cc6c3c2312d2f4a4c99e661d7bb26fcb
antares1470 <62311243+antares1470@users.noreply.github.com>
2022-05-02 15:16:57 +1000

    HOTFIX: reverted change that made transaction type parameter in most analytics required

--- CoreAnalyticView/src/au/gov/asd/tac/constellation/views/analyticview/analytics/ScoreAnalyticPlugin.java
+++ CoreAnalyticView/src/au/gov/asd/tac/constellation/views/analyticview/analytics/ScoreAnalyticPlugin.java
@@ -179,7 +179,6 @@ public abstract class ScoreAnalyticPlugin extends AnalyticPlugin<ScoreResult> {
         final PluginParameter<MultiChoiceParameterValue> transactionTypeParameter = MultiChoiceParameterType.build(TRANSACTION_TYPES_PARAMETER_ID, TransactionTypeParameterValue.class);
         transactionTypeParameter.setName("Transaction Types");
         transactionTypeParameter.setDescription("Calculate analytic only on the subgraph of transactions of these types");
-        transactionTypeParameter.setRequired(true);
         MultiChoiceParameterType.setOptionsData(transactionTypeParameter, new ArrayList<>());
         MultiChoiceParameterType.setChoicesData(transactionTypeParameter, new ArrayList<>());
         parameters.addParameter(transactionTypeParameter);
