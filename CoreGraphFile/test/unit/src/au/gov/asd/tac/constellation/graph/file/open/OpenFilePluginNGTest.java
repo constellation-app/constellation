021c2ef9ee2adb31d24ff61943bbafca71c5dffc
sol695510 <58677774+sol695510@users.noreply.github.com>
2022-06-06 17:24:55 +0930

    Update OpenFilePluginNGTest.java

--- CoreGraphFile/test/unit/src/au/gov/asd/tac/constellation/graph/file/open/OpenFilePluginNGTest.java
+++ CoreGraphFile/test/unit/src/au/gov/asd/tac/constellation/graph/file/open/OpenFilePluginNGTest.java
@@ -15,8 +15,11 @@
  */
 package au.gov.asd.tac.constellation.graph.file.open;
 
+import au.gov.asd.tac.constellation.utilities.file.FileExtensionConstants;
 import au.gov.asd.tac.constellation.utilities.gui.filechooser.FileChooser;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +28,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.times;
 import org.openide.filesystems.FileChooserBuilder;
 import org.testfx.api.FxToolkit;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -111,5 +116,48 @@ public class OpenFilePluginNGTest {
 
         openFileStaticMock.verify(()
                 -> OpenFile.openFile(file, -1), times(numberOfFiles));
+    }
+
+    /**
+     * Test of getOpenFileChooser method, of class OpenFilePlugin.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetOpenFilePluginChooser() throws IOException {
+        System.out.println("testGetOpenFilePluginChooser");
+
+        final String fileChooserTitle = "Open";
+        final String fileChooserDescription = "Constellation Files ("
+                + FileExtensionConstants.STAR + ", "
+                + FileExtensionConstants.NEBULA + ")";
+
+        final OpenFilePlugin instance = new OpenFilePlugin();
+        final JFileChooser fileChooser = instance.getOpenFileChooser().createFileChooser();
+
+        // Ensure file chooser is constructed correctly.
+        assertEquals(fileChooser.getDialogTitle(), fileChooserTitle);
+        assertEquals(fileChooser.getChoosableFileFilters().length, 1);
+        assertEquals(fileChooser.getChoosableFileFilters()[0].getDescription(), fileChooserDescription);
+
+        // If file is invalid and does not end with correct extension.
+        final File file1 = File.createTempFile("fileInvalid", ".invalid");
+        assertEquals(fileChooser.getChoosableFileFilters()[0].accept(file1), false);
+
+        // If files do not exist.
+        final File file2 = new File("/invalidPath/fileStar" + FileExtensionConstants.STAR);
+        final File file3 = new File("/invalidPath/fileNebula" + FileExtensionConstants.NEBULA);
+        assertEquals(fileChooser.getChoosableFileFilters()[0].accept(file2), false);
+        assertEquals(fileChooser.getChoosableFileFilters()[0].accept(file3), false);
+
+        // If files exist, are valid and end with correct extensions.
+        final File file4 = File.createTempFile("fileStar", FileExtensionConstants.STAR);
+        final File file5 = File.createTempFile("fileNebula", FileExtensionConstants.NEBULA);
+        assertEquals(fileChooser.getChoosableFileFilters()[0].accept(file4), true);
+        assertEquals(fileChooser.getChoosableFileFilters()[0].accept(file5), true);
+
+        Files.deleteIfExists(file1.toPath());
+        Files.deleteIfExists(file4.toPath());
+        Files.deleteIfExists(file5.toPath());
     }
 }
