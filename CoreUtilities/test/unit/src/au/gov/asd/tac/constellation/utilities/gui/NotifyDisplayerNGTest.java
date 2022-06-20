12afb0b26c55931f96c9b1b042cdd1142d62e73a
sol695510 <58677774+sol695510@users.noreply.github.com>
2022-06-20 15:30:45 +0930

    Update NotifyDisplayerNGTest.java

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/NotifyDisplayerNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/NotifyDisplayerNGTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010-2021 Australian Signals Directorate
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -53,8 +53,9 @@ import org.testng.annotations.Test;
  * @author formalhaunt
  */
 public class NotifyDisplayerNGTest {
+
     private static final Logger LOGGER = Logger.getLogger(NotifyDisplayerNGTest.class.getName());
-    
+
     @BeforeClass
     public static void setUpClass() throws Exception {
         if (!FxToolkit.isFXApplicationThreadRunning()) {
@@ -70,7 +71,7 @@ public class NotifyDisplayerNGTest {
             LOGGER.log(Level.WARNING, "FxToolkit timed out trying to cleanup stages", ex);
         }
     }
-    
+
     @Test
     public void display() {
         display(true, true, true);
@@ -78,7 +79,7 @@ public class NotifyDisplayerNGTest {
         display(false, true, true);
         display(false, false, false);
     }
-    
+
     @Test
     public void displayWithIcon() {
         displayWithIcon(true, true, true);
@@ -86,165 +87,160 @@ public class NotifyDisplayerNGTest {
         displayWithIcon(false, true, true);
         displayWithIcon(false, false, false);
     }
-    
+
     @Test
     public void displayAlert() {
         final String title = "TITLE";
         final String header = "HEADER";
         final String message = "MESSAGE";
         final Alert.AlertType alertType = Alert.AlertType.WARNING;
-        
+
         final DialogPane dialogPane = mock(DialogPane.class);
         final Scene scene = mock(Scene.class);
         final Stage stage = mock(Stage.class);
-        
+
         try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
                 (mock, cnxt) -> {
                     assertEquals(cnxt.arguments().get(0), alertType);
                     assertEquals(cnxt.arguments().get(1), "");
                     assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
-                    
+
                     when(mock.getDialogPane()).thenReturn(dialogPane);
                     when(dialogPane.getScene()).thenReturn(scene);
                     when(scene.getWindow()).thenReturn(stage);
-                })
-        ) {
+                })) {
             NotifyDisplayer.displayAlert(title, header, message, alertType);
-            
+
             assertEquals(alertMockedConstruction.constructed().size(), 1);
-            
+
             final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-            
+
             verify(mockAlert).setTitle(title);
             verify(mockAlert).setHeaderText(header);
             verify(mockAlert).setContentText(message);
-            
+
             verify(mockAlert).setResizable(true);
-            
+
             verify(stage).setAlwaysOnTop(true);
-            
+
             verify(mockAlert).showAndWait();
         }
     }
-    
+
     @Test
     public void displayLargeAlert() {
         final String title = "TITLE";
         final String header = "HEADER";
         final String message = "MESSAGE";
         final Alert.AlertType alertType = Alert.AlertType.WARNING;
-        
+
         final DialogPane dialogPane = mock(DialogPane.class);
         final Scene scene = mock(Scene.class);
         final Stage stage = mock(Stage.class);
-        
+
         try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
                 (mock, cnxt) -> {
                     assertEquals(cnxt.arguments().get(0), alertType);
                     assertEquals(cnxt.arguments().get(1), "");
                     assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
-                    
+
                     when(mock.getDialogPane()).thenReturn(dialogPane);
                     when(dialogPane.getScene()).thenReturn(scene);
                     when(scene.getWindow()).thenReturn(stage);
-                })
-        ) {
+                })) {
             NotifyDisplayer.displayLargeAlert(title, header, message, alertType);
-            
+
             assertEquals(alertMockedConstruction.constructed().size(), 1);
-            
+
             final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-            
+
             verify(mockAlert).setTitle(title);
             verify(mockAlert).setHeaderText(header);
-            
+
             final ArgumentCaptor<TextArea> captor = ArgumentCaptor.forClass(TextArea.class);
             verify(dialogPane).setExpandableContent(captor.capture());
-            
+
             assertEquals(captor.getValue().getText(), message);
             assertTrue(captor.getValue().isWrapText());
             assertFalse(captor.getValue().isEditable());
-            
+
             verify(mockAlert).setResizable(true);
-            
+
             verify(stage).setAlwaysOnTop(true);
-            
+
             verify(mockAlert).showAndWait();
         }
     }
-    
+
     @Test
     public void displayConfirmationAlert() {
         final String title = "TITLE";
         final String header = "HEADER";
         final String message = "MESSAGE";
-        
+
         final DialogPane dialogPane = mock(DialogPane.class);
         final Scene scene = mock(Scene.class);
         final Stage stage = mock(Stage.class);
-        
+
         try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
                 (mock, cnxt) -> {
                     assertEquals(cnxt.arguments().get(0), Alert.AlertType.CONFIRMATION);
                     assertEquals(cnxt.arguments().get(1), "");
                     assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.NO, ButtonType.YES});
-                    
+
                     when(mock.getDialogPane()).thenReturn(dialogPane);
                     when(dialogPane.getScene()).thenReturn(scene);
                     when(scene.getWindow()).thenReturn(stage);
-                })
-        ) {
+                })) {
             NotifyDisplayer.displayConfirmationAlert(title, header, message);
-            
+
             assertEquals(alertMockedConstruction.constructed().size(), 1);
-            
+
             final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-            
+
             verify(mockAlert).setTitle(title);
             verify(mockAlert).setHeaderText(header);
             verify(mockAlert).setContentText(message);
-            
+
             verify(mockAlert).setResizable(true);
-            
+
             verify(stage).setAlwaysOnTop(true);
-            
+
             verify(mockAlert).showAndWait();
         }
     }
-    
+
     /**
      * Configurable test for the display notification with icon method.
      *
-     * @param isEventDispatchThread true if the test is meant to be running in the event dispatch thread, false otherwise
-     * @param isFxApplicationThread true if the test is meant to be running in the fx application thread, false otherwise
-     * @param runThroughThread true if the call is meant to run through a separate thread first, false otherwise
+     * @param isEventDispatchThread true if the test is meant to be running in
+     * the event dispatch thread, false otherwise
+     * @param isFxApplicationThread true if the test is meant to be running in
+     * the fx application thread, false otherwise
+     * @param runThroughThread true if the call is meant to run through a
+     * separate thread first, false otherwise
      */
     private void displayWithIcon(final boolean isEventDispatchThread,
-                                 final boolean isFxApplicationThread,
-                                 final boolean runThroughThread) {
+            final boolean isFxApplicationThread,
+            final boolean runThroughThread) {
         try (
-                final MockedStatic<EventQueue> eventQueueMockedStatic = Mockito.mockStatic(EventQueue.class);
-                final MockedStatic<CompletableFuture> completableFutureMockedStatic = Mockito.mockStatic(CompletableFuture.class);
-                final MockedStatic<NotificationDisplayer> notificationDisplayerMockedStatic = Mockito.mockStatic(NotificationDisplayer.class);
-                final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic = Mockito.mockStatic(SwingUtilities.class);
-                final MockedStatic<Platform> platformMockedStatic = Mockito.mockStatic(Platform.class);
-        ) {
+                final MockedStatic<EventQueue> eventQueueMockedStatic = Mockito.mockStatic(EventQueue.class); final MockedStatic<CompletableFuture> completableFutureMockedStatic = Mockito.mockStatic(CompletableFuture.class); final MockedStatic<NotificationDisplayer> notificationDisplayerMockedStatic = Mockito.mockStatic(NotificationDisplayer.class); final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic = Mockito.mockStatic(SwingUtilities.class); final MockedStatic<Platform> platformMockedStatic = Mockito.mockStatic(Platform.class);) {
             setupThreadingMocks(eventQueueMockedStatic, completableFutureMockedStatic, swingUtilitiesMockedStatic, platformMockedStatic);
-            
+
             final NotificationDisplayer notificationDisplayer = mock(NotificationDisplayer.class);
             notificationDisplayerMockedStatic.when(NotificationDisplayer::getDefault).thenReturn(notificationDisplayer);
-            
+
             swingUtilitiesMockedStatic.when(SwingUtilities::isEventDispatchThread).thenReturn(isEventDispatchThread);
             platformMockedStatic.when(Platform::isFxApplicationThread).thenReturn(isFxApplicationThread);
-            
+
             final String title = "TITLE";
             final Icon icon = mock(Icon.class);
             final String message = "MESSAGE";
-            
+
             NotifyDisplayer.display(title, icon, message);
-            
+
             verify(notificationDisplayer).notify(title, icon, message, null);
-            
+
             if (runThroughThread) {
                 completableFutureMockedStatic.verify(() -> CompletableFuture.runAsync(any(Runnable.class)));
             } else {
@@ -252,38 +248,43 @@ public class NotifyDisplayerNGTest {
             }
         }
     }
-    
+
     /**
      * Configurable test for the display notification method.
      *
-     * @param isEventDispatchThread true if the test is meant to be running in the event dispatch thread, false otherwise
-     * @param isFxApplicationThread true if the test is meant to be running in the fx application thread, false otherwise
-     * @param runThroughThread true if the call is meant to run through a separate thread first, false otherwise
+     * @param isEventDispatchThread true if the test is meant to be running in
+     * the event dispatch thread, false otherwise
+     * @param isFxApplicationThread true if the test is meant to be running in
+     * the fx application thread, false otherwise
+     * @param runThroughThread true if the call is meant to run through a
+     * separate thread first, false otherwise
      */
     private void display(final boolean isEventDispatchThread,
-                         final boolean isFxApplicationThread,
-                         final boolean runThroughThread) {
+            final boolean isFxApplicationThread,
+            final boolean runThroughThread) {
         try (
-                final MockedStatic<EventQueue> eventQueueMockedStatic = Mockito.mockStatic(EventQueue.class);
-                final MockedStatic<CompletableFuture> completableFutureMockedStatic = Mockito.mockStatic(CompletableFuture.class);
-                final MockedStatic<DialogDisplayer> dialogDisplayerMockedStatic = Mockito.mockStatic(DialogDisplayer.class);
-                final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic = Mockito.mockStatic(SwingUtilities.class);
-                final MockedStatic<Platform> platformMockedStatic = Mockito.mockStatic(Platform.class);
-        ) {
+                final MockedStatic<EventQueue> eventQueueMockedStatic = Mockito.mockStatic(EventQueue.class); final MockedStatic<CompletableFuture> completableFutureMockedStatic = Mockito.mockStatic(CompletableFuture.class); final MockedStatic<DialogDisplayer> dialogDisplayerMockedStatic = Mockito.mockStatic(DialogDisplayer.class); final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic = Mockito.mockStatic(SwingUtilities.class); final MockedStatic<Platform> platformMockedStatic = Mockito.mockStatic(Platform.class);) {
             setupThreadingMocks(eventQueueMockedStatic, completableFutureMockedStatic, swingUtilitiesMockedStatic, platformMockedStatic);
-            
+
             final DialogDisplayer dialogDisplayer = mock(DialogDisplayer.class);
             dialogDisplayerMockedStatic.when(DialogDisplayer::getDefault).thenReturn(dialogDisplayer);
-            
+
             swingUtilitiesMockedStatic.when(SwingUtilities::isEventDispatchThread).thenReturn(isEventDispatchThread);
             platformMockedStatic.when(Platform::isFxApplicationThread).thenReturn(isFxApplicationThread);
-            
-            final NotifyDescriptor descriptor = mock(NotifyDescriptor.class);
-            
+
+//            final NotifyDescriptor descriptor = mock(NotifyDescriptor.class);
+            final NotifyDescriptor descriptor = new NotifyDescriptor(
+                    "message",
+                    "title",
+                    NotifyDescriptor.DEFAULT_OPTION,
+                    NotifyDescriptor.ERROR_MESSAGE,
+                    new Object[]{NotifyDescriptor.OK_OPTION},
+                    NotifyDescriptor.OK_OPTION);
+
             NotifyDisplayer.display(descriptor);
-            
+
             verify(dialogDisplayer).notify(descriptor);
-            
+
             if (runThroughThread) {
                 completableFutureMockedStatic.verify(() -> CompletableFuture.runAsync(any(Runnable.class)));
             } else {
@@ -291,19 +292,20 @@ public class NotifyDisplayerNGTest {
             }
         }
     }
-    
+
     /**
      * Sets up common mocks to prevent a multi-threaded test from happening.
      *
      * @param eventQueueMockedStatic a static mock to {@link EventQueue}
-     * @param completableFutureMockedStatic a static mock to {@link CompletableFuture}
+     * @param completableFutureMockedStatic a static mock to
+     * {@link CompletableFuture}
      * @param swingUtilitiesMockedStatic a static mock to {@link SwingUtilities}
      * @param platformMockedStatic a static mock to {@link Platform}
      */
     private void setupThreadingMocks(final MockedStatic<EventQueue> eventQueueMockedStatic,
-                                     final MockedStatic<CompletableFuture> completableFutureMockedStatic,
-                                     final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic,
-                                     final MockedStatic<Platform> platformMockedStatic) {
+            final MockedStatic<CompletableFuture> completableFutureMockedStatic,
+            final MockedStatic<SwingUtilities> swingUtilitiesMockedStatic,
+            final MockedStatic<Platform> platformMockedStatic) {
         completableFutureMockedStatic.when(() -> CompletableFuture.runAsync(any(Runnable.class))).thenAnswer(
                 iom -> {
                     final Runnable runnable = iom.getArgument(0);
