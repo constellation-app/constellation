b7f362a3c67f6ddd5fc3916b38b9636ab9d8eeec
sol695510 <58677774+sol695510@users.noreply.github.com>
2022-07-08 00:05:26 +0930

    Update SupportPackageAction.java

--- CoreUtilities/src/au/gov/asd/tac/constellation/utilities/support/SupportPackageAction.java
+++ CoreUtilities/src/au/gov/asd/tac/constellation/utilities/support/SupportPackageAction.java
@@ -62,7 +62,6 @@ public final class SupportPackageAction implements ActionListener {
                     supportPackage.createSupportPackage(new File(SupportPackage.getUserLogDirectory()), destination);
                     NotifyDisplayer.display("Support package saved successfully to " + destination.getPath(), NotifyDescriptor.INFORMATION_MESSAGE);
                 } catch (IOException ex) {
-                    // ERROR HERE
                     NotifyDisplayer.display("Failed to save support package. The error was " + ex.getLocalizedMessage(), NotifyDescriptor.ERROR_MESSAGE);
                 }
             });
