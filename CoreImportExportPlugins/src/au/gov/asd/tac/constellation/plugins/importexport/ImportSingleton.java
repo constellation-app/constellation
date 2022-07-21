85cf448dc5f84f6e0a1671f569d22e8759966c87
altair1673 <altair1673lightyears@gmail.com>
2022-07-21 10:24:13 +0930

    Fixed Some More Code Smells

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/ImportSingleton.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/ImportSingleton.java
@@ -15,8 +15,9 @@
  */
 package au.gov.asd.tac.constellation.plugins.importexport;
 
-import javafx.beans.property.BooleanProperty;
-import org.reactfx.inhibeans.property.SimpleBooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 
 /**
  *
@@ -27,10 +28,10 @@ public class ImportSingleton {
     private static ImportSingleton instance = null;
 
     // Property that triggers the different run windows in the configuration panel to clear
-    private final BooleanProperty clearDataFlag = new SimpleBooleanProperty();
+    private final IntegerProperty clearDataFlag = new SimpleIntegerProperty();
 
     private ImportSingleton() {
-        clearDataFlag.set(false);
+        clearDataFlag.set(2);
     }
 
     public static synchronized ImportSingleton getDefault() {
@@ -43,17 +44,19 @@ public class ImportSingleton {
     /**
      * Returns the clearDataFlag
      *
-     * @Return
+     * @return
      */
-    public BooleanProperty getClearDataFlag() {
+    public IntegerProperty getClearDataFlag() {
         return clearDataFlag;
     }
 
     /**
-     * Sets the value of the clearDataFlag to be the opposite of its current
-     * value
+     * Sets the value of the clearDataFlag to be the either negative or positive
+     * based on its current state
      */
     public void triggerClearDataFlag() {
-        clearDataFlag.set(!clearDataFlag.get());
+        // Multiply by negative 1 everytime to change value from negative to postive
+        // and vice-versa
+        clearDataFlag.set(-1 * clearDataFlag.get());
     }
 }
