ed093ac40ad4166a61a433b892464a5df56de08d
Delphinus8821 <73083035+Delphinus8821@users.noreply.github.com>
2022-08-01 14:10:57 +0930

    Update LayerTitlePaneNGTest.java

--- CoreLayersView/test/unit/src/au/gov/asd/tac/constellation/views/layers/components/LayerTitlePaneNGTest.java
+++ CoreLayersView/test/unit/src/au/gov/asd/tac/constellation/views/layers/components/LayerTitlePaneNGTest.java
@@ -155,6 +155,33 @@ public class LayerTitlePaneNGTest {
         verify(spiedLtp).setSelected(Mockito.eq(true));
     }
 
-  
+    /**
+     * Test of setDescription method, of class LayerTitlePane.
+     *
+     * @throws java.lang.InterruptedException
+     */
+    @Test
+    public void testSetDescription() throws InterruptedException {
+        final int layerId = 2;
+        final String layerName = "layerName";
+        final String description = "description";
+        final BitMaskQuery query = new BitMaskQuery(new Query(GraphElementType.TRANSACTION, ""), layerId, "");
+        LayerTitlePane ltp = new LayerTitlePane(layerId, layerName, query);
+        LayerTitlePane spiedLtp = spy(ltp);
 
+        doCallRealMethod().when(spiedLtp).setDescription(Mockito.anyString());
+        spiedLtp.setDescription(description);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Platform.runLater(() -> {
+            System.out.println("Queued platform task for test");
+            latch.countDown();
+        });
+
+        latch.await();
+
+        verify(spiedLtp).setDescription(Mockito.eq(description));
+    }
+    
 }
