899c1c660e76da0af5cefb12947bb25850a7acd6
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2022-10-27 12:52:27 +1100

    Bugfix to Cascading Query Plugins * Running a CascadingQueryPlugin like through a workflow plugin would   not run some plugins that when calling "edit now"

--- CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/templates/CascadingQueryPlugin.java
+++ CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/templates/CascadingQueryPlugin.java
@@ -105,9 +105,9 @@ public abstract class CascadingQueryPlugin extends AbstractPlugin {
             try {
                 interaction.setProgress(0, 0, "Executing child plugins", true);
 
-                Map<Plugin, PluginParameters> childPlugins = getChildPlugins(parameters);
+                final Map<Plugin, PluginParameters> childPlugins = getChildPlugins(parameters);
 
-                PluginSynchronizer pluginSynchronizer = new PluginSynchronizer(childPlugins.size() + 1);
+                final PluginSynchronizer pluginSynchronizer = new PluginSynchronizer(childPlugins.size() + 1);
 
                 for (final Entry<Plugin, PluginParameters> entry : childPlugins.entrySet()) {
                     PluginExecution.withPlugin(entry.getKey()).withParameters(entry.getValue()).synchronizingOn(pluginSynchronizer).executeLater(graph);
@@ -177,26 +177,12 @@ public abstract class CascadingQueryPlugin extends AbstractPlugin {
 
             // Make the progress bar appear nondeterminent
             try {
-                interaction.setProgress(0, 0, "Reading...", true);
-                read(graph, interaction, parameters);
-                if (!"Reading".equals(interaction.getCurrentMessage())) {
-                    inControlOfProgress = false;
-                }
+                interaction.setProgress(0, 0, "Executing child plugins", true);
 
-                if (inControlOfProgress) {
-                    interaction.setProgress(0, 0, "Querying...", true);
-                }
-                query(interaction, parameters);
-                if (inControlOfProgress && !"Querying...".equals(interaction.getCurrentMessage())) {
-                    inControlOfProgress = false;
-                }
+                final Map<Plugin, PluginParameters> childPlugins = getChildPlugins(parameters);
 
-                if (inControlOfProgress) {
-                    interaction.setProgress(0, 0, "Editing...", true);
-                }
-                edit(graph, interaction, parameters);
-                if (inControlOfProgress && !"Editing...".equals(interaction.getCurrentMessage())) {
-                    inControlOfProgress = false;
+                for (final Entry<Plugin, PluginParameters> entry : childPlugins.entrySet()) {
+                    PluginExecution.withPlugin(entry.getKey()).withParameters(entry.getValue()).executeNow(graph);
                 }
             } finally {
                 interaction.setProgress(2, 1, inControlOfProgress ? "Finished" : interaction.getCurrentMessage(), true);
