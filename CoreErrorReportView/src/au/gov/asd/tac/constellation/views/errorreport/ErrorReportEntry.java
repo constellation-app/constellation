78a91d755273ec3c87b9908547002d27b70170c4
OrionsGuardian <104604488+OrionsGuardian@users.noreply.github.com>
2023-05-05 14:12:10 +0930

    New Error Report View (#1817)
    * initial checkin
    
    * update 2
    
    * adjust colours
    
    * Revised popup dialog, tab flash colour, red scaled reports, details in errors, and toolbar functions.
    
    * remove test code
    
    * update dependency version
    
    * dependency version updates
    
    * update dependency version
    
    * Fix unit test
    
    * code cleanup
    
    * tweaks
    
    * added severity level filter
    
    * Improved/updated controls, functionality, and stability
    
    * Updated dialog, added background functionality, code cleanup.
    
    * Code review updates
    
    * Code cleanup and added a What's New entry
    
    * More code review changes, added icons, minor updates.
    
    * code review changes, updated icons
    
    * updated icons
    
    * Unit testing
    
    * updated unit test
    
    * update unrelated unit test
    
    * unit tests
    
    * code refactor/cleanup
    
    * minor code cleanup
    
    * update unit test
    
    * wip unit test suite
    
    * update test
    
    * update unit test
    
    * updates
    
    * more unit test coverage
    
    * update test
    
    * update unit test
    
    * minor updates

---
+++ CoreErrorReportView/src/au/gov/asd/tac/constellation/views/errorreport/ErrorReportEntry.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2010-2023 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.views.errorreport;
+
+import java.util.logging.Level;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * This is a data class to store the exception stack-trace and maintain a count
+ * of occurrences
+ *
+ * @author OrionsGuardian
+ */
+public class ErrorReportEntry {
+
+    private Level errorLevel = null;
+    private String errorData = null;
+    private String heading = null;
+    private int occurrences = 0;
+    private Date lastDate = null;
+    private boolean expanded = true;
+    private double entryId = -1;
+    private Date lastPopupDate = null;
+    private boolean preventRepeatedPopups = false;
+    private String summaryHeading = null;
+    private ErrorReportDialog dialog = null;
+
+    public ErrorReportEntry(final Level errLevel, final String errorHeading, final String summary, final String errorMessage, final double id) {
+        errorLevel = errLevel;
+        heading = errorHeading;
+        summaryHeading = summary;
+        errorData = errorMessage;
+        entryId = id;
+        lastDate = new Date();
+        occurrences = 1;
+    }
+
+    public ErrorReportEntry copy() {
+        final ErrorReportEntry dataCopy = new ErrorReportEntry(errorLevel, heading, summaryHeading, errorData, entryId);
+        dataCopy.expanded = expanded;
+        dataCopy.lastDate = new Date(lastDate.getTime());
+        dataCopy.lastPopupDate = (lastPopupDate == null ? null : new Date(lastPopupDate.getTime()));
+        dataCopy.occurrences = occurrences;
+        dataCopy.preventRepeatedPopups = preventRepeatedPopups;
+        dataCopy.dialog = dialog;
+        return dataCopy;
+    }
+
+    public String getTrimmedHeading(final int length) {
+        if (heading == null) {
+            return "-NULL-";
+        }
+        if (heading.length() < length) {
+            return heading;
+        }
+        if (length <= 4) {
+            return heading.substring(0, 4);
+        }
+        return heading.substring(0, length - 4) + " ...";
+    }
+
+    public String getHeading() {
+        if (heading == null) {
+            return "-NULL-";
+        }
+        return heading;
+    }
+
+    public void setHeading(final String errorHeading) {
+        heading = errorHeading;
+    }
+
+    public String getSummaryHeading() {
+        return summaryHeading;
+    }
+
+    public void incrementOccurrences() {
+        occurrences++;
+        lastDate = new Date();
+    }
+
+    public String getErrorData() {
+        return errorData;
+    }
+
+    public int getOccurrences() {
+        return occurrences;
+    }
+
+    public Date getLastDate() {
+        if (lastDate == null) {
+            return null;
+        }
+        return new Date(lastDate.getTime());
+    }
+
+    public String getTimeText() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(" HH:mm:ss ");
+        return sdf.format(lastDate);
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public double getEntryId() {
+        return entryId;
+    }
+
+    public ErrorReportDialog getDialog() {
+        return dialog;
+    }
+    
+    public void setDialog(final ErrorReportDialog errorDialog) {
+        dialog = errorDialog;
+    }
+    
+    public void setEntryId(final double id) {
+        entryId = id;
+    }
+
+    public void setExpanded(final boolean expandedState) {
+        expanded = expandedState;
+    }
+
+    public Date getLastPopupDate() {
+        if (lastPopupDate == null) {
+            return null;
+        }
+        return new Date(lastPopupDate.getTime());
+    }
+
+    public void setLastPopupDate(final Date popupDate) {
+        if (popupDate == null) {
+            lastPopupDate = null;
+        } else {
+            lastPopupDate = new Date(popupDate.getTime());
+        }
+    }
+
+    public boolean isBlockRepeatedPopups() {
+        return preventRepeatedPopups;
+    }
+
+    public void setBlockRepeatedPopups(final boolean blockRepeatedPopups) {
+        preventRepeatedPopups = blockRepeatedPopups;
+    }
+
+    public Level getErrorLevel(){
+        return errorLevel;
+    }
+    
+    @Override
+    public String toString() {
+        return "[ErrorReportEntry:[id=" + entryId + "]"
+                + ", [header=" + heading + "]"
+                + ", [errorLevel=" + errorLevel.getName() + "]"
+                + ", [occurrences=" + occurrences + "]"
+                + ", [lastDate=" + lastDate + "]"
+                + ", [lastPopupDate=" + lastPopupDate + "]"
+                + ", [preventRepeatedPopups=" + preventRepeatedPopups + "]"
+                + ", [dialog=" + dialog + "]"
+                + "]";
+    }
+}
