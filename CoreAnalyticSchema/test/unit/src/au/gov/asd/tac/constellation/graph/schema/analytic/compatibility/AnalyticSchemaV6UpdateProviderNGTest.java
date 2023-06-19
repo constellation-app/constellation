534596a54ae12e8d4816dc9082bf320fbfb2515b
antares1470 <62311243+antares1470@users.noreply.github.com>
2023-06-19 12:15:39 +1000

    updated test with couple extra cases

--- CoreAnalyticSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/analytic/compatibility/AnalyticSchemaV6UpdateProviderNGTest.java
+++ CoreAnalyticSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/analytic/compatibility/AnalyticSchemaV6UpdateProviderNGTest.java
@@ -22,6 +22,7 @@ import au.gov.asd.tac.constellation.graph.schema.analytic.AnalyticSchemaFactory;
 import au.gov.asd.tac.constellation.graph.schema.analytic.concept.AnalyticConcept;
 import au.gov.asd.tac.constellation.graph.schema.type.SchemaVertexType;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -70,6 +71,8 @@ public class AnalyticSchemaV6UpdateProviderNGTest {
         final int vxId2 = graph.addVertex();
         final int vxId3 = graph.addVertex();
         final int vxId4 = graph.addVertex();
+        final int vxId5 = graph.addVertex();
+        final int vxId6 = graph.addVertex();
         
         final SchemaVertexType ipv4Custom = new SchemaVertexType.Builder("IPv4 Address")
                 .build();
@@ -79,21 +82,28 @@ public class AnalyticSchemaV6UpdateProviderNGTest {
                 .build();
         final SchemaVertexType mgrsCustom = new SchemaVertexType.Builder("MGRS")
                 .build();
+        final SchemaVertexType urlCustom = new SchemaVertexType.Builder("URL")
+                .build();
         
         graph.setObjectValue(typeAttributeId, vxId1, ipv4Custom);
         graph.setObjectValue(typeAttributeId, vxId2, ipv6Custom);
         graph.setObjectValue(typeAttributeId, vxId3, emailCustom);
         graph.setObjectValue(typeAttributeId, vxId4, mgrsCustom);
+        graph.setObjectValue(typeAttributeId, vxId5, urlCustom);
         
         final SchemaVertexType vxId1TypeBefore = graph.getObjectValue(typeAttributeId, vxId1);
         final SchemaVertexType vxId2TypeBefore = graph.getObjectValue(typeAttributeId, vxId2);
         final SchemaVertexType vxId3TypeBefore = graph.getObjectValue(typeAttributeId, vxId3);
         final SchemaVertexType vxId4TypeBefore = graph.getObjectValue(typeAttributeId, vxId4);
+        final SchemaVertexType vxId5TypeBefore = graph.getObjectValue(typeAttributeId, vxId5);
+        final SchemaVertexType vxId6TypeBefore = graph.getObjectValue(typeAttributeId, vxId6);
         
         assertFalse(AnalyticConcept.VertexType.IPV4.equals(vxId1TypeBefore));
         assertFalse(AnalyticConcept.VertexType.IPV6.equals(vxId2TypeBefore));
         assertFalse(AnalyticConcept.VertexType.EMAIL_ADDRESS.equals(vxId3TypeBefore));
         assertFalse(AnalyticConcept.VertexType.MGRS.equals(vxId4TypeBefore));
+        assertFalse(AnalyticConcept.VertexType.URL.equals(vxId5TypeBefore));
+        assertNull(vxId6TypeBefore);
         
         final AnalyticSchemaV6UpdateProvider instance = new AnalyticSchemaV6UpdateProvider();
         instance.schemaUpdate(graph);
@@ -102,10 +112,16 @@ public class AnalyticSchemaV6UpdateProviderNGTest {
         final SchemaVertexType vxId2TypeAfter = graph.getObjectValue(typeAttributeId, vxId2);
         final SchemaVertexType vxId3TypeAfter = graph.getObjectValue(typeAttributeId, vxId3);
         final SchemaVertexType vxId4TypeAfter = graph.getObjectValue(typeAttributeId, vxId4);
+        final SchemaVertexType vxId5TypeAfter = graph.getObjectValue(typeAttributeId, vxId5);
+        final SchemaVertexType vxId6TypeAfter = graph.getObjectValue(typeAttributeId, vxId6);
         
         assertTrue(AnalyticConcept.VertexType.IPV4.equals(vxId1TypeAfter));
         assertTrue(AnalyticConcept.VertexType.IPV6.equals(vxId2TypeAfter));
         assertTrue(AnalyticConcept.VertexType.EMAIL_ADDRESS.equals(vxId3TypeAfter));
         assertTrue(AnalyticConcept.VertexType.MGRS.equals(vxId4TypeAfter));
+        // still false as this update doesn't cover this type
+        assertFalse(AnalyticConcept.VertexType.URL.equals(vxId5TypeAfter));
+        // still null
+        assertNull(vxId6TypeAfter);
     }
 }
