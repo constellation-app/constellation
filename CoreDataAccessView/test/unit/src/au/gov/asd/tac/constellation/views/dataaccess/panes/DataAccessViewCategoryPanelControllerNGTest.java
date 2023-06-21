844c718bf0f38fe009cee75d7c94c99fdb6d21a1
altair1673 <altair1673lightyears@gmail.com>
2023-06-21 16:06:11 +0930

    Changed tests

--- CoreDataAccessView/test/unit/src/au/gov/asd/tac/constellation/views/dataaccess/panes/DataAccessViewCategoryPanelControllerNGTest.java
+++ CoreDataAccessView/test/unit/src/au/gov/asd/tac/constellation/views/dataaccess/panes/DataAccessViewCategoryPanelControllerNGTest.java
@@ -190,7 +190,7 @@ public class DataAccessViewCategoryPanelControllerNGTest {
         when(dataAccessViewCategoryPanelControllerSpy.getPanel()).thenReturn(dataAccessViewCategoryPanelSpy);
         when(dataAccessViewCategoryPanelSpy.getHiddenCategory()).thenReturn(list1);
 
-        boolean expResult = true;
+        boolean expResult = false;
         boolean result = dataAccessViewCategoryPanelControllerSpy.isChanged();
         assertEquals(result, expResult);
         verify(dataAccessViewCategoryPanelSpy, times(1)).getHiddenCategory();
