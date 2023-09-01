42b0a4dbe0837cea5d93a0c821d6df6f9fa7f152
capricornunicorn123 <capricornunicorn123@gmail.com>
2023-09-01 13:50:44 +0930

    Test failure source location experimentation

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/NotifyDisplayerNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/NotifyDisplayerNGTest.java
@@ -72,143 +72,143 @@ public class NotifyDisplayerNGTest {
         }
     }
 
-    @Test
-    public void display() {
-        display(true, true, true);
-        display(true, false, true);
-        display(false, true, true);
-        display(false, false, false);
-    }
-
-    @Test
-    public void displayWithIcon() {
-        displayWithIcon(true, true, true);
-        displayWithIcon(true, false, true);
-        displayWithIcon(false, true, true);
-        displayWithIcon(false, false, false);
-    }
-
-    @Test
-    public void displayAlert() {
-        final String title = "TITLE";
-        final String header = "HEADER";
-        final String message = "MESSAGE";
-        final Alert.AlertType alertType = Alert.AlertType.WARNING;
-
-        final DialogPane dialogPane = mock(DialogPane.class);
-        final Scene scene = mock(Scene.class);
-        final Stage stage = mock(Stage.class);
-
-        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
-                (mock, cnxt) -> {
-                    assertEquals(cnxt.arguments().get(0), alertType);
-                    assertEquals(cnxt.arguments().get(1), "");
-                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
-
-                    when(mock.getDialogPane()).thenReturn(dialogPane);
-                    when(dialogPane.getScene()).thenReturn(scene);
-                    when(scene.getWindow()).thenReturn(stage);
-                })) {
-            NotifyDisplayer.displayAlert(title, header, message, alertType);
-
-            assertEquals(alertMockedConstruction.constructed().size(), 1);
-
-            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-
-            verify(mockAlert).setTitle(title);
-            verify(mockAlert).setHeaderText(header);
-            verify(mockAlert).setContentText(message);
-
-            verify(mockAlert).setResizable(true);
-
-            verify(stage).setAlwaysOnTop(true);
-
-            verify(mockAlert).showAndWait();
-        }
-    }
-
-    @Test
-    public void displayLargeAlert() {
-        final String title = "TITLE";
-        final String header = "HEADER";
-        final String message = "MESSAGE";
-        final Alert.AlertType alertType = Alert.AlertType.WARNING;
-
-        final DialogPane dialogPane = mock(DialogPane.class);
-        final Scene scene = mock(Scene.class);
-        final Stage stage = mock(Stage.class);
-
-        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
-                (mock, cnxt) -> {
-                    assertEquals(cnxt.arguments().get(0), alertType);
-                    assertEquals(cnxt.arguments().get(1), "");
-                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
-
-                    when(mock.getDialogPane()).thenReturn(dialogPane);
-                    when(dialogPane.getScene()).thenReturn(scene);
-                    when(scene.getWindow()).thenReturn(stage);
-                })) {
-            NotifyDisplayer.displayLargeAlert(title, header, message, alertType);
-
-            assertEquals(alertMockedConstruction.constructed().size(), 1);
-
-            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-
-            verify(mockAlert).setTitle(title);
-            verify(mockAlert).setHeaderText(header);
-
-            final ArgumentCaptor<TextArea> captor = ArgumentCaptor.forClass(TextArea.class);
-            verify(dialogPane).setExpandableContent(captor.capture());
-
-            assertEquals(captor.getValue().getText(), message);
-            assertTrue(captor.getValue().isWrapText());
-            assertFalse(captor.getValue().isEditable());
-
-            verify(mockAlert).setResizable(true);
-
-            verify(stage).setAlwaysOnTop(true);
-
-            verify(mockAlert).showAndWait();
-        }
-    }
-
-    @Test
-    public void displayConfirmationAlert() {
-        final String title = "TITLE";
-        final String header = "HEADER";
-        final String message = "MESSAGE";
-
-        final DialogPane dialogPane = mock(DialogPane.class);
-        final Scene scene = mock(Scene.class);
-        final Stage stage = mock(Stage.class);
-
-        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
-                (mock, cnxt) -> {
-                    assertEquals(cnxt.arguments().get(0), Alert.AlertType.CONFIRMATION);
-                    assertEquals(cnxt.arguments().get(1), "");
-                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.NO, ButtonType.YES});
-
-                    when(mock.getDialogPane()).thenReturn(dialogPane);
-                    when(dialogPane.getScene()).thenReturn(scene);
-                    when(scene.getWindow()).thenReturn(stage);
-                })) {
-            NotifyDisplayer.displayConfirmationAlert(title, header, message);
-
-            assertEquals(alertMockedConstruction.constructed().size(), 1);
-
-            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
-
-            verify(mockAlert).setTitle(title);
-            verify(mockAlert).setHeaderText(header);
-            verify(mockAlert).setContentText(message);
-
-            verify(mockAlert).setResizable(true);
-
-            verify(stage).setAlwaysOnTop(true);
-
-            verify(mockAlert).showAndWait();
-        }
-    }
+//    @Test
+//    public void display() {
+//        display(true, true, true);
+//        display(true, false, true);
+//        display(false, true, true);
+//        display(false, false, false);
+//    }
+//
+//    @Test
+//    public void displayWithIcon() {
+//        displayWithIcon(true, true, true);
+//        displayWithIcon(true, false, true);
+//        displayWithIcon(false, true, true);
+//        displayWithIcon(false, false, false);
+//    }
+//
+//    @Test
+//    public void displayAlert() {
+//        final String title = "TITLE";
+//        final String header = "HEADER";
+//        final String message = "MESSAGE";
+//        final Alert.AlertType alertType = Alert.AlertType.WARNING;
+//
+//        final DialogPane dialogPane = mock(DialogPane.class);
+//        final Scene scene = mock(Scene.class);
+//        final Stage stage = mock(Stage.class);
+//
+//        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
+//                (mock, cnxt) -> {
+//                    assertEquals(cnxt.arguments().get(0), alertType);
+//                    assertEquals(cnxt.arguments().get(1), "");
+//                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
+//
+//                    when(mock.getDialogPane()).thenReturn(dialogPane);
+//                    when(dialogPane.getScene()).thenReturn(scene);
+//                    when(scene.getWindow()).thenReturn(stage);
+//                })) {
+//            NotifyDisplayer.displayAlert(title, header, message, alertType);
+//
+//            assertEquals(alertMockedConstruction.constructed().size(), 1);
+//
+//            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
+//
+//            verify(mockAlert).setTitle(title);
+//            verify(mockAlert).setHeaderText(header);
+//            verify(mockAlert).setContentText(message);
+//
+//            verify(mockAlert).setResizable(true);
+//
+//            verify(stage).setAlwaysOnTop(true);
+//
+//            verify(mockAlert).showAndWait();
+//        }
+//    }
+//
+//    @Test
+//    public void displayLargeAlert() {
+//        final String title = "TITLE";
+//        final String header = "HEADER";
+//        final String message = "MESSAGE";
+//        final Alert.AlertType alertType = Alert.AlertType.WARNING;
+//
+//        final DialogPane dialogPane = mock(DialogPane.class);
+//        final Scene scene = mock(Scene.class);
+//        final Stage stage = mock(Stage.class);
+//
+//        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
+//                (mock, cnxt) -> {
+//                    assertEquals(cnxt.arguments().get(0), alertType);
+//                    assertEquals(cnxt.arguments().get(1), "");
+//                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.OK});
+//
+//                    when(mock.getDialogPane()).thenReturn(dialogPane);
+//                    when(dialogPane.getScene()).thenReturn(scene);
+//                    when(scene.getWindow()).thenReturn(stage);
+//                })) {
+//            NotifyDisplayer.displayLargeAlert(title, header, message, alertType);
+//
+//            assertEquals(alertMockedConstruction.constructed().size(), 1);
+//
+//            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
+//
+//            verify(mockAlert).setTitle(title);
+//            verify(mockAlert).setHeaderText(header);
+//
+//            final ArgumentCaptor<TextArea> captor = ArgumentCaptor.forClass(TextArea.class);
+//            verify(dialogPane).setExpandableContent(captor.capture());
+//
+//            assertEquals(captor.getValue().getText(), message);
+//            assertTrue(captor.getValue().isWrapText());
+//            assertFalse(captor.getValue().isEditable());
+//
+//            verify(mockAlert).setResizable(true);
+//
+//            verify(stage).setAlwaysOnTop(true);
+//
+//            verify(mockAlert).showAndWait();
+//        }
+//    }
+//
+//    @Test
+//    public void displayConfirmationAlert() {
+//        final String title = "TITLE";
+//        final String header = "HEADER";
+//        final String message = "MESSAGE";
+//
+//        final DialogPane dialogPane = mock(DialogPane.class);
+//        final Scene scene = mock(Scene.class);
+//        final Stage stage = mock(Stage.class);
+//
+//        try (final MockedConstruction<Alert> alertMockedConstruction = Mockito.mockConstruction(Alert.class,
+//                (mock, cnxt) -> {
+//                    assertEquals(cnxt.arguments().get(0), Alert.AlertType.CONFIRMATION);
+//                    assertEquals(cnxt.arguments().get(1), "");
+//                    assertEquals(cnxt.arguments().get(2), new ButtonType[]{ButtonType.NO, ButtonType.YES});
+//
+//                    when(mock.getDialogPane()).thenReturn(dialogPane);
+//                    when(dialogPane.getScene()).thenReturn(scene);
+//                    when(scene.getWindow()).thenReturn(stage);
+//                })) {
+//            NotifyDisplayer.displayConfirmationAlert(title, header, message);
+//
+//            assertEquals(alertMockedConstruction.constructed().size(), 1);
+//
+//            final Alert mockAlert = alertMockedConstruction.constructed().get(0);
+//
+//            verify(mockAlert).setTitle(title);
+//            verify(mockAlert).setHeaderText(header);
+//            verify(mockAlert).setContentText(message);
+//
+//            verify(mockAlert).setResizable(true);
+//
+//            verify(stage).setAlwaysOnTop(true);
+//
+//            verify(mockAlert).showAndWait();
+//        }
+//    }
 
     /**
      * Configurable test for the display notification with icon method.
