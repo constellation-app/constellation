4ca13d04983b7b4beee2a610153de38ff6842f13
antares1470 <62311243+antares1470@users.noreply.github.com>
2023-09-07 10:52:14 +1000

    fixed some potential issues around data access category ordering (#1887)
    * fixed some potential issues around data access category ordering
    
    * resolved issue with blank list if dav hasn't been opened

--- CoreDataAccessView/src/au/gov/asd/tac/constellation/views/dataaccess/tasks/LookupPluginsTask.java
+++ CoreDataAccessView/src/au/gov/asd/tac/constellation/views/dataaccess/tasks/LookupPluginsTask.java
@@ -18,15 +18,16 @@ package au.gov.asd.tac.constellation.views.dataaccess.tasks;
 import au.gov.asd.tac.constellation.utilities.text.SeparatorConstants;
 import au.gov.asd.tac.constellation.views.dataaccess.panes.DataAccessViewPreferenceKeys;
 import au.gov.asd.tac.constellation.views.dataaccess.plugins.DataAccessPlugin;
+import au.gov.asd.tac.constellation.views.dataaccess.plugins.DataAccessPluginType;
 import au.gov.asd.tac.constellation.views.dataaccess.utilities.DataAccessUtilities;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import java.util.prefs.Preferences;
+import javafx.util.Pair;
 import org.apache.commons.lang3.StringUtils;
 import org.openide.util.NbPreferences;
-import javafx.util.Pair;
 
 /**
  * Looks up all available data access plugins and populates a map with them
@@ -38,8 +39,7 @@ public class LookupPluginsTask implements Supplier<Map<String, Pair<Integer, Lis
     private static final Preferences PREFS = NbPreferences.forModule(DataAccessViewPreferenceKeys.class);
     public static final String DAV_CATS = PREFS.get(DataAccessViewPreferenceKeys.HIDDEN_DA_VIEW, DataAccessViewPreferenceKeys.HIDDEN_DA_VIEW_DEFAULT);
     public static final String VISIBLE_CATS = PREFS.get(DataAccessViewPreferenceKeys.VISIBLE_DA_VIEW, DataAccessViewPreferenceKeys.HIDDEN_DA_VIEW_DEFAULT);
-
-
+    
     @Override
     public Map<String, Pair<Integer, List<DataAccessPlugin>>> get() {
         // Creates a map with the key set being every available data access plugin type.
@@ -62,7 +62,7 @@ public class LookupPluginsTask implements Supplier<Map<String, Pair<Integer, Lis
             if (visibleCategoriesArray.length > 0) {
                 plugins.entrySet().forEach(entry -> {
                     for (int i = 0; i < visibleCategoriesArray.length; i++) {
-                        if (entry.getKey().equals(visibleCategoriesArray[i])) {
+                        if (entry.getKey().equals(visibleCategoriesArray[i].trim())) {
                             final Pair<Integer, List<DataAccessPlugin>> p = new Pair<>(i, entry.getValue());
                             pluginsWithOrder.put(entry.getKey(), p);
                             break;
@@ -71,7 +71,16 @@ public class LookupPluginsTask implements Supplier<Map<String, Pair<Integer, Lis
                 });
             }
         } else if (StringUtils.isBlank(DAV_CATS)) {
-            plugins.entrySet().forEach(entry -> pluginsWithOrder.put(entry.getKey(), new Pair(0, entry.getValue())));
+            plugins.entrySet().forEach(entry -> {
+                int position = Integer.MAX_VALUE;
+                for (final String pluginType : DataAccessPluginType.getTypeWithPosition().keySet()) {
+                    if (pluginType.equals(entry.getKey())) {
+                        position = DataAccessPluginType.getTypeWithPosition().get(pluginType);
+                    }
+                }
+
+                pluginsWithOrder.put(entry.getKey(), new Pair(position, entry.getValue()));
+            });
         }
 
         return pluginsWithOrder;
@@ -81,7 +90,7 @@ public class LookupPluginsTask implements Supplier<Map<String, Pair<Integer, Lis
         if (StringUtils.isNotBlank(categories)) {
             final String hiddenCategory = categories.replace("[", "");
             final String hiddenCategoryFinal = hiddenCategory.replace("]", "");
-            return hiddenCategoryFinal.replace(" ", "").split(SeparatorConstants.COMMA);
+            return hiddenCategoryFinal.split(SeparatorConstants.COMMA);
         }
         return new String[0];
     }
