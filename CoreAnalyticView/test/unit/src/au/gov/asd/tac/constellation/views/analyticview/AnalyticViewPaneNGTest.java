7644800ed2dc8916a3209dac586446fbdb7d2bbe
Delphinus8821 <73083035+Delphinus8821@users.noreply.github.com>
2023-11-17 13:46:20 +1030

    Update Analytic View State and Results Buttons (#1911)
    * Progress update on Analytic State
    
    * State update
    
    * Update state and code tidy
    
    * Fixed state bug when switching graphs
    
    * Update to state
    
    * Fix state updating bug
    
    * Updates to IO provider and size bug
    
    * Updates to the state
    
    * Fix null pointer
    
    * Fix state and buttons
    
    Size button still needs to be fixed when swapping between graphs
    
    * State update
    
    * Fix size bug + code cleanup
    
    * Fix code smells
    
    * Fix code smells
    
    * Remove unused class and fix size bug
    
    * Update copyright for Core Analytic View
    
    * Adding the IOProvider back
    
    * Requested changes
    
    * Requested changes
    
    * Update toggle plugins
    
    * Update every graph when the analytic view is closed
    
    * Code cleanup
    
    * Testing update
    
    * Update tests
    
    * Testing updates
    
    * Test unit tests
    
    * Updated tests
    
    * Update tests
    
    * Testing updates
    
    * Code smell fixes and more tests
    
    * Update to controller test
    
    * Checking test coverage
    
    * Update tests
    
    * Fix unit tests
    
    * Run CI again
    
    * Fix unit test
    
    * Fix unit tests
    
    * Update to tests
    
    * Requested changes
    
    * Requested changes

---
+++ CoreAnalyticView/test/unit/src/au/gov/asd/tac/constellation/views/analyticview/AnalyticViewPaneNGTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2010-2023 Australian Signals Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.views.analyticview;
+
+import au.gov.asd.tac.constellation.views.analyticview.visualisation.GraphVisualisation;
+import au.gov.asd.tac.constellation.views.analyticview.visualisation.SizeVisualisation;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.testfx.api.FxToolkit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Test class for AnalyticVuewPane 
+ * 
+ * @author Delphinus8821
+ */
+public class AnalyticViewPaneNGTest {
+
+    private static final Logger LOGGER = Logger.getLogger(AnalyticViewPaneNGTest.class.getName());
+    
+    public AnalyticViewPaneNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        if (!FxToolkit.isFXApplicationThreadRunning()) {
+            FxToolkit.registerPrimaryStage();
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        try {
+            FxToolkit.cleanupStages();
+        } catch (TimeoutException ex) {
+            LOGGER.log(Level.WARNING, "FxToolkit timedout trying to cleanup stages", ex);
+        }
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of deactiveResultChanges method, of class AnalyticViewPane.
+     */
+    @Test
+    public void testDeactiveResultChanges() {
+        System.out.println("deactiveResultChanges");
+
+        try (final MockedStatic<AnalyticViewController> controllerStatic = Mockito.mockStatic(AnalyticViewController.class)) {
+            final AnalyticViewController controller = spy(AnalyticViewController.class);
+            controllerStatic.when(AnalyticViewController::getDefault).thenReturn(controller);
+            final AnalyticViewPane instance = new AnalyticViewPane(controller);
+
+            final Map<GraphVisualisation, Boolean> newVisualisations = new HashMap<>();
+            final SizeVisualisation sizeVisualisation = mock(SizeVisualisation.class);
+            newVisualisations.put(sizeVisualisation, true);
+            when(controller.getGraphVisualisations()).thenReturn(newVisualisations);
+
+            instance.deactiveResultChanges();
+            verify(sizeVisualisation).deactivate(true);
+        }
+    }
+}
