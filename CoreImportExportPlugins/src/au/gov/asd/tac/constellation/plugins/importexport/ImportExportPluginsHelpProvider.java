59d5ce1883d1b76c0fef00876e4e4d6d5de64715
capricornunicorn123 <capricornunicorn123@gmail.com>
2024-01-22 11:01:33 +1030

    Implement review changes

--- CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/ImportExportPluginsHelpProvider.java
+++ CoreImportExportPlugins/src/au/gov/asd/tac/constellation/plugins/importexport/ImportExportPluginsHelpProvider.java
@@ -52,9 +52,9 @@ public class ImportExportPluginsHelpProvider extends HelpPageProvider {
         //Export Plugins
         map.put("au.gov.asd.tac.constellation.plugins.importexport.export", importExportModulePath + "export-from-constellation.md");
         map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToGeoJsonPlugin", importExportModulePath + "export-from-constellation.md");
-        map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial..ExportToImagePlugin", importExportModulePath + "export-from-constellation.md");
+        map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToImagePlugin", importExportModulePath + "export-from-constellation.md");
         map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToJsonPlugin", importExportModulePath + "export-from-constellation.md");
-        map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial..ExportToGeoPackagePlugin", importExportModulePath + "export-from-constellation.md");
+        map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToGeoPackagePlugin", importExportModulePath + "export-from-constellation.md");
         map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToShapefilePlugin", importExportModulePath + "export-from-constellation.md");
         map.put("au.gov.asd.tac.constellation.plugins.importexport.geospatial.ExportToKmlPlugin", importExportModulePath + "export-from-constellation.md");
         
