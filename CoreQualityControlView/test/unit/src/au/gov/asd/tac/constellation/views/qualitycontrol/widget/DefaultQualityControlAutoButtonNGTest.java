6e5fac09ccaa3d213292b930489491824de871e7
capricornunicorn123 <capricornunicorn123@gmail.com>
2024-03-07 10:25:54 +1030

    Revert "Attempt to trigger online test failure"
    This reverts commit 43523054f8ca82b2f1e9fa4379f73147d2590359.

--- CoreQualityControlView/test/unit/src/au/gov/asd/tac/constellation/views/qualitycontrol/widget/DefaultQualityControlAutoButtonNGTest.java
+++ CoreQualityControlView/test/unit/src/au/gov/asd/tac/constellation/views/qualitycontrol/widget/DefaultQualityControlAutoButtonNGTest.java
@@ -69,12 +69,12 @@ public class DefaultQualityControlAutoButtonNGTest {
         // My guess is that there is a test generating these files and not cleaning
         // up which is why this test is consistently failing when run on CI. Its that
         // file cleanup that should be fixed!!!
-//        Arrays.stream(AutosaveUtilities.getAutosaves(FileExtensionConstants.STAR_AUTOSAVE))
-//                .forEach(file -> file.delete());
-//
-//        if (!FxToolkit.isFXApplicationThreadRunning()) {
-//            FxToolkit.registerPrimaryStage();
-//        }
+        Arrays.stream(AutosaveUtilities.getAutosaves(FileExtensionConstants.STAR_AUTOSAVE))
+                .forEach(file -> file.delete());
+
+        if (!FxToolkit.isFXApplicationThreadRunning()) {
+            FxToolkit.registerPrimaryStage();
+        }
     }
 
     @AfterClass
