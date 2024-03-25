47c9947e7bd1d0060208d4b37289d7e71c503285
Quasar985 <quasar985@gmail.com>
2024-03-26 10:08:46 +1030

    Remove commented code

--- CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/utilities/NewNotePane.java
+++ CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/utilities/NewNotePane.java
@@ -66,7 +66,6 @@ import javax.swing.WindowConstants;
 public class NewNotePane {
 
     private boolean isFirstTime = true;
-    //private final Pane dialogPane;
     private final BorderPane dialogPane;
     private static final String FONT_SIZE_STRING = "-fx-font-size:%d;";
     private final String fontStyle = String.format(FONT_SIZE_STRING, FontUtilities.getApplicationFontSize());
@@ -104,11 +103,6 @@ public class NewNotePane {
     public NewNotePane(final String userChosenColour) {
         this.userChosenColour = userChosenColour;
 
-//        dialogPane = new Pane();
-//        dialogPane.setMinHeight(HEIGHT);
-//        dialogPane.setMaxHeight(HEIGHT);
-//        dialogPane.setMinWidth(WIDTH);
-//        dialogPane.setMaxWidth(WIDTH);
         dialogPane = new BorderPane();
         dialogPane.setPrefHeight(HEIGHT);
         dialogPane.setPrefWidth(WIDTH);
@@ -203,11 +197,6 @@ public class NewNotePane {
         noteHBox.setAlignment(Pos.CENTER_RIGHT);
 
         final HBox cbHBox = new HBox(30, applyToSelection, enableMarkdown);
-        //final VBox addNoteVBox = new VBox(5, tabPane, cbHBox, noteHBox);
-
-//        addNoteVBox.setAlignment(Pos.CENTER_LEFT);
-//        addNoteVBox.setStyle(fontStyle + "-fx-padding: 5px;");
-//        addNoteVBox.setMinHeight(220);
 
         final VBox paramVBox = new VBox(cbHBox, noteHBox);
         paramVBox.setStyle(fontStyle + "-fx-padding: 5px;");
@@ -261,7 +250,6 @@ public class NewNotePane {
             stage.setTitle("Create new note");
             stage.setMinHeight(HEIGHT);
             stage.setMinWidth(WIDTH);
-            //stage.setResizable(false);
             stage.setResizable(true);
 
             final Scene s = new Scene(dialogPane);
