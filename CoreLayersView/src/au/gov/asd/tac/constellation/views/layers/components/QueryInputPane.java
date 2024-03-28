ff2aa3d968c20b1aa3085f9d91249d402a00b9e7
Quasar985 <quasar985@gmail.com>
2024-03-28 12:59:01 +1030

    Update copyright and update few lines of code

--- CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/components/QueryInputPane.java
+++ CoreLayersView/src/au/gov/asd/tac/constellation/views/layers/components/QueryInputPane.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 Australian Signals Directorate
+ * Copyright 2010-2024 Australian Signals Directorate
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -235,7 +235,7 @@ public class QueryInputPane extends HBox implements RecentValuesListener {
     }
     
     public String getQuery() {
-        return field.getText() == "" ? null : field.getText();
+        return field.getText().equals("") ? null : field.getText();
     }
     
     public void setQuery(final String query) {
