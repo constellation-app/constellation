3663c99d69d46e67064b11191d7777ee962a8f1a
Quasar985 <quasar985@gmail.com>
2024-04-11 09:04:41 +0930

    Remove unused imports

--- CoreTableView/src/au/gov/asd/tac/constellation/views/tableview/components/ColumnVisibilityContextMenu.java
+++ CoreTableView/src/au/gov/asd/tac/constellation/views/tableview/components/ColumnVisibilityContextMenu.java
@@ -33,8 +33,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
