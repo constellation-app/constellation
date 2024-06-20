31e05986880e9e1c157f92739555179898b8abad
antares1470 <62311243+antares1470@users.noreply.github.com>
2024-06-20 12:07:22 +1000

    updated value input pane

--- CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/gui/ValueInputPane.java
+++ CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/gui/ValueInputPane.java
@@ -21,6 +21,7 @@ import au.gov.asd.tac.constellation.plugins.parameters.RecentValuesChangeEvent;
 import au.gov.asd.tac.constellation.plugins.parameters.RecentValuesListener;
 import au.gov.asd.tac.constellation.plugins.parameters.types.StringParameterType;
 import au.gov.asd.tac.constellation.utilities.text.SeparatorConstants;
+import au.gov.asd.tac.constellation.utilities.text.SpellCheckingTextArea;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
@@ -33,16 +34,12 @@ import javafx.scene.control.IndexRange;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import org.apache.commons.lang3.StringUtils;
-import org.controlsfx.control.textfield.TextFields;
 
 /**
  * A text box allowing entry of single line text, multiple line text
@@ -66,7 +63,7 @@ public class ValueInputPane extends HBox implements RecentValuesListener {
 
     private final ChangeListener<Number> recentValueSelectionListener;
     private final ComboBox<String> recentValuesCombo;
-    private final TextInputControl field;
+    private final SpellCheckingTextArea field;
     private final String parameterId;
     private final boolean required;
     private int comboBoxWidth = EMPTY_WIDTH;
@@ -168,14 +165,19 @@ public class ValueInputPane extends HBox implements RecentValuesListener {
                 };
             });
 
+            field = new SpellCheckingTextArea(parameter.isSpellCheckEnabled());
             if (suggestedHeight > 1) {
-                field = new TextArea();
-                ((TextArea) field).setWrapText(true);
-                ((TextArea) field).setPrefRowCount(suggestedHeight);
+                field.setWrapText(true);
             } else {
-                field = new TextField();
-                Platform.runLater(() -> TextFields.bindAutoCompletion((TextField) field, recentValuesCombo.getItems()));
+                field.autoComplete(recentValuesCombo.getItems());
             }
+            
+            Platform.runLater(() -> {
+                final Text t = (Text) field.lookup(".text");
+                if (t != null) {
+                    field.setPrefHeight(numberOfLines * t.getBoundsInLocal().getHeight() + field.EXTRA_HEIGHT);
+                }
+            });
 
             field.setPrefWidth(defaultWidth);
             field.setPromptText(parameter.getDescription());
@@ -209,41 +211,6 @@ public class ValueInputPane extends HBox implements RecentValuesListener {
                 recentValuesCombo.setDisable(!parameter.isEnabled());
             }
             
-            field.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-                if (event.getCode() == KeyCode.DELETE) {
-                    final IndexRange selection = field.getSelection();
-                    if (selection.getLength() == 0) {
-                        field.deleteNextChar();
-                    } else {
-                        field.deleteText(selection);
-                    }
-                    event.consume();
-                } else if (event.isShortcutDown() && event.isShiftDown() && (event.getCode() == KeyCode.RIGHT)) {
-                    field.selectNextWord();
-                    event.consume();
-                } else if (event.isShortcutDown() && event.isShiftDown() && (event.getCode() == KeyCode.LEFT)) {
-                    field.selectPreviousWord();
-                    event.consume();
-                } else if (event.isShortcutDown() && (event.getCode() == KeyCode.RIGHT)) {
-                    field.nextWord();
-                    event.consume();
-                } else if (event.isShortcutDown() && (event.getCode() == KeyCode.LEFT)) {
-                    field.previousWord();
-                    event.consume();
-                } else if (event.isShiftDown() && (event.getCode() == KeyCode.RIGHT)) {
-                    field.selectForward();
-                    event.consume();
-                } else if (event.isShiftDown() && (event.getCode() == KeyCode.LEFT)) {
-                    field.selectBackward();
-                    event.consume();
-                } else if (event.isShortcutDown() && (event.getCode() == KeyCode.A)) {
-                    field.selectAll();
-                    event.consume();
-                } else if (event.getCode() == KeyCode.ESCAPE) {
-                    event.consume();
-                }
-            });
-
             final Tooltip tooltip = new Tooltip("");
             tooltip.setStyle("-fx-text-fill: white;");
             field.textProperty().addListener((ov, t, t1) -> {
