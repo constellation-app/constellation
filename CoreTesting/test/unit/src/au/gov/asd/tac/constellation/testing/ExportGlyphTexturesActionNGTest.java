55a7e6ded5ff4a07bbbf180fb1d77c39c4fd131e
Quasar985 <quasar985@gmail.com>
2024-07-05 09:05:38 +0930

    Fix tests in ExportGlyphTexturesActionNGTest

--- CoreTesting/test/unit/src/au/gov/asd/tac/constellation/testing/ExportGlyphTexturesActionNGTest.java
+++ CoreTesting/test/unit/src/au/gov/asd/tac/constellation/testing/ExportGlyphTexturesActionNGTest.java
@@ -70,7 +70,15 @@ public class ExportGlyphTexturesActionNGTest {
                 .thenCallRealMethod();
         
         fileChooserStaticMock.when(()
+                -> FileChooser.createFileChooserBuilder(any(String.class), any(String.class)))
+                .thenCallRealMethod();
+        
+        fileChooserStaticMock.when(()
                 -> FileChooser.createFileChooserBuilder(any(String.class), any(String.class), any(String.class)))
+                .thenCallRealMethod();
+        
+        fileChooserStaticMock.when(()
+                -> FileChooser.createFileChooserBuilder(any(String.class), any(String.class), any(String.class), any(Boolean.class)))
                 .thenCallRealMethod();
         
         sharedDrawableStaticMock = mockStatic(SharedDrawable.class);
@@ -95,6 +103,9 @@ public class ExportGlyphTexturesActionNGTest {
         final ActionEvent e = null;
         final File file = new File("test.png");
         final Optional<File> optionalFile = Optional.ofNullable(file);
+        
+        System.out.println("is optional file null");
+        System.out.println(optionalFile == null);
 
         fileChooserStaticMock.when(()
                 -> FileChooser.openSaveDialog(any(FileChooserBuilder.class)))
