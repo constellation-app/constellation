6cab77e282c83827157145a571e6be982209a1c3
OrionsGuardian <oriondominus@gmail.com>
2024-07-15 14:14:26 +0930

    Update unit test

--- CoreHelp/test/unit/src/au/gov/asd/tac/constellation/help/utilities/GeneratorNGTest.java
+++ CoreHelp/test/unit/src/au/gov/asd/tac/constellation/help/utilities/GeneratorNGTest.java
@@ -85,6 +85,7 @@ public class GeneratorNGTest {
 
                 tocXMLFiles.add(new File(System.getProperty("user.dir") + layersTOC));
                 tocXMLFiles.add(new File(System.getProperty("user.dir") + notesTOC));
+                tocXMLFiles.add(new File("/non_consty_path/missing_file.md"));
                 generatorStaticMock.when(() -> Generator.getXMLFiles(Mockito.any())).thenReturn(tocXMLFiles);
                 generatorStaticMock.when(() -> Generator.getBaseDirectory()).thenCallRealMethod();
                 generatorStaticMock.when(() -> Generator.getResource()).thenCallRealMethod();
