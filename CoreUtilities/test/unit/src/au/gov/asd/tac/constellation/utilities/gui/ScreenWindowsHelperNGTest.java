04b1d1a6111a008f3018606e0e9f6945bd243cd0
andromeda-224 <ameda31224@gmail.com>
2024-08-27 11:27:46 +0930

    Update ScreenWindowsHelperNGTest.java

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/ScreenWindowsHelperNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/gui/ScreenWindowsHelperNGTest.java
@@ -1,18 +1,3 @@
-package au.gov.asd.tac.constellation.utilities.gui;
-
-
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Window;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 /*
  * Copyright 2010-2024 Australian Signals Directorate
  *
@@ -28,65 +13,84 @@ import org.testng.annotations.Test;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package au.gov.asd.tac.constellation.utilities.gui;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Window;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  *
  * @author Andromeda-224
  */
 public class ScreenWindowsHelperNGTest {
-    static final Logger LOGGER = Logger.getLogger(ScreenWindowsHelperNGTest.class.getName());
-        
-    /**
-     * Test getMainWindowCentrePoint method of class ScreenWindowsHelper.
-     */
+
+    final int testPointXY = 10;
+    final int testSizeWidthHeight = 100;
+
     @Test
-    public void testGetMainWindowCentrePoint() {
-        //Test method with no windows
+    public void testGetMainWindowCentrePointNull() {
+        //Test with no windows, should return null
         Assert.assertTrue(ScreenWindowsHelper.getMainWindowCentrePoint() == null);
-        
-        // Test method with mocked Window
+    }
+
+    @Test
+    public void testGetMainWindowCentrePointMainWindow() {
+
+        // Test method with mocked Window and dimensions
         final Window returnedWindow = mock(Window.class);
-        final int testPointXY = 10;
-        final int testSizeWidthHeight = 100;
         final Dimension testDimension = new Dimension(testSizeWidthHeight,
                 testSizeWidthHeight);
-        
-        when(returnedWindow.getName()).thenReturn("TestMainWindow");
+
+        when(returnedWindow.getName()).thenReturn("MainWindow");
         when(returnedWindow.getX()).thenReturn(testPointXY);
         when(returnedWindow.getY()).thenReturn(testPointXY);
         when(returnedWindow.getSize()).thenReturn(testDimension);
 
         try (final MockedStatic<Window> mockedWindowStatic = Mockito.mockStatic(Window.class)) {
-            mockedWindowStatic.when(Window::getWindows).thenReturn(new Window[] {returnedWindow});
-            
+            mockedWindowStatic.when(Window::getWindows).thenReturn(new Window[]{returnedWindow});
+
             Point testReturnedPoint = ScreenWindowsHelper.getMainWindowCentrePoint();
-            Assert.assertEquals((int)testReturnedPoint.getX(), testPointXY + (testSizeWidthHeight / 2));
-            Assert.assertEquals((int)testReturnedPoint.getY(), testPointXY + (testSizeWidthHeight / 2));
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "\n******* ERROR RUNNING TEST *******\n", e);
-            Assert.fail();
+            Assert.assertEquals((int) testReturnedPoint.getX(), testPointXY + (testSizeWidthHeight / 2));
+            Assert.assertEquals((int) testReturnedPoint.getY(), testPointXY + (testSizeWidthHeight / 2));
         }
-        
-        // test the case when it's not main window
+    }
+
+    @Test
+    public void testGetMainWindowCentrePointNotMainWindow() {
+
+        // test the case when it's not main window, should return null
+        final Window returnedWindow = mock(Window.class);
+        final Dimension testDimension = new Dimension(testSizeWidthHeight,
+                testSizeWidthHeight);
+
+        when(returnedWindow.getX()).thenReturn(testPointXY);
+        when(returnedWindow.getY()).thenReturn(testPointXY);
+        when(returnedWindow.getSize()).thenReturn(testDimension);
         when(returnedWindow.getName()).thenReturn("TestWindow");
-        
+
         try (final MockedStatic<Window> mockedWindowStatic = Mockito.mockStatic(Window.class)) {
-            mockedWindowStatic.when(Window::getWindows).thenReturn(new Window[] {returnedWindow});
-            
+            mockedWindowStatic.when(Window::getWindows).thenReturn(new Window[]{returnedWindow});
+
             Point testReturnedPoint = ScreenWindowsHelper.getMainWindowCentrePoint();
             Assert.assertEquals(testReturnedPoint, null);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "\n******* ERROR RUNNING TEST *******\n", e);
-            Assert.fail();
         }
-                
+    }
+
+    @Test
+    public void testGetMainWindowCentrePointMockedPoint() {
+
         // Test method with mocked point
         final Point point = mock(Point.class);
         try (final MockedStatic<ScreenWindowsHelper> screenWindowsHelperStatic = Mockito.mockStatic(ScreenWindowsHelper.class)) {
             screenWindowsHelperStatic.when(ScreenWindowsHelper::getMainWindowCentrePoint).thenReturn(point);
             Assert.assertEquals(ScreenWindowsHelper.getMainWindowCentrePoint(), point);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "\n******* ERROR RUNNING TEST *******\n", e);
-            Assert.fail();
         }
     }
 }
