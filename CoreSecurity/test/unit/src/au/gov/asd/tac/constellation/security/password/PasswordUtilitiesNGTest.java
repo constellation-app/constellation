661568855233bf2eec942fb09a232345d2f5ea4b
Quasar985 <quasar985@gmail.com>
2024-09-20 11:25:57 +0930

    Fix code smells and add tests

---
+++ CoreSecurity/test/unit/src/au/gov/asd/tac/constellation/security/password/PasswordUtilitiesNGTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2010-2024 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.security.password;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.openide.util.Lookup;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Quasar985
+ */
+public class PasswordUtilitiesNGTest {
+
+    /**
+     * Test of getIV method, of class PasswordUtilities.
+     */
+    @Test
+    public void testGetIV() {
+        System.out.println("getIV");
+
+        // Exception can only be tested first
+        final Lookup mockLookup = mock(Lookup.class);
+        when(mockLookup.lookup(PasswordSecret.class)).thenReturn(null);
+        try (final MockedStatic<Lookup> mockLookupStatic = Mockito.mockStatic(Lookup.class)) {
+            // Setup static mock
+            mockLookupStatic.when(Lookup::getDefault).thenReturn(mockLookup);
+
+            PasswordUtilities.getIV();
+            // If exception was not thrown, fail
+            fail("NoSuchElementException should have been thrown");
+        } catch (NoSuchElementException e) {
+            // Left blank
+        }
+
+        final byte[] firstResult = PasswordUtilities.getIV();
+        assertNotNull(firstResult);
+        final byte[] secondResult = PasswordUtilities.getIV();
+        assertEquals(firstResult, secondResult);
+    }
+
+    /**
+     * Test of getKey method, of class PasswordUtilities.
+     */
+    @Test
+    public void testGetKey() {
+        System.out.println("getKey");
+
+        // Exception can only be tested first
+        final Lookup mockLookup = mock(Lookup.class);
+        when(mockLookup.lookup(PasswordSecret.class)).thenReturn(null);
+        try (final MockedStatic<Lookup> mockLookupStatic = Mockito.mockStatic(Lookup.class)) {
+            // Setup static mock
+            mockLookupStatic.when(Lookup::getDefault).thenReturn(mockLookup);
+
+            PasswordUtilities.getKey();
+            // If exception was not thrown, fail
+            fail("NoSuchElementException should have been thrown");
+        } catch (NoSuchElementException e) {
+            // Left blank
+        }
+
+        final byte[] firstResult = PasswordUtilities.getKey();
+        assertNotNull(firstResult);
+        final byte[] secondResult = PasswordUtilities.getKey();
+        assertEquals(firstResult, secondResult);
+    }
+
+    /**
+     * Test of generateKey method, of class PasswordUtilities.
+     */
+    @Test
+    public void testGenerateKey() {
+        System.out.println("generateKey");
+        try {
+            final byte[] result = PasswordUtilities.generateKey();
+            assertNotNull(result);
+        } catch (NoSuchAlgorithmException e) {
+            fail("NoSuchAlgorithmException exception");
+        }
+
+    }
+}
