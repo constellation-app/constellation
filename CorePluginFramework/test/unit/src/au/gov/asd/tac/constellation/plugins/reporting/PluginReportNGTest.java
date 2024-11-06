a3054a2d15ff2f177848aa25a672a6fa8e52f941
andromeda-224 <ameda31224@gmail.com>
2024-11-06 12:05:25 +1030

    Update PluginReportNGTest.java

--- CorePluginFramework/test/unit/src/au/gov/asd/tac/constellation/plugins/reporting/PluginReportNGTest.java
+++ CorePluginFramework/test/unit/src/au/gov/asd/tac/constellation/plugins/reporting/PluginReportNGTest.java
@@ -18,16 +18,18 @@ package au.gov.asd.tac.constellation.plugins.reporting;
 import au.gov.asd.tac.constellation.graph.Graph;
 import au.gov.asd.tac.constellation.graph.locking.DualGraph;
 import au.gov.asd.tac.constellation.plugins.Plugin;
+import au.gov.asd.tac.constellation.plugins.PluginException;
 import au.gov.asd.tac.constellation.plugins.PluginGraphs;
 import au.gov.asd.tac.constellation.plugins.PluginInteraction;
 import au.gov.asd.tac.constellation.plugins.parameters.PluginParameters;
 import au.gov.asd.tac.constellation.plugins.templates.PluginTags;
 import au.gov.asd.tac.constellation.plugins.templates.SimplePlugin;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -42,21 +44,11 @@ public class PluginReportNGTest {
 
     private Plugin plugin;
     private Graph graph;
+    private long startTimeMillis;
     private static final String PLUGIN_TEST_NAME = "Test";
     private static final String PLUGIN_TEST_DESCRIPTION = "DESCRIPTION";
     private static final String PLUGIN_TEST_MESSAGE = "TEST MESSAGE";
     
-    public PluginReportNGTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
     @BeforeMethod
     public void setUpMethod() throws Exception {
         plugin = new TestPlugin();
@@ -64,10 +56,7 @@ public class PluginReportNGTest {
         graphReport = new GraphReport(graph.getId());
 
         report = new PluginReport(graphReport, plugin);
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
+        startTimeMillis = System.currentTimeMillis();
     }
     
     @Test
@@ -89,6 +78,7 @@ public class PluginReportNGTest {
     public void testAddMessage() {
         report.addMessage(PLUGIN_TEST_MESSAGE);
         assertEquals(report.getLastMessage(), PLUGIN_TEST_MESSAGE);
+        assertEquals(report.getAllMessages(), PLUGIN_TEST_MESSAGE);
     }
     
     @Test
@@ -110,8 +100,85 @@ public class PluginReportNGTest {
         assertEquals(Arrays.toString(report.getTags()).contains(PluginTags.SEARCH), true);
         
         assertEquals(report.hasLowLevelTag(), false);
+        final List<String> filteredTags = Arrays.asList(PluginTags.ANALYTIC, 
+                PluginTags.GENERAL,
+                PluginTags.SEARCH);
+        assertEquals(report.containsAllTags(filteredTags), true);
+        
+        final List<String> anyTags = Arrays.asList(PluginTags.LOW_LEVEL,
+                PluginTags.EXPORT);
+        assertEquals(report.containsAnyTag(anyTags), false);
     }
     
+    @Test
+    public void testExecutionStage() {
+        report.setExecutionStage(PluginExecutionStageConstants.RUNNING);
+        assertEquals(report.getExecutionStage(), PluginExecutionStageConstants.RUNNING);
+    }
+    
+    @Test
+    public void testUndone() {
+        report.setUndone(true);
+        assertEquals(report.isUndone(), true);
+    }
+    
+    @Test
+    public void testAddListener() {
+        final PluginReportListener mockListener = Mockito.mock(PluginReportListener.class);
+        report.addPluginReportListener(mockListener);
+        report.firePluginReportChangedEvent();
+        Mockito.verify(mockListener, Mockito.times(1)).pluginReportChanged(report);
+    }
+        
+    @Test
+    public void testRemoveListener() {
+        final PluginReportListener mockListener = Mockito.mock(PluginReportListener.class);
+        report.addPluginReportListener(mockListener);
+        report.removePluginReportListener(mockListener);
+        report.firePluginReportChangedEvent();        
+        Mockito.verify(mockListener, Mockito.times(0)).pluginReportChanged(report);
+    }
+    
+    @Test
+    public void testTime() {
+        // test start time with some tolerance
+        assertEquals(report.getStartTime(), startTimeMillis, 3); 
+        //test stop time
+        final long currentTimeMillis = System.currentTimeMillis();
+        report.stop();
+        assertEquals(report.getCurrentStep(), 1);
+        assertEquals(report.getTotalSteps(), 0);
+        assertEquals(report.getStopTime(), currentTimeMillis);
+    }
+    
+    @Test
+    public void testError() {
+        final InterruptedException intEx = Mockito.mock(InterruptedException.class);
+        report.setError(intEx);
+        assertEquals(report.getExecutionStage(), PluginExecutionStageConstants.STOPPED);
+        assertEquals(report.getError(), intEx);
+        final PluginException pluginEx = Mockito.mock(PluginException.class);
+        report.setError(pluginEx);
+        assertEquals(report.getExecutionStage(), PluginExecutionStageConstants.STOPPED);
+        assertEquals(report.getError(), pluginEx);
+        final IOException ioEx = Mockito.mock(IOException.class);
+        report.setError(ioEx);
+        assertEquals(report.getError(), ioEx);
+        assertEquals(report.getExecutionStage(), PluginExecutionStageConstants.STOPPED);
+    }
+     
+    @Test
+    public void testChildReport() {
+        assertEquals(report.getUChildReports().size(), 0);
+        final SimplePlugin newPlugin = Mockito.mock(SimplePlugin.class);
+        when(newPlugin.getName()).thenReturn("simple_plugin_name");
+        final String[] testTags = {PluginTags.ANALYTIC};
+        when(newPlugin.getTags()).thenReturn(testTags);
+        final PluginReport addedChildReport = report.addChildReport(newPlugin);
+        assertEquals(addedChildReport.getPluginName(), newPlugin.getName());
+        assertEquals(report.getUChildReports().size(), 1);
+        assertEquals(report.getUChildReports().getFirst(), addedChildReport);
+    }
     
     private class TestPlugin extends SimplePlugin {
 
@@ -131,7 +198,7 @@ public class PluginReportNGTest {
 
         @Override
         public String[] getTags() {
-            String[] testTags = {PluginTags.ANALYTIC, PluginTags.GENERAL,
+            final String[] testTags = {PluginTags.ANALYTIC, PluginTags.GENERAL,
                 PluginTags.SEARCH};
             return testTags;
         }
