6507eecd650067a6ab8ae18aca9df4dbdce9b7fc
Quasar985 <159855864+Quasar985@users.noreply.github.com>
2024-11-12 08:27:50 +1030

    Update LookAndFeelModuleInstall.java (#2227)

--- CorePreferences/src/au/gov/asd/tac/constellation/preferences/LookAndFeelModuleInstall.java
+++ CorePreferences/src/au/gov/asd/tac/constellation/preferences/LookAndFeelModuleInstall.java
@@ -17,6 +17,7 @@ package au.gov.asd.tac.constellation.preferences;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 import org.openide.modules.ModuleInstall;
 import org.openide.util.NbPreferences;
 
@@ -24,20 +25,36 @@ import org.openide.util.NbPreferences;
  * Set the default look and feel to be the FlatLaf Dark theme. This needs to run when modules are loaded so that the LAF
  * can be set to the Swing parts of the application before they load.
  *
+ *
  * @author arcturus
  */
 public class LookAndFeelModuleInstall extends ModuleInstall {
 
-    private static final String FLAT_DARK_THEME_CLASS = "com.formdev.flatlaf.FlatDarkLaf";
+    private static final String FLAT_DARK_THEME_CLASS = FlatDarkLaf.class.getName();
     private static final String FLAT_DARK_THEME_NAME = "FlatLaf Dark";
     private static final String LAF = "laf";
     private static final String NO_THEME_SET = "No Theme Set";
 
     @Override
     public void validate() throws IllegalStateException {
+
+        // Find if FlatLaf Dark has already been installed
+        boolean defaultInstalled = false;
+        for (final LookAndFeelInfo lookAndFeel : UIManager.getInstalledLookAndFeels()) {
+            if (lookAndFeel.getName().equals(FLAT_DARK_THEME_NAME)) {
+                defaultInstalled = true;
+                break;
+            }
+        }
+
+        // If FlatLaf Dark is not installed, install
+        if (!defaultInstalled) {
+            UIManager.installLookAndFeel(new UIManager.LookAndFeelInfo(FLAT_DARK_THEME_NAME, FLAT_DARK_THEME_CLASS));
+        }
+
+        // If no theme is set, set theme to FlatLaf Dark
         if (NO_THEME_SET.equals(NbPreferences.root().node(LAF).get(LAF, NO_THEME_SET))) {
             NbPreferences.root().node(LAF).put(LAF, FLAT_DARK_THEME_CLASS);
-            UIManager.installLookAndFeel(new UIManager.LookAndFeelInfo(FLAT_DARK_THEME_NAME, FlatDarkLaf.class.getName()));
         }
     }
 }
