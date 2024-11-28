2b636f029c726b8c1c0777ae91463aa895ee9835
antares1470 <62311243+antares1470@users.noreply.github.com>
2024-11-28 14:53:43 +1100

    added unit tests for analytic schema factory

--- CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/VisualSchemaFactoryNGTest.java
+++ CoreVisualSchema/test/unit/src/au/gov/asd/tac/constellation/graph/schema/visual/VisualSchemaFactoryNGTest.java
@@ -28,7 +28,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
