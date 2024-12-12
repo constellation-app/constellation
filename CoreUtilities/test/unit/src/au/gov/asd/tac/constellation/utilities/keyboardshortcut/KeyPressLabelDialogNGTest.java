a3c0cd657a74c9c4cf8b4e6e9744e795b1362287
spica198 <spica.a198@gmail.com>
2024-12-12 23:01:12 +1030

    #1875

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/keyboardshortcut/KeyPressLabelDialogNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/keyboardshortcut/KeyPressLabelDialogNGTest.java
@@ -72,7 +72,7 @@ public class KeyPressLabelDialogNGTest {
                  
                  KeyPressLabelDialog k2 = new KeyPressLabelDialog();
                  assertEquals(kl.getDefaultValue(), "test");
-                 assertEquals(kl.getLabel().getText(), StringUtils.EMPTY);
+                 assertEquals(kl.getLabel().getText(), "test");
             }
         });
 
