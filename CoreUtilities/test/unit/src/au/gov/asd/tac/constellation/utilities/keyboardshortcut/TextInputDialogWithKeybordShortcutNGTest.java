1a3961f204898ddba6782161a021fbe0966a90b0
spica198 <spica.a198@gmail.com>
2024-12-15 23:17:42 +1030

    #1875

--- CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/keyboardshortcut/TextInputDialogWithKeybordShortcutNGTest.java
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/keyboardshortcut/TextInputDialogWithKeybordShortcutNGTest.java
@@ -39,6 +39,7 @@ import org.testfx.util.WaitForAsyncUtils;
 import org.testng.Assert;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -113,6 +114,50 @@ public class TextInputDialogWithKeybordShortcutNGTest {
             }
         });
 
+    }
+    
+    @Test
+    public void testClickOnShortcutButton() throws Exception {
+        Optional<String> ks = Optional.of("ctrl 1");
+        final File preferenceDirectory = new File(System.getProperty("java.io.tmpdir") + "/my-preferences.json");
+
+        final Future<Optional<KeyboardShortcutSelectionResult>> future = WaitForAsyncUtils.asyncFx(
+                () -> JsonIODialog.getPreferenceFileName(ks, preferenceDirectory));
+
+        final Stage dialog = getDialog(robot);
+        dialog.setX(0);
+        dialog.setY(0);
+
+        final String input = "myPreferenceFile";
+
+        robot.clickOn(
+                robot.from(dialog.getScene().getRoot())
+                        .lookup(".text-field")
+                        .queryAs(TextField.class)
+        ).write(input);
+
+        robot.clickOn(
+                robot.from(dialog.getScene().getRoot())
+                        .lookup(".button")
+                        .lookup(hasText("Shortcut"))
+                        .queryAs(Button.class)
+        );
+
+          robot.clickOn(
+                robot.from(dialog.getScene().getRoot())
+                        .lookup(".button")
+                        .lookup(hasText("Cancel"))
+                        .queryAs(Button.class)
+        );
+          
+          robot.clickOn(
+                robot.from(dialog.getScene().getRoot())
+                        .lookup(".button")
+                        .lookup(hasText("OK"))
+                        .queryAs(Button.class)
+        );
+          
+    
     }
 
     private Stage getDialog(final FxRobot robot) {
