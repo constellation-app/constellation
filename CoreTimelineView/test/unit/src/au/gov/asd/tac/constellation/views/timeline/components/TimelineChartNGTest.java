5a12d59a2ed3fadc76c60fd7782558df53f56c15
Quasar985 <159855864+Quasar985@users.noreply.github.com>
2025-03-05 10:26:33 +1030

    Fix performance and other misc issues with timeline view (#2244)
    * Create list to hold all node items first before adding to series
    
    * Remove system out
    
    * Fix scroll not working if range is 0
    
    * Fix issue where single transaction would be viewed strangely
    
    * add checking for null dateTime and ignore
    
    * Create TimelineTopComponentNGTest.java
    
    * Commented out test functions
    
    * Update TimelineTopComponentNGTest.java
    
    * Update TimelineTopComponentNGTest.java
    
    * Update TimelineTopComponentNGTest.java
    
    * Add fx toolkit to tests
    
    * Uncomment remaining tests
    
    * Update tests
    
    * Add more getters and setters, improve test cases
    
    * Fix exception and clean up test
    
    * Testing headless code
    
    * Update TimelineTopComponentNGTest.java
    
    * Attempt to fix headless issues
    
    * Remove commented code and add tests for getters
    
    * Refactor TimelinePanel to use getter instead of public variable, add more tests
    
    * Update TimelineChartNGTest.java
    
    * Create TimelinePanelNGTest.java
    
    * Formatting
    
    * Update TimelineTopComponentNGTest.java
    
    * Remove commented code
    
    * Add very basic memory saving techniques
    
    * Bug fix
    
    * Add comment
    
    * Add more variable cleanup
    
    * Fix typo
    
    * Update test
    
    * Add tests
    
    * Add extra null checking
    
    * Remove commented code
    
    * Fix memory not being freed
    
    * Fix timeline issues cause by timelineChart reset
    
    * Fixed memory not being freed on closing the timeline view
    
    * Fix code smells
    
    * Replace timeout time with static variable
    
    * Fix timeline not being updated after closing
    
    * Add comments
    
    * Add wrapper function to updateTimeline to interrupt and restart when called again before completion
    
    * Add tests for GraphManager
    
    * Create OverviewPanel tests
    
    * Comment out some of test
    
    * Fix code smells
    
    * Add basic test to timeline top component
    
    * Make interruption exception catch final
    
    * Fix issues caused by rapid use of scrolling and zooming
    
    * Remove commented code
    
    * Add re-throw thread interrupts
    
    * Update tests for timelinePanel and comments
    
    * Fix incorrect data being displayed on timelineView
    
    * Remove commented code
    
    * Add spinner to timelineView
    
    * Fix test bug
    
    * Update test cases
    
    * Update TimelinePanelNGTest.java
    
    * Add more tests for overview panel and cleanup code

---
+++ CoreTimelineView/test/unit/src/au/gov/asd/tac/constellation/views/timeline/components/TimelineChartNGTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2010-2025 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.views.timeline.components;
+
+import au.gov.asd.tac.constellation.views.timeline.TimelinePanel;
+import au.gov.asd.tac.constellation.views.timeline.TimelineTopComponent;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.input.ScrollEvent;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.testfx.api.FxToolkit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Quasar985
+ */
+public class TimelineChartNGTest {
+
+    private static final Logger LOGGER = Logger.getLogger(TimelineChartNGTest.class.getName());
+
+    @BeforeClass
+    public void setUpClass() throws Exception {
+        if (!FxToolkit.isFXApplicationThreadRunning()) {
+            FxToolkit.registerPrimaryStage();
+        }
+    }
+
+    @AfterClass
+    public void tearDownClass() throws Exception {
+        try {
+            FxToolkit.cleanupStages();
+        } catch (TimeoutException ex) {
+            LOGGER.log(Level.WARNING, "FxToolkit timedout trying to cleanup stages", ex);
+        }
+    }
+
+    /**
+     * Test of performZoom method, of class TimelineChart.
+     */
+    @Test
+    public void testPerformZoomInvalidScrollEvent() {
+        System.out.println("performZoom with invalid scroll event");
+
+        final ScrollEvent mockScrollEvent = mock(ScrollEvent.class);
+        when(mockScrollEvent.getEventType()).thenReturn(ScrollEvent.ANY);
+
+        final TimelinePanel parent = mock(TimelinePanel.class);
+        final Axis<Number> xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+
+        final double mouseX = 0.0;
+
+        final TimelineChart instance = new TimelineChart(parent, xAxis, yAxis);
+        instance.performZoom(mockScrollEvent, mouseX);
+
+        verify(mockScrollEvent, times(1)).getEventType();
+        verify(mockScrollEvent, times(0)).getDeltaY();
+        verify(parent, times(0)).getCoordinator();
+    }
+
+    /**
+     * Test of performZoom method, of class TimelineChart.
+     */
+    @Test
+    public void testPerformZoomPositiveDelta() {
+        System.out.println("performZoom with positive delta");
+
+        final ScrollEvent mockScrollEvent = mock(ScrollEvent.class);
+        final double scrollDelta = 1D;
+        when(mockScrollEvent.getEventType()).thenReturn(ScrollEvent.SCROLL);
+        when(mockScrollEvent.getDeltaY()).thenReturn(scrollDelta);
+
+        final TimelinePanel parent = mock(TimelinePanel.class);
+        final TimelineTopComponent coordinator = mock(TimelineTopComponent.class);
+        when(parent.getCoordinator()).thenReturn(coordinator);
+
+        final Axis<Number> xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+
+        final double mouseX = 0.0;
+        final double lower = 0D;
+        final double upper = 10D;
+
+        final TimelineChart instance = new TimelineChart(parent, xAxis, yAxis);
+        instance.setExtents(lower, upper);
+        instance.performZoom(mockScrollEvent, mouseX);
+
+        verify(mockScrollEvent, times(1)).getEventType();
+        verify(mockScrollEvent, times(1)).getDeltaY();
+        verify(parent, times(1)).getCoordinator();
+    }
+
+    @Test
+    public void testPerformZoomNegativeDelta() {
+        System.out.println("performZoom with negative delta");
+
+        final ScrollEvent mockScrollEvent = mock(ScrollEvent.class);
+        final double scrollDelta = -1D;
+        when(mockScrollEvent.getEventType()).thenReturn(ScrollEvent.SCROLL);
+        when(mockScrollEvent.getDeltaY()).thenReturn(scrollDelta);
+
+        final TimelinePanel parent = mock(TimelinePanel.class);
+        final TimelineTopComponent coordinator = mock(TimelineTopComponent.class);
+        when(parent.getCoordinator()).thenReturn(coordinator);
+
+        final Axis<Number> xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+
+        final double mouseX = 0.0;
+        final double lower = 0D;
+        final double upper = 10D;
+
+        final TimelineChart instance = new TimelineChart(parent, xAxis, yAxis);
+        instance.setExtents(lower, upper);
+        instance.performZoom(mockScrollEvent, mouseX);
+
+        verify(mockScrollEvent, times(1)).getEventType();
+        verify(mockScrollEvent, times(1)).getDeltaY();
+        verify(parent, times(1)).getCoordinator();
+    }
+
+}
