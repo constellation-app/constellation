c9ba3659d97e9b1eeb5124beebf95582fb042c7e
Delphinus8821 <73083035+Delphinus8821@users.noreply.github.com>
2025-03-26 13:43:43 +1030

    Fix issue with Analytic View state when minimized (#2290)
    * Fix issue with Analytic View state when minimized
    
    Also fixed null pointer happening in Notes View when a graph is closed and the welcome page becomes active
    
    * Fixed formatting

--- CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/NotesViewTopComponent.java
+++ CoreNotesView/src/au/gov/asd/tac/constellation/views/notes/NotesViewTopComponent.java
@@ -117,7 +117,7 @@ public class NotesViewTopComponent extends JavaFxTopComponent<NotesViewPane> imp
 
     @Override
     protected void handleNewGraph(final Graph graph) {
-        if (needsUpdate() && activeGraph != graph) {
+        if (needsUpdate() && activeGraph != graph && graph != null) {
             activeGraph = graph;
             notesViewPane.clearNotes();
             notesViewController.readState(activeGraph);
