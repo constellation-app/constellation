7cc950744a955b8b087198fc30e6db7e4805dd7b
Quasar985 <quasar985@gmail.com>
2025-06-27 11:28:37 +0930

    Create JavaFxUtilitiesNGTest.java

---
+++ CoreUtilities/test/unit/src/au/gov/asd/tac/constellation/utilities/javafx/JavaFxUtilitiesNGTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010-2025 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.utilities.javafx;
+
+import javafx.scene.paint.Color;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Quasar985
+ */
+public class JavaFxUtilitiesNGTest {
+
+    /**
+     * Test of awtColorToFXColor method, of class JavaFxUtilities.
+     */
+    @Test
+    public void testAwtColorToFXColor() {
+        System.out.println("awtColorToFXColor");
+        final java.awt.Color awtColor = java.awt.Color.RED;
+        final Color expResult = Color.color(1, 0, 0); // expect red
+
+        final Color result = JavaFxUtilities.awtColorToFXColor(awtColor);
+        assertEquals(result, expResult);
+    }
+
+}
