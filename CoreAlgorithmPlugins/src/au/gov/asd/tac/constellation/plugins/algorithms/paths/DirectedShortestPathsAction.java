f66e8f0b63870a9a0675d167036c2f9e4e084fa2
Quasar985 <quasar985@gmail.com>
2025-10-09 15:40:10 +1030

    Add alert when no nodes are selected

--- CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/paths/DirectedShortestPathsAction.java
+++ CoreAlgorithmPlugins/src/au/gov/asd/tac/constellation/plugins/algorithms/paths/DirectedShortestPathsAction.java
@@ -15,11 +15,19 @@
  */
 package au.gov.asd.tac.constellation.plugins.algorithms.paths;
 
+import au.gov.asd.tac.constellation.graph.Graph;
+import au.gov.asd.tac.constellation.graph.GraphElementType;
+import au.gov.asd.tac.constellation.graph.ReadableGraph;
 import au.gov.asd.tac.constellation.graph.node.GraphNode;
+import au.gov.asd.tac.constellation.graph.schema.visual.concept.VisualConcept;
 import au.gov.asd.tac.constellation.plugins.PluginExecution;
 import au.gov.asd.tac.constellation.plugins.algorithms.AlgorithmPluginRegistry;
+import au.gov.asd.tac.constellation.utilities.gui.NotifyDisplayer;
+import au.gov.asd.tac.constellation.utilities.gui.ScreenWindowsHelper;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javafx.application.Platform;
+import javafx.scene.control.Alert.AlertType;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
@@ -41,14 +49,44 @@ public final class DirectedShortestPathsAction implements ActionListener {
 
     private final GraphNode context;
 
+    private static final String ALERT_HEADER_TEXT = "No nodes selected!";
+    private static final String ALERT_TEXT = "Directed Shortest Paths requires at least 1 node to be selected";
+
     public DirectedShortestPathsAction(final GraphNode context) {
         this.context = context;
     }
 
     @Override
     public void actionPerformed(final ActionEvent event) {
+        if (!isAnyNodeSelected(context.getGraph())) {
+            Platform.runLater(() -> NotifyDisplayer.displayAlert("Attention", ALERT_HEADER_TEXT, ALERT_TEXT, AlertType.NONE, ScreenWindowsHelper.getMainWindowCentrePoint()));
+            return;
+        }
         PluginExecution.withPlugin(AlgorithmPluginRegistry.DIRECTED_SHORTEST_PATHS)
                 .interactively(true)
                 .executeLater(context.getGraph());
+    }
+
+    private boolean isAnyNodeSelected(final Graph graph) {
+        if (graph == null) {
+            return false;
+        }
+
+        try (final ReadableGraph rg = graph.getReadableGraph()) {
+            final int vxSelectedAttr = rg.getAttribute(GraphElementType.VERTEX, VisualConcept.VertexAttribute.SELECTED.getName());
+
+            if (vxSelectedAttr == Graph.NOT_FOUND) {
+                return false;
+            }
+
+            final int vxCount = rg.getVertexCount();
+            for (int position = 0; position < vxCount; position++) {
+                if (rg.getBooleanValue(vxSelectedAttr, rg.getVertex(position))) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
