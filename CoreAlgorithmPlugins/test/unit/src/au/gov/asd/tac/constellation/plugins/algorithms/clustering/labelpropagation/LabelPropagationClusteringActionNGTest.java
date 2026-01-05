584e14cb1943a297116898569821385aa7a9c55a
andromeda-224 <ameda31224@gmail.com>
2026-01-05 15:38:31 +1030

    Added unit tests

---
+++ CoreAlgorithmPlugins/test/unit/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/labelpropagation/LabelPropagationClusteringActionNGTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2010-2026 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.plugins.algorithms.clustering.labelpropagation;
+
+
+import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.graph.node.GraphNode;
+import au.gov.asd.tac.constellation.plugins.PluginInteraction;
+import au.gov.asd.tac.constellation.plugins.algorithms.clustering.ClusterUtilities;
+import au.gov.asd.tac.constellation.plugins.algorithms.clustering.labelpropagation.LabelPropagationClusteringAction.LabelPropagationClusteringCleanupPlugin;
+import au.gov.asd.tac.constellation.plugins.parameters.PluginParameters;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author andromeda-224
+ */
+public class LabelPropagationClusteringActionNGTest {
+    
+    public LabelPropagationClusteringActionNGTest() {
+    }
+
+    @Test
+    public void testConstructor() {
+        System.out.println("testConstructor");
+        final GraphNode mockGraphNode = mock(GraphNode.class);
+        final LabelPropagationClusteringAction instance = new LabelPropagationClusteringAction(mockGraphNode);
+        assertEquals(instance.getClass(), LabelPropagationClusteringAction.class);
+    }
+    
+    @Test
+    public void testLabelPropagationClusteringCleanupPlugin() throws InterruptedException {
+        System.out.println("testLabelPropagationClusteringCleanupPlugin");
+        final GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
+        final int link = 101;
+        
+        when(mockGraph.getVertexCount()).thenReturn(1);
+        when(mockGraph.getLinkCount()).thenReturn(1);
+        when(mockGraph.getVertexCapacity()).thenReturn(1);
+        when(mockGraph.getLinkCount()).thenReturn(1);
+        when(mockGraph.getVertexLink(0, 0)).thenReturn(link);
+        when(mockGraph.getVertexLinkCount(0)).thenReturn(1);
+        when(mockGraph.getLinkTransactionCount(link)).thenReturn(1);
+        PluginParameters parametersMock = mock(PluginParameters.class);
+        PluginInteraction pluginInteractionMock = mock(PluginInteraction.class);
+        
+        final LabelPropagationClusteringCleanupPlugin instance = new LabelPropagationClusteringCleanupPlugin();
+        assertEquals(instance.getClass(), LabelPropagationClusteringCleanupPlugin.class);
+        assertEquals(instance.getName(), "Label Propagation Clustering: Cleanup");
+        try (MockedStatic<ClusterUtilities> clusterUtilitiesMockStatic = Mockito.mockStatic(ClusterUtilities.class)) {
+            instance.edit(mockGraph, pluginInteractionMock, parametersMock);
+            clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.colorClusters(Mockito.any(), Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt()), Mockito.times(1));
+            clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.explodeGraph(Mockito.any(), Mockito.anyInt()), Mockito.times(1));
+            instance.edit(mockGraph, pluginInteractionMock, parametersMock);
+            clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.colorClusters(Mockito.any(), Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt()), Mockito.times(2));
+            clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.explodeGraph(Mockito.any(), Mockito.anyInt()), Mockito.times(2));
+        } catch (Exception ex) {
+           fail(ex.toString());
+        }
+    }
+
+}
---
+++ CoreAlgorithmPlugins/test/unit/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/labelpropagation/LabelPropagationClusteringNGTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2010-2026 Australian Signals Directorate
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.gov.asd.tac.constellation.plugins.algorithms.clustering.labelpropagation;
+
+
+import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author andromeda-224
+ */
+public class LabelPropagationClusteringNGTest {
+    
+    public LabelPropagationClusteringNGTest() {
+    }
+
+    @Test
+    public void testConstructor() {
+        System.out.println("testConstructor");
+        final GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
+        final LabelPropagationClustering instance = new LabelPropagationClustering(mockGraph);
+        assertEquals(instance.getClass(), LabelPropagationClustering.class);
+    }
+    
+    @Test
+    public void testCluster() {
+        System.out.println("testCluster");
+        final GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
+        final int link = 101;
+        
+        when(mockGraph.getVertexCount()).thenReturn(1);
+        when(mockGraph.getLinkCount()).thenReturn(1);
+        when(mockGraph.getVertexCapacity()).thenReturn(1);
+        when(mockGraph.getLinkCount()).thenReturn(1);
+        when(mockGraph.getVertexLink(0, 0)).thenReturn(link);
+        when(mockGraph.getVertexLinkCount(0)).thenReturn(1);
+        when(mockGraph.getLinkTransactionCount(link)).thenReturn(1);
+
+        final LabelPropagationClustering instance = new LabelPropagationClustering(mockGraph);
+        assertEquals(instance.getClass(), LabelPropagationClustering.class);
+        instance.cluster();
+        assertEquals(instance.getCluster(0), 0);
+        verify(mockGraph, times(3)).getVertex(anyInt());
+        verify(mockGraph, times(1)).setIntValue(anyInt(), anyInt(), anyInt());
+    }
+
+}
