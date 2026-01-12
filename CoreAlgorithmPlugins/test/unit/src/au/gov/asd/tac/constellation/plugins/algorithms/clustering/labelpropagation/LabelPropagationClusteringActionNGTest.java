ca3f25b8054968ccaf9b4bf216c43d6885e72b9e
andromeda-224 <ameda31224@gmail.com>
2026-01-12 18:23:56 +1030

    Update LabelPropagationClusteringActionNGTest.java

--- CoreAlgorithmPlugins/test/unit/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/labelpropagation/LabelPropagationClusteringActionNGTest.java
+++ CoreAlgorithmPlugins/test/unit/src/au/gov/asd/tac/constellation/plugins/algorithms/clustering/labelpropagation/LabelPropagationClusteringActionNGTest.java
@@ -15,16 +15,21 @@
  */
 package au.gov.asd.tac.constellation.plugins.algorithms.clustering.labelpropagation;
 
-
 import au.gov.asd.tac.constellation.graph.GraphWriteMethods;
+import au.gov.asd.tac.constellation.graph.node.GraphNode;
 import au.gov.asd.tac.constellation.plugins.PluginInteraction;
 import au.gov.asd.tac.constellation.plugins.algorithms.clustering.ClusterUtilities;
 import au.gov.asd.tac.constellation.plugins.algorithms.clustering.labelpropagation.LabelPropagationClusteringAction.LabelPropagationClusteringCleanupPlugin;
 import au.gov.asd.tac.constellation.plugins.parameters.PluginParameters;
+import java.awt.event.ActionEvent;
+import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 /**
@@ -32,13 +37,13 @@ import org.testng.annotations.Test;
  * @author andromeda-224
  */
 public class LabelPropagationClusteringActionNGTest {
-        
+
     @Test
     public void testLabelPropagationClusteringCleanupPlugin() throws InterruptedException {
         System.out.println("testLabelPropagationClusteringCleanupPlugin");
         final GraphWriteMethods mockGraph = mock(GraphWriteMethods.class);
         final int link = 101;
-        
+
         when(mockGraph.getVertexCount()).thenReturn(1);
         when(mockGraph.getLinkCount()).thenReturn(1);
         when(mockGraph.getVertexCapacity()).thenReturn(1);
@@ -48,7 +53,7 @@ public class LabelPropagationClusteringActionNGTest {
         when(mockGraph.getLinkTransactionCount(link)).thenReturn(1);
         PluginParameters parametersMock = mock(PluginParameters.class);
         PluginInteraction pluginInteractionMock = mock(PluginInteraction.class);
-        
+
         final LabelPropagationClusteringCleanupPlugin instance = new LabelPropagationClusteringCleanupPlugin();
         try (MockedStatic<ClusterUtilities> clusterUtilitiesMockStatic = Mockito.mockStatic(ClusterUtilities.class)) {
             instance.edit(mockGraph, pluginInteractionMock, parametersMock);
@@ -58,7 +63,25 @@ public class LabelPropagationClusteringActionNGTest {
             clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.colorClusters(Mockito.any(), Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt()), Mockito.times(2));
             clusterUtilitiesMockStatic.verify(() -> ClusterUtilities.explodeGraph(Mockito.any(), Mockito.anyInt()), Mockito.times(2));
         } catch (Exception ex) {
-           throw ex;
+            throw ex;
+        }
+    }
+
+    @Test
+    public void testLabelPropagationClusteringActionPerformed() throws InterruptedException {
+        System.out.println("testLabelPropagationClusteringActionPerformed");
+
+        try (MockedConstruction<LabelPropagationClusteringCleanupPlugin> mockConstruction = Mockito.mockConstruction(LabelPropagationClusteringCleanupPlugin.class)) {
+            final GraphNode mockContext = mock(GraphNode.class);
+            final LabelPropagationClusteringAction instance = new LabelPropagationClusteringAction(mockContext);
+            final ActionEvent mockEvent = mock(ActionEvent.class);
+            
+            instance.actionPerformed(mockEvent);            
+            // Verify that the constructor was called exactly once
+            assertEquals(1, mockConstruction.constructed().size());
+            verify(mockContext, times(1)).getGraph();
+        } catch (Exception ex) {
+            throw ex;
         }
     }
 }
