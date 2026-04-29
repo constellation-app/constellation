0073f76ba17ec3c3caddeb0a86f38f33c7068f8f
Auriga2 <64826928+Auriga2@users.noreply.github.com>
2026-04-29 12:31:50 +0930

    Fix the lambda expression to use a final variable

--- CoreGraphFramework/src/au/gov/asd/tac/constellation/graph/StoreGraph.java
+++ CoreGraphFramework/src/au/gov/asd/tac/constellation/graph/StoreGraph.java
@@ -1535,12 +1535,11 @@ public class StoreGraph extends LockingTarget implements GraphWriteMethods, Seri
         if (existingAttributes != null && existingAttributes[elementType.ordinal()] >= 0) {
             final int attribute = existingAttributes[elementType.ordinal()];
             final String existingAttributeType = attributes[attribute].getAttributeType();
-            String message = "";
-            if (existingAttributeType.equals(attributeType)) {
-                message = elementType + " attribute Name `" + label + "` already exists. Please choose a different Name.";
-            } else {
-                message = elementType + " attribute Name `" + label + "` already exists, of type " + existingAttributeType + ". Please choose a different Name.";
-            }
+
+            final String message = existingAttributeType.equals(attributeType)
+                    ? elementType + " attribute name `" + label + "` already exists. Please choose a different name."
+                    : elementType + " attribute name `" + label + "` already exists (type: " + existingAttributeType + "). Please choose a different name.";
+
             Platform.runLater(() -> NotifyDisplayer.displayAlert("Add " + elementType + " Attribute", "Duplicate Attribute Name", message, Alert.AlertType.ERROR));
             return attribute;
         }
