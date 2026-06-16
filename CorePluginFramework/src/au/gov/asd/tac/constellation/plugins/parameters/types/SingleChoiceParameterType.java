cf28e883485d24ff66ae9da92798e74a9d03ee79
Delphinus8821 <delphinus8821@gmail.com>
2026-06-16 12:52:43 +0930

    Add change listener to single choice inputs

--- CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/SingleChoiceParameterType.java
+++ CorePluginFramework/src/au/gov/asd/tac/constellation/plugins/parameters/types/SingleChoiceParameterType.java
@@ -140,6 +140,9 @@ public class SingleChoiceParameterType extends PluginParameterType<SingleChoiceP
 
             parameterValue.setOptions(options);
             parameter.setProperty(CHOICES, new Object());
+            
+            // TODO: find out why this is needed after the single choice input was updated 
+            parameter.fireChangeEvent(ParameterChange.VALUE);
         }
     }
 
