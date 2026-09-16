e6992cb5390029364a3640515d6cca5ff20996b2
Auriga2 <64826928+Auriga2@users.noreply.github.com>
2026-09-16 10:31:38 +0930

    Better fix with review comments

--- CoreUtilities/src/au/gov/asd/tac/constellation/utilities/geospatial/Shape.java
+++ CoreUtilities/src/au/gov/asd/tac/constellation/utilities/geospatial/Shape.java
@@ -651,7 +651,7 @@ public class Shape {
                                             && attributeValue != null
                                             && attributesOfValidType.get(entry.getKey()) != null
                                             && attributesOfValidType.get(entry.getKey()).contains(compatibleAttributeName)) {
-                                        writableFeature.setAttribute(compatibleAttributeName.strip(), attributeValue);
+                                        writableFeature.setAttribute(compatibleAttributeName, attributeValue);
                                     } else if (schemaAttributes.containsKey(compatibleAttributeName) && attributeValue != null) {
                                         writableFeature.setAttribute(compatibleAttributeName, attributeValue.toString());
                                     }
@@ -729,6 +729,6 @@ public class Shape {
             compatibleHeader = compatibleHeader.substring(0, 10);
         }
 
-        return compatibleHeader;
+        return compatibleHeader.strip();
     }
 }
